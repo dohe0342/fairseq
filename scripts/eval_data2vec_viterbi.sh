547cea5881ba1cdb4f17d9ac848007a0a8c262d3
dohe0342 <kimdohe1070@gmail.com>
2022-02-17 17:02:55 +0900

    from local

--- scripts/eval_data2vec_viterbi.sh
+++ scripts/eval_data2vec_viterbi.sh
@@ -1,7 +1,7 @@
 model=$1
 python ../examples/speech_recognition/new/infer.py --config-dir ../examples/speech_recognition/new/conf \
-	--config-name infer --beam 1 task=audio_finetuning task.data=/home/work/workspace/LibriSpeech/manifests common.user_dir=examples/data2vec \
+	--config-name infer task=audio_finetuning task.data=/home/work/workspace/LibriSpeech/manifests common.user_dir=examples/data2vec \
 	task.labels=ltr decoding.type=viterbi \
 	decoding.unique_wer_file=False \
 	dataset.gen_subset=dev-clean \
-	common_eval.path=/home/work/workspace/models/data2vec_model/$model decoding.beam=5 distributed_training.distributed_world_size=1
+	common_eval.path=/home/work/workspace/models/data2vec_model/$model decoding.beam=1 distributed_training.distributed_world_size=1

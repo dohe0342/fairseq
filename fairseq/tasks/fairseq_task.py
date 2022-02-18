450fa91ebfd3115c26f75353203f7f58baf266c9
dohe0342 <kimdohe1070@gmail.com>
2022-02-18 16:45:35 +0900

    from local

--- fairseq/tasks/fairseq_task.py
+++ fairseq/tasks/fairseq_task.py
@@ -505,6 +505,8 @@ class FairseqTask(object):
                   gradient
                 - logging outputs to display while training
         """
+        print('fucking!!!!!!!', uses_branch)
+
         if uses_branch:
             for i in range(12):     
                 model.train()

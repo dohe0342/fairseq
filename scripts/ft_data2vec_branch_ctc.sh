cbaedcf46c75346c1212c935467054ca2f8c022a
dohe0342 <kimdohe1070@gmail.com>
2022-02-18 21:21:10 +0900

    from local

--- scripts/ft_data2vec_branch_ctc.sh
+++ scripts/ft_data2vec_branch_ctc.sh
@@ -4,7 +4,7 @@ fairseq-hydra-train \
     --config-name base_100h \
 	common.user_dir=examples/data2vec \
 	checkpoint.best_checkpoint_metric=wer_12 \
-	dataset.valid_subset=dev-other-sub \
+	dataset.valid_subset=dev-other \
 	criterion._name=branch_ctc \
     task.data=/home/work/workspace/LibriSpeech/manifests \
     model.w2v_path=/home/work/workspace/models/data2vec_model/audio_base_ls.pt \

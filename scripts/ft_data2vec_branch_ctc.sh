f7fbf3bc9bba3ad28b6b4316752c1e01c59d6117
dohe0342 <kimdohe1070@gmail.com>
2022-02-18 20:44:01 +0900

    from local

--- scripts/ft_data2vec_branch_ctc.sh
+++ scripts/ft_data2vec_branch_ctc.sh
@@ -3,6 +3,7 @@ fairseq-hydra-train \
 	--config-dir /home/work/workspace/fairseq/examples/wav2vec/config/finetuning \
     --config-name base_100h \
 	common.user_dir=examples/data2vec \
+	dataset.valid_subset=dev-other-sub \
 	criterion._name=branch_ctc \
     task.data=/home/work/workspace/LibriSpeech/manifests \
     model.w2v_path=/home/work/workspace/models/data2vec_model/audio_base_ls.pt \

a7bdcbfe18e32f3dc92e5b98ea3f2f2149b65b99
dohe0342 <kimdohe1070@gmail.com>
2022-02-21 19:28:50 +0900

    from local

--- scripts/ft_data2vec.sh
+++ scripts/ft_data2vec.sh
@@ -5,5 +5,4 @@ fairseq-hydra-train \
 	common.user_dir=examples/data2vec \
     task.data=/home/work/workspace/LibriSpeech/manifests \
     model.w2v_path=/home/work/workspace/models/data2vec_model/audio_base_ls.pt \
-	model.freeze_finetune_updates=80000 \
 	+task.uses_branch=False

8a1563909892973114c404e078561a4e377d0266
dohe0342 <kimdohe1070@gmail.com>
2022-02-24 00:32:57 +0900

    from local

--- scripts/ft_data2vec_branch_ctc.sh
+++ scripts/ft_data2vec_branch_ctc.sh
@@ -2,13 +2,13 @@ git pull
 fairseq-hydra-train \
 	--config-dir /workspace/fairseq/examples/wav2vec/config/finetuning \
     --config-name base_100h \
-	+model.overrides=uses_branch:true \
 	common.user_dir=examples/data2vec \
 	checkpoint.best_checkpoint_metric=wer_12 \
 	dataset.valid_subset=dev-other \
 	criterion._name=branch_ctc \
     task.data=/workspace/LibriSpeech/manifests \
     model.w2v_path=/workspace/models/data2vec_model/audio_base_ls.pt \
+	#+task.uses_branch=true \
+	#+model.overrides=uses_branch:true \
 	#task.overrides=uses_branch:true \
 	#+model.branch_ctc=true \
-	#+task.uses_branch=true \

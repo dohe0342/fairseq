97814ac915b8ec46ff21c5561a374b6f1c112467
dohe0342 <kimdohe1070@gmail.com>
2022-02-24 18:21:44 +0900

    from local

---
+++ scripts/summit/ft_data2vec_branch_ctc.sh
@@ -0,0 +1,14 @@
+git pull
+fairseq-hydra-train \
+	--config-dir /workspace/fairseq/examples/wav2vec/config/finetuning \
+    --config-name base_100h \
+	common.user_dir=examples/data2vec \
+	checkpoint.best_checkpoint_metric=wer_12 \
+	dataset.valid_subset=dev-other \
+	criterion._name=branch_ctc \
+    task.data=/workspace/LibriSpeech/manifests \
+    model.w2v_path=/workspace/models/data2vec_model/audio_base_ls.pt \
+	+task.uses_branch=true \
+	+model.branch_ctc=true \
+	#+model.overrides=uses_branch:true \
+	#task.overrides=uses_branch:true \
---
+++ scripts/whale/ft_data2vec.sh
@@ -0,0 +1,8 @@
+git pull
+fairseq-hydra-train \
+	--config-dir /home/work/workspace/fairseq/examples/wav2vec/config/finetuning \
+    --config-name base_100h \
+	common.user_dir=examples/data2vec \
+    task.data=/home/work/workspace/LibriSpeech/manifests \
+    model.w2v_path=/home/work/workspace/models/data2vec_model/audio_base_ls.pt \
+	+task.uses_branch=False

1260a20964aa9732a27d4ce0c3753a2a321b8d8b
dohe0342 <kimdohe1070@gmail.com>
2022-02-24 23:08:21 +0900

    from local

--- scripts/whale/eval_data2vec_viterbi.sh
+++ scripts/whale/eval_data2vec_viterbi.sh
@@ -3,9 +3,12 @@ model=$1
 
 python /home/work/workspace/fairseq/examples/speech_recognition/new/infer.py \
 	--config-dir /home/work/workspace/fairseq/examples/speech_recognition/new/conf \
---config-name infer task=audio_finetuning task.data=/home/work/workspace/LibriSpeech/manifests common.user_dir=examples/data2vec \
-task.labels=ltr decoding.type=viterbi \
-decoding.unique_wer_file=False \
-dataset.gen_subset=test-other \
-decoding.beam=5 \
-common_eval.path=/home/work/workspace/models/data2vec_model/$model distributed_training.distributed_world_size=1
+	--config-name infer \
+	task=audio_finetuning \
+	task.data=/home/work/workspace/LibriSpeech/manifests \
+	common.user_dir=examples/data2vec \
+	task.labels=ltr decoding.type=viterbi \
+	decoding.unique_wer_file=False \
+	dataset.gen_subset=test-other \
+	decoding.beam=5 \
+	common_eval.path=/home/work/workspace/models/data2vec_model/$model distributed_training.distributed_world_size=1

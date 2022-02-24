98730648a6b442eb1c61be7cfdf086f88a43bb83
dohe0342 <kimdohe1070@gmail.com>
2022-02-24 18:22:30 +0900

    from local

--- scripts/ft_data2vec.sh
+++
@@ -1,8 +0,0 @@
-git pull
-fairseq-hydra-train \
-	--config-dir /workspace/fairseq/examples/wav2vec/config/finetuning \
-    --config-name base_100h \
-	common.user_dir=examples/data2vec \
-    task.data=/workspace/LibriSpeech/manifests \
-    model.w2v_path=/workspace/models/data2vec_model/audio_base_ls.pt \
-	+task.uses_branch=False
--- scripts/pretrain_data2vec.sh
+++
@@ -1,5 +0,0 @@
-python fairseq_cli/hydra_train.py -m --config-dir /home/work/workspace/fairseq/examples/data2vec/config/audio/pretraining \
-  --config-name base_librispeech \
-  task.data=/home/work/workspace/LibriSpeech/train-clean-100 common.user_dir=examples/data2vec model._name=$1
-
-#python ~/workspace/send_email.py -c "data2vec kd done (error maybe) :( "

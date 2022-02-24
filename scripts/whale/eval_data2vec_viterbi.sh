393d04c41a88904f2d52cc7f70484062dde074a6
dohe0342 <kimdohe1070@gmail.com>
2022-02-24 23:07:43 +0900

    from local

--- scripts/whale/eval_data2vec_viterbi.sh
+++ scripts/whale/eval_data2vec_viterbi.sh
@@ -1,7 +1,8 @@
 git pull
 model=$1
 
-python /home/work/workspace/fairseq/examples/speech_recognition/new/infer.py --config-dir ../examples/speech_recognition/new/conf \
+python /home/work/workspace/fairseq/examples/speech_recognition/new/infer.py \
+	--config-dir /home/work/workspace/fairseq/examples/speech_recognition/new/conf \
 --config-name infer task=audio_finetuning task.data=/home/work/workspace/LibriSpeech/manifests common.user_dir=examples/data2vec \
 task.labels=ltr decoding.type=viterbi \
 decoding.unique_wer_file=False \

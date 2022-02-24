8a0dd3c72be5cf48ff34af981eb10e841324a1d4
dohe0342 <kimdohe1070@gmail.com>
2022-02-24 19:46:30 +0900

    from local

--- fairseq/models/wav2vec/wav2vec2.py
+++ fairseq/models/wav2vec/wav2vec2.py
@@ -290,11 +290,6 @@ class Wav2Vec2Config(FairseqDataclass):
     )
     fp16: bool = field(default=False, metadata={"help": "If fp16 is being used"})
 
-    branch_ctc: bool = field(
-        default=False,
-        metadata={"help": "use branch ctc. make ctc layer after each transformer layer"},
-    )
-
 
 @register_model("wav2vec2", dataclass=Wav2Vec2Config)
 class Wav2Vec2Model(BaseFairseqModel):

331b49502cbb3187325c4921821acac3a2a74b5e
dohe0342 <kimdohe1070@gmail.com>
2022-02-24 23:51:58 +0900

    from local

--- scripts/whale/eval_data2vec_viterbi.sh
+++ scripts/whale/eval_data2vec_viterbi.sh
@@ -1,15 +1,18 @@
 git pull
 model=$1
 
-python /home/work/workspace/fairseq/examples/speech_recognition/new/infer.py \
-	--config-dir /home/work/workspace/fairseq/examples/speech_recognition/new/conf \
-	--config-name infer \
-	task=audio_finetuning \
-	task.data=/home/work/workspace/LibriSpeech/manifests \
-	common.user_dir=examples/data2vec \
-	task.labels=ltr \
-	decoding.type=viterbi \
-	decoding.unique_wer_file=False \
-	dataset.gen_subset=test-other \
-	common_eval.path=/home/work/workspace/models/data2vec_model/$model \
-	distributed_training.distributed_world_size=1
+for subset in "dev-clean" "dev-other" "test-clean" "test-other"
+do
+	python /home/work/workspace/fairseq/examples/speech_recognition/new/infer.py \
+		--config-dir /home/work/workspace/fairseq/examples/speech_recognition/new/conf \
+		--config-name infer \
+		task=audio_finetuning \
+		task.data=/home/work/workspace/LibriSpeech/manifests \
+		common.user_dir=examples/data2vec \
+		task.labels=ltr \
+		decoding.type=viterbi \
+		decoding.unique_wer_file=False \
+		dataset.gen_subset=$subset \
+		common_eval.path=/home/work/workspace/models/data2vec_model/$model \
+		distributed_training.distributed_world_size=1
+done

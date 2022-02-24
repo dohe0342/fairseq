d1ea8e216ceda318cab0caf956b5949748fe3df9
dohe0342 <kimdohe1070@gmail.com>
2022-02-24 23:39:25 +0900

    from local

--- examples/speech_recognition/new/infer.py
+++ examples/speech_recognition/new/infer.py
@@ -307,8 +307,7 @@ class InferenceProcessor:
             models=self.models,
             sample=sample,
         )
-        '''
-        print(sample)
+        #print(sample)
         
         label_dict = {0: '',
                       1: '29',
@@ -363,7 +362,6 @@ class InferenceProcessor:
             print(hypo_sentence)
 
             exit()
-        '''
 
         num_generated_tokens = sum(len(h[0]["tokens"]) for h in hypos)
         self.gen_timer.stop(num_generated_tokens)

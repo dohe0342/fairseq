2f026f8db364bee05a809f0b6a68003cb69495b6
dohe0342 <kimdohe1070@gmail.com>
2022-02-28 14:41:52 +0900

    from local

--- fairseq/models/wav2vec/wav2vec2_asr.py
+++ fairseq/models/wav2vec/wav2vec2_asr.py
@@ -705,6 +705,7 @@ class Wav2VecEncoderBranchCtcV1(Wav2VecEncoder):
 
 class Wav2VecEncoderBranchCtcV2(Wav2VecEncoder):
     def __init__(self, cfg: Wav2Vec2AsrConfig, output_size=None):
+        self.cfg = cfg
         self.apply_mask = cfg.apply_mask
 
         arg_overrides = {

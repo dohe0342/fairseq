23487f994cb1e35e46c43bd97d9b14cb3cfe167f
dohe0342 <kimdohe1070@gmail.com>
2022-02-28 19:52:13 +0900

    from local

--- fairseq/models/wav2vec/wav2vec2.py
+++ fairseq/models/wav2vec/wav2vec2.py
@@ -1002,7 +1002,7 @@ class TransformerEncoder(nn.Module):
         self.apply(init_bert_params)
 
     def forward(self, x, padding_mask=None, layer=None, tgt_layer=None, layer_wise_detach=None):
-        x, layer_results, dropped_layer = self.extract_features(x, padding_mask=padding_mask,tgt_layer=tgt_layer, layer_wise_detach=layer_wise_detach)
+        x, layer_results = self.extract_features(x, padding_mask=padding_mask,tgt_layer=tgt_layer, layer_wise_detach=layer_wise_detach)
 
         if self.layer_norm_first and layer is None:
             x = self.layer_norm(x)

a71d5d8d6c3b72ccd3b0eb9affd84014aa434b26
dohe0342 <kimdohe1070@gmail.com>
2022-02-28 16:10:07 +0900

    from local

--- fairseq/models/wav2vec/wav2vec2_asr.py
+++ fairseq/models/wav2vec/wav2vec2_asr.py
@@ -870,27 +870,27 @@ class Wav2VecEncoderBranchCtcV2(Wav2VecEncoder):
         
         result = []
         if self.proj:
-            if x7 != 0:
+            if type(x7) == int:
                 x7 = self.final_dropout(x7)
                 x7 = self.proj[6](x7)
                 result.append(x7)
-            if x8 != 0:
+            if type(x8) == int:
                 x8 = self.final_dropout(x8)
                 x8 = self.proj[7](x8)
                 result.append(x8)
-            if x9 != 0:
+            if type(x9) == int:
                 x9 = self.final_dropout(x9)
                 x9 = self.proj[8](x9)
                 result.append(x9)
-            if x10 != 0:
+            if type(x10) === int:
                 x10 = self.final_dropout(x10)
                 x10 = self.proj[9](x10)
                 result.append(x10)
-            if x11 != 0:
+            if type(x11) == int:
                 x11 = self.final_dropout(x11)
                 x11 = self.proj[10](x11)
                 result.append(x11)
-            if x12 != 0:
+            if type(x12) == int:
                 x12 = self.final_dropout(x12)
                 x12 = self.proj[11](x12)
                 result.append(x12)

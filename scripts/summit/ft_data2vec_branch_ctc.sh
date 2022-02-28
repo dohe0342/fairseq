e51bf47c96d34a64149dd68578686d0206f85613
dohe0342 <kimdohe1070@gmail.com>
2022-02-28 16:27:29 +0900

    from local

--- scripts/summit/ft_data2vec_branch_ctc.sh
+++ scripts/summit/ft_data2vec_branch_ctc.sh
@@ -1,7 +1,7 @@
 git pull
 fairseq-hydra-train \
 	--config-dir /workspace/fairseq/examples/wav2vec/config/finetuning \
-    --config-name base_100h \
+    --config-name base_100h_summit \
 	common.user_dir=examples/data2vec \
 	checkpoint.best_checkpoint_metric=wer_12 \
 	dataset.valid_subset=dev-other \

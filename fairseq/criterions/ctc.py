16f00322d259afe5d3b8a472fd74d2eb3c6e2040
dohe0342 <kimdohe1070@gmail.com>
2022-02-28 14:14:36 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -495,7 +495,7 @@ class BranchCtcCriterionV2(CtcCriterion):
                 non_padding_mask = ~net_output["padding_mask"]
                 input_lengths = non_padding_mask.long().sum(-1)
             else:
-                input_lengths = lprobs.new_full(
+                input_lengths = lprobs[0].new_full(
                     (lprobs.size(1),), lprobs.size(0), dtype=torch.long
                 )
 

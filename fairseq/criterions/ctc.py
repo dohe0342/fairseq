32067a846f1a2c4340a74b59593cd17bee866411
dohe0342 <kimdohe1070@gmail.com>
2022-02-28 12:29:26 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -477,3 +477,186 @@ class BranchCtcCriterion(CtcCriterion):
                 else float("nan"),
             )
 
+
+@register_criterion("branch_ctc_v2", dataclass=CtcCriterionConfig)
+class BranchCtcCriterion(CtcCriterion):
+    def __init__(self, cfg: CtcCriterionConfig, task: FairseqTask):
+        super().__init__(CtcCriterionConfig, task)
+    def forward(self, model, sample, reduce=True, tgt_layer=0):
+        net_output = model(tgt_layer=tgt_layer, **sample["net_input"])
+        lprobs = model.get_normalized_probs(
+            net_output, log_probs=True
+        ).contiguous()  # (T, B, C) from the encoder
+
+        if "src_lengths" in sample["net_input"]:
+            input_lengths = sample["net_input"]["src_lengths"]
+        else:
+            if net_output["padding_mask"] is not None:
+                non_padding_mask = ~net_output["padding_mask"]
+                input_lengths = non_padding_mask.long().sum(-1)
+            else:
+                input_lengths = lprobs.new_full(
+                    (lprobs.size(1),), lprobs.size(0), dtype=torch.long
+                )
+
+        pad_mask = (sample["target"] != self.pad_idx) & (
+            sample["target"] != self.eos_idx
+        )
+        targets_flat = sample["target"].masked_select(pad_mask)
+        if "target_lengths" in sample:
+            target_lengths = sample["target_lengths"]
+        else:
+            target_lengths = pad_mask.sum(-1)
+
+        with torch.backends.cudnn.flags(enabled=False):
+            loss = F.ctc_loss(
+                lprobs,
+                targets_flat,
+                input_lengths,
+                target_lengths,
+                blank=self.blank_idx,
+                reduction="sum",
+                zero_infinity=self.zero_infinity,
+            )
+
+        ntokens = (
+            sample["ntokens"] if "ntokens" in sample else target_lengths.sum().item()
+        )
+
+        sample_size = sample["target"].size(0) if self.sentence_avg else ntokens
+        logging_output = {
+            f"loss_{tgt_layer}": utils.item(loss.data),  # * sample['ntokens'],
+            "ntokens": ntokens,
+            "nsentences": sample["id"].numel(),
+            "sample_size": sample_size,
+        }
+
+        if not model.training:
+            import editdistance
+
+            with torch.no_grad():
+                lprobs_t = lprobs.transpose(0, 1).float().contiguous().cpu()
+
+                c_err = 0
+                c_len = 0
+                w_errs = 0
+                w_len = 0
+                wv_errs = 0
+                for lp, t, inp_l in zip(
+                    lprobs_t,
+                    sample["target_label"]
+                    if "target_label" in sample
+                    else sample["target"],
+                    input_lengths,
+                ):
+                    lp = lp[:inp_l].unsqueeze(0)
+
+                    decoded = None
+                    if self.w2l_decoder is not None:
+                        decoded = self.w2l_decoder.decode(lp)
+                        if len(decoded) < 1:
+                            decoded = None
+                        else:
+                            decoded = decoded[0]
+                            if len(decoded) < 1:
+                                decoded = None
+                            else:
+                                decoded = decoded[0]
+
+                    p = (t != self.task.target_dictionary.pad()) & (
+                        t != self.task.target_dictionary.eos()
+                    )
+                    targ = t[p]
+                    targ_units = self.task.target_dictionary.string(targ)
+                    targ_units_arr = targ.tolist()
+
+                    toks = lp.argmax(dim=-1).unique_consecutive()
+                    pred_units_arr = toks[toks != self.blank_idx].tolist()
+
+                    c_err += editdistance.eval(pred_units_arr, targ_units_arr)
+                    c_len += len(targ_units_arr)
+
+                    targ_words = post_process(targ_units, self.post_process).split()
+
+                    pred_units = self.task.target_dictionary.string(pred_units_arr)
+                    pred_words_raw = post_process(pred_units, self.post_process).split()
+
+                    if decoded is not None and "words" in decoded:
+                        pred_words = decoded["words"]
+                        w_errs += editdistance.eval(pred_words, targ_words)
+                        wv_errs += editdistance.eval(pred_words_raw, targ_words)
+                    else:
+                        dist = editdistance.eval(pred_words_raw, targ_words)
+                        w_errs += dist
+                        wv_errs += dist
+
+                    w_len += len(targ_words)
+
+                logging_output[f"wv_errors_{tgt_layer}"] = wv_errs
+                logging_output[f"w_errors_{tgt_layer}"] = w_errs
+                logging_output[f"w_total_{tgt_layer}"] = w_len
+                logging_output[f"c_errors_{tgt_layer}"] = c_err
+                logging_output[f"c_total_{tgt_layer}"] = c_len
+
+        return loss, sample_size, logging_output
+
+    @staticmethod
+    def reduce_metrics(logging_outputs, layer_num=12) -> None:
+        """Aggregate logging outputs from data parallel training."""
+        loss_sum = utils.item(sum(log.get(f"loss_{layer_num}", 0) for log in logging_outputs))
+        ntokens = utils.item(sum(log.get("ntokens", 0) for log in logging_outputs))
+        nsentences = utils.item(
+            sum(log.get("nsentences", 0) for log in logging_outputs)
+        )
+        sample_size = utils.item(
+            sum(log.get("sample_size", 0) for log in logging_outputs)
+        )
+
+        metrics.log_scalar(
+            f"loss_{layer_num}", loss_sum / sample_size / math.log(2), sample_size, round=3
+        )
+        metrics.log_scalar("ntokens", ntokens)
+        metrics.log_scalar("nsentences", nsentences)
+        if sample_size != ntokens:
+            metrics.log_scalar(
+                "nll_loss", loss_sum / ntokens / math.log(2), ntokens, round=3
+            )
+
+        c_errors = sum(log.get(f"c_errors_{layer_num}", 0) for log in logging_outputs)
+        metrics.log_scalar(f"_c_errors_{layer_num}", c_errors)
+        c_total = sum(log.get(f"c_total_{layer_num}", 0) for log in logging_outputs)
+        metrics.log_scalar(f"_c_total_{layer_num}", c_total)
+        w_errors = sum(log.get(f"w_errors_{layer_num}", 0) for log in logging_outputs)
+        metrics.log_scalar(f"_w_errors_{layer_num}", w_errors)
+        wv_errors = sum(log.get(f"wv_errors_{layer_num}", 0) for log in logging_outputs)
+        metrics.log_scalar(f"_wv_errors_{layer_num}", wv_errors)
+        w_total = sum(log.get(f"w_total_{layer_num}", 0) for log in logging_outputs)
+        metrics.log_scalar(f"_w_total_{layer_num}", w_total)
+        
+        if c_total > 0:
+            metrics.log_derived(
+                f"uer_{layer_num}",
+                lambda meters: safe_round(
+                    meters[f"_c_errors_{layer_num}"].sum * 100.0 / meters[f"_c_total_{layer_num}"].sum, 3
+                )
+                if meters[f"_c_total_{layer_num}"].sum > 0
+                else float("nan"),
+            )
+        if w_total > 0:
+            metrics.log_derived(
+                f"wer_{layer_num}",
+                lambda meters: safe_round(
+                    meters[f"_w_errors_{layer_num}"].sum * 100.0 / meters[f"_w_total_{layer_num}"].sum, 3
+                )
+                if meters[f"_w_total_{layer_num}"].sum > 0
+                else float("nan"),
+            )
+            metrics.log_derived(
+                f"raw_wer_{layer_num}",
+                lambda meters: safe_round(
+                    meters[f"_wv_errors_{layer_num}"].sum * 100.0 / meters[f"_w_total_{layer_num}"].sum, 3
+                )
+                if meters[f"_w_total_{layer_num}"].sum > 0
+                else float("nan"),
+            )
+

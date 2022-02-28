daab18d5940cc59b00e685eb50f3880f9d8f7f8c
dohe0342 <kimdohe1070@gmail.com>
2022-02-28 14:27:12 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -483,7 +483,7 @@ class BranchCtcCriterionV2(CtcCriterion):
     def __init__(self, cfg: CtcCriterionConfig, task: FairseqTask):
         super().__init__(CtcCriterionConfig, task)
     def forward(self, model, sample, reduce=True):
-        net_output = model(tgt_layer=tgt_layer, **sample["net_input"])
+        net_output = model(**sample["net_input"])
         lprobs_list = model.get_normalized_probs(
             net_output, log_probs=True
         ).contiguous()  # (T, B, C) from the encoder

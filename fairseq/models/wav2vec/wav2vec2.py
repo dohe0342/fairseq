8838f91242a029db93f5904351125e40d73fe595
dohe0342 <kimdohe1070@gmail.com>
2022-02-28 15:52:46 +0900

    from local

--- fairseq/models/wav2vec/wav2vec2.py
+++ fairseq/models/wav2vec/wav2vec2.py
@@ -1099,7 +1099,7 @@ class TransformerEncoder(nn.Module):
                 )
             for i, layer_result in enumerate(layer_results):
                 if not layer_result:
-                    layer_results[i] = undo_pad(layer_result)
+                    layer_results[i] = undo_pad(*layer_result)
 
         return x, layer_results
 

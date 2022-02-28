2b1636539f59dde0ac086a703d8e9ef99e924dad
dohe0342 <kimdohe1070@gmail.com>
2022-02-28 18:00:08 +0900

    from local

--- scripts/whale/ft_data2vec_branch_ctc.sh
+++ scripts/whale/ft_data2vec_branch_ctc.sh
@@ -5,9 +5,9 @@ fairseq-hydra-train \
 	common.user_dir=examples/data2vec \
 	checkpoint.best_checkpoint_metric=wer_12 \
 	dataset.valid_subset=dev-other \
-	criterion._name=branch_ctc \
+	criterion._name=branch_ctc_v2 \
     task.data=/home/work/workspace/LibriSpeech/manifests \
     model.w2v_path=/home/work/workspace/models/data2vec_model/audio_base_ls.pt \
-	+model.branch_ctc_v1=true \
+	+model.branch_ctc_v2=true \
 	#+model.overrides=uses_branch:true \
 	#task.overrides=uses_branch:true \

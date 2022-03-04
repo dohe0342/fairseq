63262dd88c81071d02263944829deee76df0b05d
dohe0342 <kimdohe1070@gmail.com>
2022-03-04 23:24:01 +0900

    from local

--- scripts/whale/pretrain_data2vec.sh
+++ scripts/whale/pretrain_data2vec.sh
@@ -1,3 +1,4 @@
+git pull
 python /home/work/workspace/fairseq/fairseq_cli/hydra_train.py -m --config-dir /home/work/workspace/fairseq/examples/data2vec/config/audio/pretraining \
   --config-name base_librispeech \
   task.data=/home/work/workspace/LibriSpeech/manifests common.user_dir=examples/data2vec #model._name=$1

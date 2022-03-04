37978dcd5ad9cca1538dcb7d614897a33cd49065
dohe0342 <kimdohe1070@gmail.com>
2022-03-04 21:40:11 +0900

    from local

--- fairseq/modules/functional.py
+++ fairseq/modules/functional.py
@@ -4692,17 +4692,22 @@ def multi_head_attention_forward(
                 _b = _b[_start:]
             v = linear(value, _w, _b)
     else:
-        q_proj_weight_non_opt = torch.jit._unwrap_optional(q_proj_weight)
-        len1, len2 = q_proj_weight_non_opt.size()
-        assert len1 == embed_dim and len2 == query.size(-1)
+        if fuse:
+            qk_proj_weight_non_opt = torch.jit._unwrap_optional(fused_w)
+            len1, len2 = qk_proj_weight_non_opt.size()
+            assert len1 == (key.size(-1)+1) and len2 == (embed_dim+1)
+        else:
+            q_proj_weight_non_opt = torch.jit._unwrap_optional(q_proj_weight)
+            len1, len2 = q_proj_weight_non_opt.size()
+            assert len1 == embed_dim and len2 == query.size(-1)
 
-        k_proj_weight_non_opt = torch.jit._unwrap_optional(k_proj_weight)
-        len1, len2 = k_proj_weight_non_opt.size()
-        assert len1 == embed_dim and len2 == key.size(-1)
+            k_proj_weight_non_opt = torch.jit._unwrap_optional(k_proj_weight)
+            len1, len2 = k_proj_weight_non_opt.size()
+            assert len1 == embed_dim and len2 == key.size(-1)
 
-        v_proj_weight_non_opt = torch.jit._unwrap_optional(v_proj_weight)
-        len1, len2 = v_proj_weight_non_opt.size()
-        assert len1 == embed_dim and len2 == value.size(-1)
+            v_proj_weight_non_opt = torch.jit._unwrap_optional(v_proj_weight)
+            len1, len2 = v_proj_weight_non_opt.size()
+            assert len1 == embed_dim and len2 == value.size(-1)
 
         if in_proj_bias is not None:
             q = linear(query, q_proj_weight_non_opt, in_proj_bias[0:embed_dim])

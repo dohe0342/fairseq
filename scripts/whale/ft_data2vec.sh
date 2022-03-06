3ee28f664c494731ac2bfb64884d79e44ddf1b47
dohe0342 <kimdohe1070@gmail.com>
2022-03-07 02:14:40 +0900

    from local

--- scripts/whale/ft_data2vec.sh
+++ scripts/whale/ft_data2vec.sh
@@ -4,5 +4,6 @@ fairseq-hydra-train \
     --config-name base_100h \
 	common.user_dir=examples/data2vec \
     task.data=/home/work/workspace/LibriSpeech/manifests \
-    model.w2v_path=/home/work/workspace/models/data2vec_model/audio_base_ls.pt \
+    model.w2v_path=/home/work/workspace/fairseq/scripts/whale/multirun/2022-03-04/18-15-58/0/checkpoints/checkpoint_last.pt\
+    #model.w2v_path=/home/work/workspace/models/data2vec_model/audio_base_ls.pt \
 	#+model.init_transformer=true

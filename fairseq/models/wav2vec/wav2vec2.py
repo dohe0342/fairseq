2c3090ceba85a915973625d00691e68a22db0346
dohe0342 <kimdohe1070@gmail.com>
2022-03-09 16:06:30 +0900

    from local

--- fairseq/models/wav2vec/wav2vec2.py
+++ fairseq/models/wav2vec/wav2vec2.py
@@ -1355,7 +1355,6 @@ class LayerInstanceNorm(nn.Module):
         self.normalized_shape = tuple(normalized_shape)  # type: ignore[arg-type]
         self.eps = eps
         self.elementwise_affine = elementwise_affine
-        '''
         if self.elementwise_affine:
             self.weight = nn.Parameter(torch.empty(self.normalized_shape, **factory_kwargs))
             self.bias = nn.Parameter(torch.empty(self.normalized_shape, **factory_kwargs))
@@ -1364,7 +1363,6 @@ class LayerInstanceNorm(nn.Module):
             self.register_parameter('bias', None)
         
         #self.reset_parameters()
-        '''
     #def reset_parameters(self) -> None:
     #    if self.elementwise_affine:
     #        init.ones_(self.weight)

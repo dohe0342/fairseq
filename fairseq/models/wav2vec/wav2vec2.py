b272c164b54ead939c6a6fad6575a6e5ce031b44
dohe0342 <kimdohe1070@gmail.com>
2022-03-09 15:53:58 +0900

    from local

--- fairseq/models/wav2vec/wav2vec2.py
+++ fairseq/models/wav2vec/wav2vec2.py
@@ -1310,7 +1310,7 @@ class TransformerSentenceEncoderLayer(nn.Module):
             x = self.dropout1(x)
             x = residual + x
             
-            ada_ln_p = random.random() < 0.01
+            ada_ln_p = random.random() < 0.
             if ada_ln_p:
                 x = self.self_attn_ins_layer_norm(x)
             else:

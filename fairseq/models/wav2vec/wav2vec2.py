6d4f6fda726b142b5a3a2ec14deff652d7cc7670
dohe0342 <kimdohe1070@gmail.com>
2022-03-09 16:30:31 +0900

    from local

--- fairseq/models/wav2vec/wav2vec2.py
+++ fairseq/models/wav2vec/wav2vec2.py
@@ -1361,7 +1361,6 @@ class LayerInstanceNorm(nn.Module):
         else:
             self.register_parameter('weight', None)
             self.register_parameter('bias', None)
-        print('insnorm weight = ', self.weight)
         
         #self.reset_parameters()
     #def reset_parameters(self) -> None:

e0a4a8e0846f1b1ef9ffe22ab12f475e0041d308
dohe0342 <kimdohe1070@gmail.com>
2022-03-12 14:25:37 +0900

    from local

--- fairseq/trainer.py
+++ fairseq/trainer.py
@@ -801,9 +801,10 @@ class Trainer(object):
         logging_outputs, sample_size, ooms = [], 0, 0
         for i, sample in enumerate(samples):  # delayed update loop
             sample, is_dummy_batch = self._prepare_sample(sample)
-            for key in sample:
-                for k in sample['net_input']:
-                    print(k)
+            #for key in sample:
+            #    for k in sample['net_input']:
+            #        print(k)
+            print(sample)
             #print('sample size = ', sample['net_input']['source'].size())
 
             def maybe_no_sync():

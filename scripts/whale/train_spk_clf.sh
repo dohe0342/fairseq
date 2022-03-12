2f3fcb4bfd5416ecc63cdb706d5623f5939a8cdb
dohe0342 <kimdohe1070@gmail.com>
2022-03-12 18:35:33 +0900

    from local

--- scripts/whale/train_spk_clf.sh
+++ scripts/whale/train_spk_clf.sh
@@ -2,21 +2,16 @@ git pull
 source ~/.bashrc
 model=$1
 
-for subset in "dev-clean" "dev-other" "test-clean" "test-other"
-do
-	echo "====================   $model // $subset   ===================="
-	CUDA_VISIBLE_DEVICES=$2 python /home/work/workspace/fairseq/examples/speech_recognition/new/train_spk_clf.py \
-		--config-dir /home/work/workspace/fairseq/examples/speech_recognition/new/conf \
-		--config-name infer \
-		task=audio_finetuning \
-		task.data=/home/work/workspace/LibriSpeech/manifests \
-		common.user_dir=examples/data2vec \
-		task.labels=ltr \
-		decoding.type=viterbi \
-		decoding.unique_wer_file=False \
-		dataset.gen_subset=$subset \
-		common_eval.path=/home/work/workspace/models/data2vec_model/$model \
-		distributed_training.distributed_world_size=1
-	echo ""
-	echo ""
-done
+echo "====================   $model // $subset   ===================="
+CUDA_VISIBLE_DEVICES=$2 python /home/work/workspace/fairseq/examples/speech_recognition/new/train_spk_clf.py \
+	--config-dir /home/work/workspace/fairseq/examples/speech_recognition/new/conf \
+	--config-name infer \
+	task=audio_finetuning \
+	task.data=/home/work/workspace/LibriSpeech/manifests \
+	common.user_dir=examples/data2vec \
+	task.labels=ltr \
+	decoding.type=viterbi \
+	decoding.unique_wer_file=False \
+	dataset.gen_subset=dev-other \
+	common_eval.path=/home/work/workspace/models/data2vec_model/$model \
+	distributed_training.distributed_world_size=1

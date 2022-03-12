2237f3adb8345370c8859a025b87b7ad9c50cf9e
dohe0342 <kimdohe1070@gmail.com>
2022-03-12 15:29:45 +0900

    from local

--- fairseq/models/wav2vec/wav2vec2_asr.py
+++ fairseq/models/wav2vec/wav2vec2_asr.py
@@ -1068,6 +1068,8 @@ class Wav2VecEncoderSpkClf(Wav2VecEncoder):
 
         if self.proj:
             x = self.proj(x)
+        
+        print(x.size())
 
         return {
             "encoder_out": x,  # T x B x C

099b713947f5a3d095ddff296dd33b9068a0cd48
dohe0342 <kimdohe1070@gmail.com>
2022-03-12 18:51:29 +0900

    from local

--- examples/speech_recognition/new/train_spk_clf.py
+++ examples/speech_recognition/new/train_spk_clf.py
@@ -445,6 +445,7 @@ class InferenceProcessor:
         for i, hypo in enumerate(hypos):
             if i == 0:
                 features = hypo[0]['emission'].mean(0).unsqueeze(0)
+                print(features.size())
             else:
                 features = torch.stack([features, \ 
                                         hypo[0]['emission'].mean(0).unsqueeze(0)], dim=0)

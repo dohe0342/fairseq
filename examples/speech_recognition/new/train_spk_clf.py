f7f67e7db0a04d3f90fa79010aea3e1a0588c4f2
dohe0342 <kimdohe1070@gmail.com>
2022-03-14 21:48:37 +0900

    from local

--- examples/speech_recognition/new/train_spk_clf.py
+++ examples/speech_recognition/new/train_spk_clf.py
@@ -553,7 +553,7 @@ def main(cfg: InferConfig) -> float:
     #with InferenceProcessor(cfg) as processor:
     #processor = InferenceProcessor(cfg)
     criterion = torch.nn.CrossEntropyLoss()
-    optim = [torch.optim.Adam(spk_clf[i].parameters(), lr=0.00001) for i in range(11)]
+    optim = [torch.optim.Adam(spk_clf[i].parameters(), lr=0.00001) for i in range(clf_num)]
 
     for epoch in range(10):
         output = []

2416442bb21e4366ffb4dedcd8d42e979c94cc8b
dohe0342 <kimdohe1070@gmail.com>
2022-03-14 11:54:50 +0900

    from local

--- examples/speech_recognition/new/train_spk_clf.py
+++ examples/speech_recognition/new/train_spk_clf.py
@@ -450,7 +450,13 @@ class InferenceProcessor:
             models=self.models,
             sample=sample,
         )
-        
+        encoder_input = { 
+            k: v for k, v in sample["net_input"].items() if k != "prev_output_tokens"
+        }
+
+        model = models[0]
+        encoder_out = model(**encoder_input)
+
         features = None
         target = []
         #print(hypos[4][0]['emission'].size())

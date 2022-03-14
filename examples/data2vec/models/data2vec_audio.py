245d7651721504929c9345cbaf12de4f61816def
dohe0342 <kimdohe1070@gmail.com>
2022-03-14 23:38:41 +0900

    from local

--- examples/data2vec/models/data2vec_audio.py
+++ examples/data2vec/models/data2vec_audio.py
@@ -390,7 +390,7 @@ class Data2VecAudioModel(BaseFairseqModel):
                 "x": x,
                 "padding_mask": padding_mask,
                 "layer_results": layer_results,
-                "dropped_layer": layer_results[-1],
+                #"dropped_layer": layer_results[-1],
             }
 
         result = {

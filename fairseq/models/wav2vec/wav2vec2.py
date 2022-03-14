704aeebdbe120c4dc3c8e12ecf77b4c281dd48d8
dohe0342 <kimdohe1070@gmail.com>
2022-03-14 23:15:38 +0900

    from local

--- fairseq/models/wav2vec/wav2vec2.py
+++ fairseq/models/wav2vec/wav2vec2.py
@@ -676,6 +676,7 @@ class Wav2Vec2Model(BaseFairseqModel):
             mask_indices = None
 
         x, layer_results = self.encoder(x, padding_mask=padding_mask, layer=layer)
+        print('len', len(layer_results))
 
         if features_only:
             return {

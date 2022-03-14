977fe56717559944f704a61981d2f797d0673b96
dohe0342 <kimdohe1070@gmail.com>
2022-03-14 23:20:05 +0900

    from local

--- examples/speech_recognition/new/train_spk_clf.py
+++ examples/speech_recognition/new/train_spk_clf.py
@@ -422,8 +422,8 @@ def main(cfg: InferConfig) -> float:
 
     logger.info(cfg.common_eval.path)
     
-    emb_dim = 768
-    clf_num = 12
+    emb_dim = 1024
+    clf_num = 24
     class_num = 251 if cfg.dataset.gen_subset=='train-100' else 1166
     batch_count = 2992 if cfg.dataset.gen_subset=='train-100' else 14586
 

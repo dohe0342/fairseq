4b825775a193570b693bdf3e29c2a9b81526fa18
dohe0342 <kimdohe1070@gmail.com>
2022-03-14 11:40:03 +0900

    from local

--- examples/speech_recognition/new/train_spk_clf.py
+++ examples/speech_recognition/new/train_spk_clf.py
@@ -457,7 +457,7 @@ class InferenceProcessor:
         print(hypos[0][0]['emission'].dtype)
         for i, hypo in enumerate(hypos):
             if i == 0:
-                features = hypo[0]['emission'].mean(0).unsqueeze(0)
+                features = hypo[0]['emission'].mean(0).unsqueeze(0).to('cuda')
             else:
                 features = torch.cat([features, hypo[0]['emission'].mean(0).unsqueeze(0)], dim=0)
         

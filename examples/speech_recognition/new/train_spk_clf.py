25748abf02a053628589ce0a59ecd07eb217792d
dohe0342 <kimdohe1070@gmail.com>
2022-03-14 11:36:08 +0900

    from local

--- examples/speech_recognition/new/train_spk_clf.py
+++ examples/speech_recognition/new/train_spk_clf.py
@@ -454,7 +454,7 @@ class InferenceProcessor:
         
         features = None
         target = []
-        print(hypos.type)
+        print(hypos[0][0]['emission'].type)
         for i, hypo in enumerate(hypos):
             if i == 0:
                 features = hypo[0]['emission'].mean(0).unsqueeze(0)

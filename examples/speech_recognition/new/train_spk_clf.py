75bd6da102b6ae18b4b67278876dbdf943be6637
dohe0342 <kimdohe1070@gmail.com>
2022-03-14 13:48:56 +0900

    from local

--- examples/speech_recognition/new/train_spk_clf.py
+++ examples/speech_recognition/new/train_spk_clf.py
@@ -570,7 +570,7 @@ def main(cfg: InferConfig) -> float:
                 loss = criterion(prob[i], target)
                 loss.backward()
                 optim[i].step()
-        print(res*100/all)
+    print(res*100/all)
     '''
         processor.log_generation_time()
 

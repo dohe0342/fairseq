8c19cb5b3aa00b645205e1966211644712e2a191
dohe0342 <kimdohe1070@gmail.com>
2022-03-18 16:28:01 +0900

    from local

--- fairseq/models/wav2vec/wav2vec2_asr.py
+++ fairseq/models/wav2vec/wav2vec2_asr.py
@@ -1066,7 +1066,7 @@ class Wav2VecEncoderSpkClf(Wav2VecEncoder):
 
         if self.proj:
             x = self.proj_ctc(x)
-            spk = self.proj_spk(x)
+            spk_logit = self.proj_spk(x)
         
         return {
             "encoder_out": x,  # T x B x C

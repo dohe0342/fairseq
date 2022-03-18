3896a3fe8064a6e1e1f12d0ff59993e6b497bcfe
dohe0342 <kimdohe1070@gmail.com>
2022-03-18 10:44:58 +0900

    from local

--- examples/speech_recognition/new/train_spk_clf_conv.py
+++ examples/speech_recognition/new/train_spk_clf_conv.py
@@ -356,7 +356,7 @@ class InferenceProcessor:
             model = self.models[0]
             #encoder_out = model(**encoder_input)
             encoder_out = model.w2v_encoder.w2v_model.feature_extractor(sample['net_input']['source'])
-            print(encoder_out.size())
+            encoder_out = encoder_out.mean(dim=1)
         
         features = None
         target = []

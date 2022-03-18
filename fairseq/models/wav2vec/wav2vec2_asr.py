4a8a4538997e7dcdb7e19474bb835524c2b2bb2f
dohe0342 <kimdohe1070@gmail.com>
2022-03-18 18:46:01 +0900

    from local

--- fairseq/models/wav2vec/wav2vec2_asr.py
+++ fairseq/models/wav2vec/wav2vec2_asr.py
@@ -1081,7 +1081,7 @@ class Wav2VecEncoderSpkClf(Wav2VecEncoder):
             "encoder_out": x,  # T x B x C
             "padding_mask": padding_mask,  # B x T,
             "layer_results": res["layer_results"],
-            "spk_prob": spk_pob,
+            "spk_prob": spk_prob,
         }
 
 class TransformerDecoder(FairseqIncrementalDecoder):

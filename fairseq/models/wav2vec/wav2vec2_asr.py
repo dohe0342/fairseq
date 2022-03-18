3dfed253ff6ec182b18598b9c4777df82363d897
dohe0342 <kimdohe1070@gmail.com>
2022-03-18 16:25:06 +0900

    from local

--- fairseq/models/wav2vec/wav2vec2_asr.py
+++ fairseq/models/wav2vec/wav2vec2_asr.py
@@ -242,8 +242,8 @@ class Wav2VecCtc(BaseFairseqModel):
             w2v_encoder = Wav2VecEncoderBranchCtcV1(cfg, len(task.target_dictionary))
         elif cfg.branch_ctc_v2 or cfg.branch_ctc_v3:
             w2v_encoder = Wav2VecEncoderBranchCtcV2(cfg, len(task.target_dictionary))
-        #elif add_apk_info:
-        #    w2v_encoder = Wav2VecEncoderSpkClf(cfg, len(task.target_dictionary), task.spk_num)
+        elif del_apk_info:
+            w2v_encoder = Wav2VecEncoderSpkClf(cfg, len(task.target_dictionary), task.spk_num)
 
         else:
             w2v_encoder = Wav2VecEncoder(cfg, len(task.target_dictionary))

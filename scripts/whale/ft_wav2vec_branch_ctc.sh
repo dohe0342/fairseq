da3266e005b9c9b26374047f5893e82149193aa6
dohe0342 <kimdohe1070@gmail.com>
2022-03-21 11:48:20 +0900

    from local

---
+++ scripts/whale/ft_wav2vec_branch_ctc.sh
@@ -0,0 +1,14 @@
+git pull
+fairseq-hydra-train \
+	--config-dir /home/work/workspace/fairseq/examples/wav2vec/config/finetuning \
+    --config-name base_100h_whale \
+	common.user_dir=examples/data2vec \
+	checkpoint.best_checkpoint_metric=wer \
+	dataset.valid_subset=dev-other \
+	criterion._name=spk_clf \
+    task.data=/home/work/workspace/LibriSpeech/manifests \
+    model.w2v_path=/home/work/workspace/models/data2vec_model/audio_base_ls.pt \
+	+model.del_spk_info=true \
+	#+model.branch_ctc_v2=true \
+	#+model.overrides=uses_branch:true \
+	#task.overrides=uses_branch:true \

7363a72e754d4857b61436f8426f7c0a9ecf7896
dohe0342 <kimdohe1070@gmail.com>
2022-03-21 11:49:08 +0900

    from local

--- scripts/whale/ft_data2vec_branch_ctc.sh
+++ scripts/whale/ft_data2vec_branch_ctc.sh
@@ -7,7 +7,7 @@ fairseq-hydra-train \
 	dataset.valid_subset=dev-other \
 	criterion._name=spk_clf \
     task.data=/home/work/workspace/LibriSpeech/manifests \
-    model.w2v_path=/home/work/workspace/models/data2vec_model/audio_base_ls.pt \
+    model.w2v_path=/home/work/workspace/models/wav2vec_model/wav2vec_small.pt \
 	+model.del_spk_info=true \
 	#+model.branch_ctc_v2=true \
 	#+model.overrides=uses_branch:true \

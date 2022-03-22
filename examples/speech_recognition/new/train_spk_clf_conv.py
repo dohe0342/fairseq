4e6328223b373a4a0544e536e59f6d1f7afd33b6
dohe0342 <kimdohe1070@gmail.com>
2022-03-22 17:06:53 +0900

    from local

--- examples/speech_recognition/new/train_spk_clf_conv.py
+++ examples/speech_recognition/new/train_spk_clf_conv.py
@@ -362,6 +362,7 @@ class InferenceProcessor:
         
         print(encoder_out[0].size())
         print(encoder_out[1].size())
+
         #features = [encoder_out['layer_results'][i][0].mean(0).to('cuda') for i in range(len(self.spk_clf))]
         features = [encoder_out.mean(2)]
         

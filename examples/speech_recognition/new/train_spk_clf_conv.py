f0b5af422484d99007392e0bddba851e19e36f8a
dohe0342 <kimdohe1070@gmail.com>
2022-03-22 17:05:50 +0900

    from local

--- examples/speech_recognition/new/train_spk_clf_conv.py
+++ examples/speech_recognition/new/train_spk_clf_conv.py
@@ -354,10 +354,8 @@ class InferenceProcessor:
             }
 
             model = self.models[0]
-            #encoder_out = model(**encoder_input)
-            encoder_out = model.w2v_encoder.w2v_model.feature_extractor(sample['net_input']['source'])
-            
-            encoder_out1 = model.w2v_encoder.w2v_model.post_extract_proj(encoder_out)
+            encoder_out = model(**encoder_input)
+            #encoder_out = model.w2v_encoder.w2v_model.feature_extractor(sample['net_input']['source'])
         
         features = None
         target = []

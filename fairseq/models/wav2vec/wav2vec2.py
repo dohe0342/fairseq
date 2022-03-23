0fb9b705d6e84e9ae99e58a9c63c8ba2a1bcf332
dohe0342 <kimdohe1070@gmail.com>
2022-03-23 15:25:15 +0900

    from local

--- fairseq/models/wav2vec/wav2vec2.py
+++ fairseq/models/wav2vec/wav2vec2.py
@@ -679,7 +679,7 @@ class Wav2Vec2Model(BaseFairseqModel):
         enc_input = x.clone()
         x, layer_results = self.encoder(x, padding_mask=padding_mask, layer=layer)
         
-        if 0:
+        if 1:
             return (conv_features, enc_input, x)
 
         if features_only:
@@ -1047,7 +1047,7 @@ class TransformerEncoder(nn.Module):
                 padding_mask, self.required_seq_len_multiple, dim=-1, value=True
             )
         x = F.dropout(x, p=self.dropout, training=self.training)
-        if 0:
+        if 1:
             return x, None
 
         # B x T x C -> T x B x C

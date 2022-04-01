9f150803259370a8e23e7440284cc8c2fa95a108
dohe0342 <kimdohe1070@gmail.com>
2022-04-01 14:15:55 +0900

    from local

--- fairseq/models/viewmaker.py
+++ fairseq/models/viewmaker.py
@@ -319,6 +319,20 @@ class ConvLayer(torch.nn.Module):
         return out
 
 
+class ConvLaye2(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride):
+        super(ConvLayer, self).__init__()
+        reflection_padding = kernel_size // 2
+        self.reflection_pad = torch.nn.ReflectionPad2d(reflection_padding)
+        self.conv2d = torch.nn.Conv2d(
+            in_channels, out_channels, kernel_size, stride)
+
+    def forward(self, x):
+        out = self.reflection_pad(x)
+        out = self.conv2d(out)
+        return out
+
+
 class ResidualBlock(torch.nn.Module):
     """ResidualBlock
     introduced in: https://arxiv.org/abs/1512.03385

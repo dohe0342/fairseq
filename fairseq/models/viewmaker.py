27fe3844040ee5eab4da3a9b07f744b8ef7d096c
dohe0342 <kimdohe1070@gmail.com>
2022-04-01 13:13:29 +0900

    from local

--- fairseq/models/viewmaker.py
+++ fairseq/models/viewmaker.py
@@ -338,6 +338,28 @@ class ResidualBlock(torch.nn.Module):
         return out
 
 
+class ResidualBlock(torch.nn.Module):
+    """ResidualBlock
+    introduced in: https://arxiv.org/abs/1512.03385
+    recommended architecture: http://torch.ch/blog/2016/02/04/resnets.html
+    """
+
+    def __init__(self, channels, activation='relu'):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = ConvLayer(channels, channels, kernel_size=3, stride=1)
+        self.in1 = torch.nn.InstanceNorm2d(channels, affine=True)
+        self.conv2 = ConvLayer(channels, channels, kernel_size=3, stride=1)
+        self.in2 = torch.nn.InstanceNorm2d(channels, affine=True)
+        self.act = ACTIVATIONS[activation]()
+
+    def forward(self, x):
+        residual = x
+        out = self.act(self.in1(self.conv1(x)))
+        out = self.in2(self.conv2(out))
+        out = out + residual
+        return out
+
+
 class UpsampleConvLayer(torch.nn.Module):
     """UpsampleConvLayer
     Upsamples the input and then does a convolution. This method gives better results

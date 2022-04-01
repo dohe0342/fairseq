8b7247add99ae1194c43714c2d87f59eeb6bbce2
dohe0342 <kimdohe1070@gmail.com>
2022-04-01 14:23:04 +0900

    from local

--- fairseq/models/viewmaker.py
+++ fairseq/models/viewmaker.py
@@ -190,7 +190,8 @@ class Viewmaker2(torch.nn.Module):
         self.in3 = torch.nn.InstanceNorm2d(128, affine=True)
         '''
         
-        self.conv1 = nn.Conv1d(512+100, 512, kernel_size=2, stride=1)
+        #self.conv1 = nn.Conv1d(512+100, 512, kernel_size=2, stride=1)
+        self.conv1 = ConvLayer(512+100, 512, kernel_size=2, stride=1)
         self.in1 = torch.nn.InstanceNorm1d(512, affine=True)
         self.conv2 = nn.Conv1d(512, 512, kernel_size=2, stride=1)
         self.in2 = torch.nn.InstanceNorm1d(512, affine=True)

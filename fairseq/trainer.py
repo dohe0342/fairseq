53a9b64dd66c66a799e1377f0d9699c39091c7c0
dohe0342 <kimdohe1070@gmail.com>
2022-04-02 13:51:27 +0900

    from local

--- fairseq/trainer.py
+++ fairseq/trainer.py
@@ -294,7 +294,7 @@ class Trainer(object):
                 chain(self.model.parameters(), self.criterion.parameters()),
             )
         )
-        print(list(filter(lambda p: p.requires_grad, self.model.paramters())))
+        print(list(filter(lambda p: p.requires_grad, self.model.parameters())))
 
         if self.is_fsdp and self.cfg.common.fp16:
             # FullyShardedDataParallel always uses MemoryEfficientFP16 wrapper,

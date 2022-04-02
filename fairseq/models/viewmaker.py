d2735b951b3dd7ea4d6b399696e84d9ce6abb092
dohe0342 <kimdohe1070@gmail.com>
2022-04-02 11:04:31 +0900

    from local

--- fairseq/models/viewmaker.py
+++ fairseq/models/viewmaker.py
@@ -153,7 +153,7 @@ class Viewmaker(torch.nn.Module):
 
 class Viewmaker2(torch.nn.Module):
     '''Viewmaker network that stochastically maps a multichannel 2D input to an output of the same size.'''
-    def __init__(self, num_channels=512, distortion_budget=0.05, activation='relu',  
+    def __init__(self, num_channels=512, distortion_budget=0.05, activation='gelu',  
                 clamp=True, frequency_domain=False, downsample_to=False, num_res_blocks=3):
         '''Initialize the Viewmaker network.
 

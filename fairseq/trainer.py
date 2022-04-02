5a45df1be8d514f432db74006c8c727e8fe82e34
dohe0342 <kimdohe1070@gmail.com>
2022-04-02 13:21:42 +0900

    from local

--- fairseq/trainer.py
+++ fairseq/trainer.py
@@ -797,6 +797,7 @@ class Trainer(object):
         extra_kwargs["uses_branch_v2"] = self.cfg.model.branch_ctc_v2
         extra_kwargs["uses_branch_v3"] = self.cfg.model.branch_ctc_v3
         
+        print('fuck'*1000)
         # forward and backward pass
         logging_outputs, sample_size, ooms = [], 0, 0
         for i, sample in enumerate(samples):  # delayed update loop

ed9a6c424f8b06727f6280cacd982ef8150f98d4
dohe0342 <kimdohe1070@gmail.com>
2022-04-02 14:10:25 +0900

    from local

--- fairseq/models/wav2vec/wav2vec2_asr.py
+++ fairseq/models/wav2vec/wav2vec2_asr.py
@@ -254,7 +254,7 @@ class Wav2VecCtc(BaseFairseqModel):
         elif cfg.del_spk_info:
             w2v_encoder = Wav2VecEncoderSpkClf(cfg, len(task.target_dictionary), 251)
         elif cfg.viewmaker:
-            w2v_encoder = Wav2VecEncoderSpkClf(cfg, len(task.target_dictionary))
+            w2v_encoder = Wav2VecEncoderViewMaker(cfg, len(task.target_dictionary))
 
         else:
             w2v_encoder = Wav2VecEncoder(cfg, len(task.target_dictionary))

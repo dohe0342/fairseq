55977d2ffceba37009265bdfd75555edd4da6aab
dohe0342 <kimdohe1070@gmail.com>
2022-04-02 14:02:14 +0900

    from local

--- fairseq/trainer.py
+++ fairseq/trainer.py
@@ -287,7 +287,7 @@ class Trainer(object):
             self._build_optimizer(pcgrad=self.cfg.model.branch_ctc_v3)  # this will initialize self._lr_scheduler
         return self._lr_scheduler
 
-    def _build_optimizer(self, pcgrad=False, name='zzzzzzz'):
+    def _build_optimizer(self, pcgrad=False, name='viewmaker'):
         params = list(
             filter(
                 lambda p: p.requires_grad,

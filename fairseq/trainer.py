0d673ca9b6dc90f82896b7d8b6fd4f5d1f4ad594
dohe0342 <kimdohe1070@gmail.com>
2022-04-02 14:20:13 +0900

    from local

--- fairseq/trainer.py
+++ fairseq/trainer.py
@@ -284,6 +284,7 @@ class Trainer(object):
     @property
     def lr_scheduler(self):
         if self._lr_scheduler is None:
+            print('fuck'*100)
             self._build_optimizer(pcgrad=self.cfg.model.branch_ctc_v3)  # this will initialize self._lr_scheduler
         return self._lr_scheduler
 

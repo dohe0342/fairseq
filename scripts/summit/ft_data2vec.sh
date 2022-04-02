f7c33a0800f93bf84b682f449b0e40278930a897
dohe0342 <kimdohe1070@gmail.com>
2022-04-02 12:47:31 +0900

    from local

--- scripts/summit/ft_data2vec.sh
+++ scripts/summit/ft_data2vec.sh
@@ -1,7 +1,7 @@
 git pull
 fairseq-hydra-train \
 	--config-dir /workspace/fairseq/examples/wav2vec/config/finetuning \
-    --config-name base_100h \
+    --config-name base_100h_summit \
 	common.user_dir=examples/data2vec \
     task.data=/workspace/LibriSpeech/manifests \
     model.w2v_path=/workspace/models/data2vec_model/audio_base_ls.pt \

8de0ba3de70de7a785aa160ed627187cd1242c51
dohe0342 <kimdohe1070@gmail.com>
2022-04-02 13:34:13 +0900

    from local

--- scripts/summit/ft_data2vec_branch_ctc.sh
+++ scripts/summit/ft_data2vec_branch_ctc.sh
@@ -1,13 +1,15 @@
 git pull
 fairseq-hydra-train \
-	--config-dir /workspace/fairseq/examples/wav2vec/config/finetuning \
-    --config-name base_100h_summit \
-	common.user_dir=examples/data2vec \
-	checkpoint.best_checkpoint_metric=wer_12 \
+	--config-dir /home/work/workspace/fairseq/examples/wav2vec/config/finetuning \
+    --config-name base_100h_whale \
+	common.user_dir=/home/work/workspace/fairseq/examples/data2vec \
+	checkpoint.best_checkpoint_metric=wer \
+	checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/2022-03-30/07-39-06/checkpoints \
 	dataset.valid_subset=dev-other \
-	criterion._name=branch_ctc_v2 \
-    task.data=/workspace/LibriSpeech/manifests \
-    model.w2v_path=/workspace/models/data2vec_model/audio_base_ls.pt \
-	+model.branch_ctc_v2=true \
+	criterion._name=spk_clf \
+    task.data=/home/work/workspace/LibriSpeech/manifests \
+    model.w2v_path=/home/work/workspace/models/data2vec_model/audio_base_ls.pt \
+	+model.del_spk_info=true \
+	#+model.branch_ctc_v2=true \
 	#+model.overrides=uses_branch:true \
 	#task.overrides=uses_branch:true \

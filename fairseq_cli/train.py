78c3cd1539e6f3b6018c0db90f200ef353ab883d
dohe0342 <kimdohe1070@gmail.com>
2022-04-02 13:26:57 +0900

    from local

--- fairseq_cli/train.py
+++ fairseq_cli/train.py
@@ -94,10 +94,10 @@ def main(cfg: FairseqConfig) -> None:
     else:
         model = task.build_model(cfg.model)
     criterion = task.build_criterion(cfg.criterion)
-    logger.info(model)
-    logger.info("task: {}".format(task.__class__.__name__))
-    logger.info("model: {}".format(model.__class__.__name__))
-    logger.info("criterion: {}".format(criterion.__class__.__name__))
+    #logger.info(model)
+    #logger.info("task: {}".format(task.__class__.__name__))
+    #logger.info("model: {}".format(model.__class__.__name__))
+    #logger.info("criterion: {}".format(criterion.__class__.__name__))
     logger.info(
         "num. shared model params: {:,} (num. trained: {:,})".format(
             sum(

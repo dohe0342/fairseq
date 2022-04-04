2076f4d0331bb13f4d7282f01a4cc630b0e5c8d3
dohe0342 <kimdohe1070@gmail.com>
2022-04-04 15:05:09 +0900

    from local

--- examples/data2vec/models/data2vec_audio.py
+++ examples/data2vec/models/data2vec_audio.py
@@ -367,8 +367,8 @@ class Data2VecAudioModel(BaseFairseqModel):
             features_newview = viewmaker(features_newview)
             features_newview = features_newview.transpose(1,2)
         
-        print(features_newview[0][0].unsqueeze().size())
-        print(torch.mm(features_newview[0][0].unsqueeze(), features[0][0].unsqueeze().T))
+        print(features_newview[0][0].unsqueeze(dim=0).size())
+        print(torch.mm(features_newview[0][0].unsqueeze(dim=0), features[0][0].unsqueeze(dim=0).T))
             
         if self.post_extract_proj is not None:
             features = self.post_extract_proj(features)

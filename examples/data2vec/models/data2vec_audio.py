f13dba55f3538be55501b40c9101e9a09e1db431
dohe0342 <kimdohe1070@gmail.com>
2022-04-04 14:40:05 +0900

    from local

--- examples/data2vec/models/data2vec_audio.py
+++ examples/data2vec/models/data2vec_audio.py
@@ -365,6 +365,7 @@ class Data2VecAudioModel(BaseFairseqModel):
         if viewmaker is not None:
             print(features.dtype)
             features_newview = features.transpose(1,2)
+            print(features_newview.dtype)
             features_newview = viewmaker(features_newview)
             print(features_newview.size())
 

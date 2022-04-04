64eeec58b501f452994798a1ef8f479327bd35fa
dohe0342 <kimdohe1070@gmail.com>
2022-04-04 14:30:58 +0900

    from local

--- scripts/summit/ft_data2vec.sh
+++ scripts/summit/ft_data2vec.sh
@@ -5,3 +5,4 @@ fairseq-hydra-train \
 	common.user_dir=examples/data2vec \
     task.data=/workspace/LibriSpeech/manifests \
     model.w2v_path=/workspace/models/data2vec_model/audio_base_ls.pt \
+	+model.viewmaker=true

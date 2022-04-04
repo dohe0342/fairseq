193ec6836a08f212cce77c4ea2c5186a01c9783d
dohe0342 <kimdohe1070@gmail.com>
2022-04-04 20:03:09 +0900

    from local

--- examples/data2vec/models/data2vec_audio.py
+++ examples/data2vec/models/data2vec_audio.py
@@ -366,8 +366,9 @@ class Data2VecAudioModel(BaseFairseqModel):
             features_newview = features.transpose(1,2)
             features_newview = viewmaker(features_newview)
             features_newview = features_newview.transpose(1,2)
-
-        loss = torch.sqrt(torch.square(features_newview - features.transpose(1,2)).sum())
+        
+        loss = torch.sqrt(torch.square(features_newview - features).sum())
+        features = features_newview
         #print(torch.mm(features_newview[0][30].unsqueeze(dim=0), features[0][30].unsqueeze(dim=0).T))
             
         if self.post_extract_proj is not None:

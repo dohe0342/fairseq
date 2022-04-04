e77f5934c4c6b67a4e2b629720b7a308cba67567
dohe0342 <kimdohe1070@gmail.com>
2022-04-04 20:12:28 +0900

    from local

--- examples/data2vec/models/data2vec_audio.py
+++ examples/data2vec/models/data2vec_audio.py
@@ -368,7 +368,6 @@ class Data2VecAudioModel(BaseFairseqModel):
             features_newview = features_newview.transpose(1,2)
         
         loss = torch.sqrt(torch.square(features_newview - features).sum())
-        print(loss)
         features = features_newview
         #print(torch.mm(features_newview[0][30].unsqueeze(dim=0), features[0][30].unsqueeze(dim=0).T))
             

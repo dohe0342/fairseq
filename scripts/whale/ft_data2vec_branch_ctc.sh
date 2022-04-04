4a8858b2c49dfd7594e8be0774ec634bdfb0c506
dohe0342 <kimdohe1070@gmail.com>
2022-04-04 10:44:54 +0900

    from local

--- scripts/whale/ft_data2vec_branch_ctc.sh
+++ scripts/whale/ft_data2vec_branch_ctc.sh
@@ -4,12 +4,12 @@ fairseq-hydra-train \
     --config-name base_100h_whale \
 	common.user_dir=/home/work/workspace/fairseq/examples/data2vec \
 	checkpoint.best_checkpoint_metric=wer \
-	checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/2022-03-30/07-39-06/checkpoints \
 	dataset.valid_subset=dev-other \
 	criterion._name=spk_clf \
     task.data=/home/work/workspace/LibriSpeech/manifests \
     model.w2v_path=/home/work/workspace/models/data2vec_model/audio_base_ls.pt \
 	+model.del_spk_info=true \
+	#checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/2022-03-30/07-39-06/checkpoints \
 	#+model.branch_ctc_v2=true \
 	#+model.overrides=uses_branch:true \
 	#task.overrides=uses_branch:true \

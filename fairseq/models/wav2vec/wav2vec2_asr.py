7cea96892057b62edd6186fc2f3a4bc2f706db5a
dohe0342 <kimdohe1070@gmail.com>
2022-04-05 14:30:30 +0900

    from local

--- fairseq/models/wav2vec/wav2vec2_asr.py
+++ fairseq/models/wav2vec/wav2vec2_asr.py
@@ -1505,7 +1505,7 @@ class ViewMaker(BaseFairseqModel):
         for i in range(result.size()[0]):
             for j in range(i):
                 #print(torch.square(result[i]-x[i]).sum())
-                print(torch.mm(result[i], x[i].T).size())
+                print(torch.mm(result[i].T, x[i]).size())
                 #print(result[i].size())
 
         if self.clamp:

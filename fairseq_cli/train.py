3964e8110544c12db67c0adb894e93481d3062cb
dohe0342 <kimdohe1070@gmail.com>
2022-04-06 14:35:39 +0900

    from local

--- fairseq_cli/train.py
+++ fairseq_cli/train.py
@@ -315,9 +315,9 @@ def train(
                 # the end-of-epoch stats will still be preserved
                 metrics.reset_meters("train_inner")
         end_of_epoch = not itr.has_next()
-        #valid_losses, should_stop = validate_and_save(
-        #    cfg, trainer, task, epoch_itr, valid_subsets, end_of_epoch
-        #)
+        valid_losses, should_stop = validate_and_save(
+            cfg, trainer, task, epoch_itr, valid_subsets, end_of_epoch
+        )
 
         if should_stop:
             break

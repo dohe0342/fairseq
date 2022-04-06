4608d5fce3fbd3ff168d7d22fa13469f3aef8ef2
dohe0342 <kimdohe1070@gmail.com>
2022-04-06 15:34:03 +0900

    from local

--- fairseq/models/viewmaker.py
+++ fairseq/models/viewmaker.py
@@ -436,11 +436,9 @@ class Viewmaker3(torch.nn.Module):
 # ---
 
 class FCLayer(torch.nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride):
-        super(ConvLayer, self).__init__()
-        reflection_padding = kernel_size // 2
-        self.reflection_pad = torch.nn.ReflectionPad2d(reflection_padding)
-        self.conv2d = torch.nn.Conv2d(
+    def __init__(self, in_channels, out_channels):
+        super(FCLayer, self).__init__()
+        self.conv2d = torch.nn.linear(
             in_channels, out_channels, kernel_size, stride)
 
     def forward(self, x):

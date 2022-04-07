36fcbabf0329f3dced397d0b456a5f525406e949
dohe0342 <kimdohe1070@gmail.com>
2022-04-07 16:32:19 +0900

    from local

--- fairseq/tasks/fairseq_task.py
+++ fairseq/tasks/fairseq_task.py
@@ -571,7 +571,7 @@ class FairseqTask(object):
                     optimizer[0].backward(loss[0], retain_graph=True)
                     if loss[1].data > 15.:
                         optimizer[1].backward(loss[1])
-                    elif loss[1].data != 0. and loss[1].data <= 15.:
+                    elif loss[1].data != 0. and loss[1].data <= 10.:
                         #print('start view maker')
                         optimizer[1].backward(-0.001*loss[0] + loss[1])
                         #optimizer[1].backward(loss[1])

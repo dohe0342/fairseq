5c16f48e51f2084f0f7549b22c69d494e2300702
dohe0342 <kimdohe1070@gmail.com>
2022-04-08 14:56:53 +0900

    from local

--- examples/data2vec/models/data2vec_audio.py
+++ examples/data2vec/models/data2vec_audio.py
@@ -427,6 +427,7 @@ class Data2VecAudioModel(BaseFairseqModel):
                 "layer_results": layer_results,
                 "dropped_layer": dropped_layer,
                 "loss": loss,
+                "conv": conv_features,
             }
 
         result = {

b436b2706e0b847a1a9141a5503dc7d75fa50209
dohe0342 <kimdohe1070@gmail.com>
2022-04-14 11:13:07 +0900

    from local

--- fairseq/models/wav2vec/wav2vec2_asr.py
+++ fairseq/models/wav2vec/wav2vec2_asr.py
@@ -1445,9 +1445,6 @@ class ViewMaker(BaseFairseqModel):
         return torch.cat((x, noise), dim=1)
 
     def basic_net(self, y, num_res_blocks=3, bound_multiplier=1):
-        if num_res_blocks not in list(range(6)):
-            raise ValueError(f'num_res_blocks must be in {list(range(6))}, got {num_res_blocks}.')
-        
         y = self.add_noise_channel(y, num=self.num_noise, bound_multiplier=bound_multiplier)
         y = self.act(self.in1(self.conv1(y)))
         y = self.act(self.in2(self.conv2(y, pad=True)))

1b96d2af05dd3ba91b48f0a017791b67c52471b8
dohe0342 <kimdohe1070@gmail.com>
2022-04-14 14:03:54 +0900

    from local

--- fairseq/models/viewmaker.py
+++ fairseq/models/viewmaker.py
@@ -569,6 +569,7 @@ class UpsampleConvLayer(torch.nn.Module):
 if __name__ == '__main__':
     #summary(Viewmaker2().to('cuda'), torch.zeros((5,512,200)).to('cuda'))
     viewmaker = Viewmaker2().to('cuda')
+    optim = torch.optim.Adam(viewmaker.params())
 
     input_list = glob.glob('/home/work/workspace/fairseq/scripts/whale/conv_feat/*.npy')
     for input in input_list:

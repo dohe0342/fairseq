c6e89ce2cb2790aba203a0c2ddc0d1cb324745a0
dohe0342 <kimdohe1070@gmail.com>
2022-04-14 14:42:49 +0900

    from local

--- fairseq/models/viewmaker.py
+++ fairseq/models/viewmaker.py
@@ -590,4 +590,6 @@ if __name__ == '__main__':
                 input_norm = input[num] / input[num].norm()
                 output_norm = output[num] / output[num].norm()
                 sim = torch.abs(torch.mm(input_norm.unsqueeze(0), output_norm.unsqueeze(0).T)[0][0])
-            print(loss.data)
+                sim_avg += float(sim)
+            sim_avg /= (input.size()[0]/3)
+            print(sim_avg,loss.data)

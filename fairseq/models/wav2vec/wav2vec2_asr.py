ed3982885227c606fb001e33cc3dd70165d3de60
dohe0342 <kimdohe1070@gmail.com>
2022-04-15 14:38:37 +0900

    from local

--- fairseq/models/wav2vec/wav2vec2_asr.py
+++ fairseq/models/wav2vec/wav2vec2_asr.py
@@ -1370,7 +1370,7 @@ ACTIVATIONS = {
 class ViewMaker1(BaseFairseqModel):
     '''Viewmaker network that stochastically maps a multichannel 2D input to an output of the same size.'''
     def __init__(self, num_channels=512, distortion_budget=0.05, activation='gelu',
-                clamp=True, frequency_domain=False, downsample_to=False, num_res_blocks=5, num_noise=1):
+                clamp=True, frequency_domain=False, downsample_to=False, num_res_blocks=5, num_noise=10):
         '''Initialize the Viewmaker network.
 
         Args:

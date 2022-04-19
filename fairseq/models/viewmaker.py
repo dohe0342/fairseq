3f038ded4a1d3583a78f5817c346e21d1b0d7319
dohe0342 <kimdohe1070@gmail.com>
2022-04-19 16:59:20 +0900

    from local

--- fairseq/models/viewmaker.py
+++ fairseq/models/viewmaker.py
@@ -613,5 +613,5 @@ if __name__ == '__main__':
             sim_avg /= (input.size()[0]/3)
             print(sim_avg,loss.data)
             '''
-            print(name, input.size(), float(sim_avg.data), float(sim_max20.data), float(sim_min20.data), float(loss.data))
+            print(name, seq_len, float(sim_avg.data), float(sim_max20.data), float(sim_min20.data), float(loss.data))
         print('')

1d5fbde26a2e126a976db3a0bcf93c1674d7319c
dohe0342 <kimdohe1070@gmail.com>
2022-04-20 13:44:57 +0900

    from local

--- fairseq/models/wav2vec/wav2vec2_asr.py
+++ fairseq/models/wav2vec/wav2vec2_asr.py
@@ -1358,6 +1358,7 @@ class Wav2VecEncoderViewMaker(Wav2VecEncoder):
                     
         return {
             "encoder_out": x,  # T x B x C
+            "encoder_out_new": x_new, 
             "padding_mask": padding_mask,  # B x T,
             "layer_results": res["layer_results"],
             "loss": res["loss"],

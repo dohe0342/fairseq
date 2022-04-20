ca77654e604116aa2edf2062fe78978d164e6a25
dohe0342 <kimdohe1070@gmail.com>
2022-04-20 14:40:44 +0900

    from local

--- fairseq_cli/train.py
+++ fairseq_cli/train.py
@@ -299,7 +299,6 @@ def train(
     num_updates = trainer.get_num_updates()
     logger.info("Start iterating over samples")
     for i, samples in enumerate(progress):
-        print(i)
         with metrics.aggregate("train_inner"), torch.autograd.profiler.record_function(
             "train_step-%d" % i
         ):

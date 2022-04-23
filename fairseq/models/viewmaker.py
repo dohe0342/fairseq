8de8b9e9ff4961b599e482ae07a15d90af39e94a
dohe0342 <kimdohe1070@gmail.com>
2022-04-23 13:23:28 +0900

    from local

--- fairseq/models/viewmaker.py
+++ fairseq/models/viewmaker.py
@@ -573,7 +573,6 @@ if __name__ == '__main__':
 
     input_list = glob.glob('/home/work/workspace/fairseq/scripts/whale/conv_feat/*.npy')
     
-    print('here!!!!!!'*100)
     for i in range(1, 3):
         sim_list = []
         for input in input_list:

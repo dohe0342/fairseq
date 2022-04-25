37710df51d2f6eea1461b9f77b9a086081b9a39d
dohe0342 <kimdohe1070@gmail.com>
2022-04-25 11:43:05 +0900

    from local

--- examples/data2vec/models/data2vec_audio.py
+++ examples/data2vec/models/data2vec_audio.py
@@ -367,6 +367,16 @@ class Data2VecAudioModel(BaseFairseqModel):
         if viewmaker is not None:
             criterion = nn.MSELoss(reduction='mean')
             features_newview, delta = viewmaker(conv_features, padding_mask)
+            
+			input, output = input.T, output.T
+            input /= input.norm(dim=0)
+            output /= output.norm(dim=0)
+
+            sim = torch.mm(input.T, output)
+            sim = sim.diagonal(0)
+            sim = torch.sort(sim)[0]
+            #sim = sim.sum() / sim.size()[0]
+            sim_avg = sim.sum() / sim.size()[0]
             loss = criterion(features_newview.reshape(-1, 512), features.reshape(-1, 512))
         
         if self.post_extract_proj is not None:

54d3448eb40c040308715ef5498a18097f899a84
dohe0342 <kimdohe1070@gmail.com>
2022-04-25 20:07:25 +0900

    from local

--- scripts/whale/ft_data2vec.sh
+++ scripts/whale/ft_data2vec.sh
@@ -7,7 +7,7 @@ fairseq-hydra-train \
 	task.normalize=true \
     model.w2v_path=/home/work/workspace/models/data2vec_model/audio_base_ls.pt \
 	criterion._name=viewmaker \
-	checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/2022-04-25/06-11-46/checkpoints/checkpoint_last.pt
+	checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/2022-04-25/06-11-46/checkpoints/checkpoint_last.pt \
 	+model.viewmaker=true \
 	#model.apply_mask=false \
     #model.w2v_path=/home/work/workspace/fairseq/scripts/whale/multirun/2022-03-04/18-15-58/0/checkpoints/checkpoint_last.pt\

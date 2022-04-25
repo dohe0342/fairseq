8ff73241fd198388278a64db1099afa32cb129cf
dohe0342 <kimdohe1070@gmail.com>
2022-04-25 11:11:45 +0900

    from local

--- fairseq/tasks/fairseq_task.py
+++ fairseq/tasks/fairseq_task.py
@@ -570,7 +570,7 @@ class FairseqTask(object):
                         loss[1] *= 0
                 with torch.autograd.profiler.record_function("backward"):
                     optimizer[0].backward(loss[0], retain_graph=True)
-                    optimizer[1].backward(-loss[0])
+                    optimizer[1].backward(-loss[0]+loss[1])
 
             '''
             model.train()

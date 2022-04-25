a9fd77c49539a3b4982068590a57a2dc6bccb994
dohe0342 <kimdohe1070@gmail.com>
2022-04-26 08:45:43 +0900

    from local

--- examples/data2vec/models/data2vec_audio.py
+++ examples/data2vec/models/data2vec_audio.py
@@ -433,7 +433,7 @@ class Data2VecAudioModel(BaseFairseqModel):
 
         if features_only:
             return {
-                "x": x_new,
+                "x": x,
                 "x_new": x_new,
                 "padding_mask": padding_mask,
                 "layer_results": layer_results,

28fcd6ecebd4c024e181d4c9b5f00140e43976c2
dohe0342 <kimdohe1070@gmail.com>
2022-04-25 14:27:30 +0900

    from local

--- fairseq_cli/train.py
+++ fairseq_cli/train.py
@@ -298,11 +298,13 @@ def train(
     should_stop = False
     num_updates = trainer.get_num_updates()
     logger.info("Start iterating over samples")
+
+    print(epoch_itr.epoch)
     for i, samples in enumerate(progress):
         with metrics.aggregate("train_inner"), torch.autograd.profiler.record_function(
             "train_step-%d" % i
         ):
-            log_output = trainer.train_step(samples)
+            log_output = trainer.train_step(samples, 1)
 
         if log_output is not None:  # not OOM, overflow, ...
             # log mid-epoch stats

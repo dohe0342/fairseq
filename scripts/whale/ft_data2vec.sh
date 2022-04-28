9685b94cd8d12203d6a5577d0a7b6c411ad4f3c7
dohe0342 <kimdohe1070@gmail.com>
2022-04-28 14:23:33 +0900

    from local

--- scripts/whale/ft_data2vec.sh
+++ scripts/whale/ft_data2vec.sh
@@ -1,4 +1,5 @@
 git pull
+for i in {0..9}; do
 fairseq-hydra-train \
 	--config-dir /home/work/workspace/fairseq/examples/wav2vec/config/finetuning \
     --config-name base_100h_whale \
@@ -13,3 +14,4 @@ fairseq-hydra-train \
     #model.w2v_path=/home/work/workspace/fairseq/scripts/whale/multirun/2022-03-04/18-15-58/0/checkpoints/checkpoint_last.pt\
     #model.w2v_path=/home/work/workspace/models/data2vec_model/audio_base_ls.pt \
 	#+model.init_transformer=true
+done

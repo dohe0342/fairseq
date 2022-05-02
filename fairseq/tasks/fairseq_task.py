23525cb752562d60a17714544a30dd0115443165
dohe0342 <kimdohe1070@gmail.com>
2022-05-02 13:11:45 +0900

    from local

--- fairseq/tasks/fairseq_task.py
+++ fairseq/tasks/fairseq_task.py
@@ -573,7 +573,7 @@ class FairseqTask(object):
                     #optimizer[0].backward(loss[0])
                     optimizer[0].backward(loss[0], retain_graph=True)
                     #optimizer[1].backward(loss[1])
-                    optimizer[1].backward(-0.0005*loss[0]+loss[1])
+                    optimizer[1].backward(-0.0001*loss[0]+loss[1])
 
             '''
             model.train()

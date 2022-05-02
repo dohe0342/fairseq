68c33158db452dcbfaa884762c59edcd44279995
dohe0342 <kimdohe1070@gmail.com>
2022-05-02 11:46:41 +0900

    from local

--- scripts/whale/ft_data2vec.sh
+++ scripts/whale/ft_data2vec.sh
@@ -9,10 +9,10 @@ fairseq-hydra-train \
     model.w2v_path=/home/work/workspace/models/data2vec_model/audio_base_ls.pt \
 	criterion._name=viewmaker \
 	checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/viewmaker_try6_0005 \
-	job-logging-cfg.console.file.filename='viewmaker_try6_0005.log'
 	+model.viewmaker=true \
 	#model.apply_mask=false \
     #model.w2v_path=/home/work/workspace/fairseq/scripts/whale/multirun/2022-03-04/18-15-58/0/checkpoints/checkpoint_last.pt\
     #model.w2v_path=/home/work/workspace/models/data2vec_model/audio_base_ls.pt \
 	#+model.init_transformer=true
+	#job-logging-cfg.console.file.filename='viewmaker_try6_0005.log'
 done

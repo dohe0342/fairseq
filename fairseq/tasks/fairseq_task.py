1c535a768780eb0013dba470c57464b188123249
dohe0342 <kimdohe1070@gmail.com>
2022-05-06 11:19:59 +0900

    from local

--- fairseq/tasks/fairseq_task.py
+++ fairseq/tasks/fairseq_task.py
@@ -579,6 +579,7 @@ class FairseqTask(object):
                         optimizer[0].backward(loss[0][0]+loss[0][1], retain_graph=True)
                         optimizer[1].backward(-0.0001*(loss[0][0]+loss[0][1])+loss[1])
                     '''
+                    print('sample size = ', sample_size)
                     optimizer[0].backward(loss[0][0]+loss[0][1], retain_graph=True)
                     optimizer[1].backward(-0.000001*(loss[0][0]+loss[0][1])+loss[1])
 

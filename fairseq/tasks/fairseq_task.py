bb17476b773184b11f1fc65cc624d2ac776d8370
dohe0342 <kimdohe1070@gmail.com>
2022-05-08 20:16:50 +0900

    from local

--- fairseq/tasks/fairseq_task.py
+++ fairseq/tasks/fairseq_task.py
@@ -581,9 +581,9 @@ class FairseqTask(object):
                     '''
                     loss[0][1] /= sample_size
                     optimizer[0].backward(loss[0][0]+loss[0][1], retain_graph=True)
-                    #optimizer[1].backward(-0.0001*(loss[0][0]+loss[0][1])+loss[1]) ## try12
-                    optimizer[1].backward(-0.00001*(loss[0][1])+loss[1]) ## try15
-                    optimizer[1].backward(-0.00005*(loss[0][1])+loss[1]) ## try15
+                    ##optimizer[1].backward(-0.0001*(loss[0][0]+loss[0][1])+loss[1]) ## try12
+                    ##optimizer[1].backward(-0.00001*(loss[0][1])+loss[1]) ## try15
+                    optimizer[1].backward(-0.00005*(loss[0][1])+loss[1]) ## try16
             '''
             model.train()
             model.set_num_updates(update_num)

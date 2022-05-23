594f6414d7c484d7a112ba874384c3693666457a
dohe0342 <kimdohe1070@gmail.com>
2022-05-23 11:36:59 +0900

    from local

--- scripts/whale/eval_multi_model.py
+++ scripts/whale/eval_multi_model.py
@@ -4,6 +4,6 @@ import os
 model_list = sorted(glob.glob('/home/work/workspace/exp/viewmaker_try23_labmda_cosine_annealing_progressive_linear_growing/model/*.pt'))
 print(model_list)
 
-for model in model_list:
+for model in model_list[::-1]:
     print(model)
     os.system(f"./eval_multimodel.sh {model} 0")

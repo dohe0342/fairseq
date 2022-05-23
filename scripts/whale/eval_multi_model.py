2dc748c7aeef76e9219404daeea5a751ab9a7482
dohe0342 <kimdohe1070@gmail.com>
2022-05-23 14:08:24 +0900

    from local

--- scripts/whale/eval_multi_model.py
+++ scripts/whale/eval_multi_model.py
@@ -3,7 +3,12 @@ import os
 
 model_list = sorted(glob.glob('/home/work/workspace/exp/viewmaker_try23_labmda_cosine_annealing_progressive_linear_growing/model/*.pt'))[2:]
 
-for model in model_list[::-1]:
+wer_list = []
+for enum, model in enumerate(model_list):
     wer = model.split('_')[-1].replace('.pt','')
-    print(wer)
+    wer_list.append((enum, float(wer)))
+    #print(wer)
     #os.system(f"./eval_multimodel.sh {model} 0")
+
+wer_list = sorted(wer_list, key=lambda x:x[1], reverse=True)
+print(wer_list)

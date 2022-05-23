6c8a996bb9e544f49482fc85e18856ca86ffd611
dohe0342 <kimdohe1070@gmail.com>
2022-05-24 01:22:33 +0900

    from local

--- examples/data2vec/models/data2vec_audio.py
+++ examples/data2vec/models/data2vec_audio.py
@@ -363,7 +363,7 @@ class Data2VecAudioModel(BaseFairseqModel):
         
         #features[:,:,:250] = 0.
         #features[0,:,400:] = 0.
-        features[:,:,:50] = 0.
+        #features[:,:,:50] = 0.
         #features[:,:,90:] = 0.
     
         loss = None

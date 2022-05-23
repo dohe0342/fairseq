beb95c4d75d51bab4653b601c0d78b7cb7ec497f
dohe0342 <kimdohe1070@gmail.com>
2022-05-23 16:37:29 +0900

    from local

--- examples/speech_recognition/new/infer.py
+++ examples/speech_recognition/new/infer.py
@@ -230,6 +230,7 @@ class InferenceProcessor:
             strict=False,
             num_shards=self.cfg.checkpoint.checkpoint_shard_count,
         )
+        '''
         with torch.no_grad():
             for name1, param1 in models[0].named_parameters():
                 for name2, param2 in models[1].named_parameters():
@@ -237,13 +238,7 @@ class InferenceProcessor:
                         param1.data = (param1.data+param2.data)/2.
                         #print(f'set {name1} to ensemble param')
                         break
-
-                    '''
-                    if 'k_proj.bias' in name or 'q_proj.bias' in name:
-                    #if 'w2v_model.encoder' in name and 'bias' in name:
-                        param.data = torch.nn.Parameter(torch.zeros(param.size()[0]).to('cuda'))
-                        print(f'set {name} to 0.')
-                    '''
+        '''
         
         for model in models:
             self.optimize_model(model)

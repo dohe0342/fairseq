67c9c366fbf46caae38e3f6adb82a16082d806e5
dohe0342 <kimdohe1070@gmail.com>
2022-05-23 11:32:24 +0900

    from local

--- scripts/whale/eval_multi_model.py
+++ scripts/whale/eval_multi_model.py
@@ -1,4 +1,7 @@
 import glob
 import os
 
-model_list = 
+model_list = sorted(glob.glob('/home/work/workspace/exp/viewmaker_try23_lambda_cosine_annealing_progressive_lienar_growing/model/*.pt'))
+
+for model in model_list:
+    os.system(f"./eval_multimodel.sh {model}")

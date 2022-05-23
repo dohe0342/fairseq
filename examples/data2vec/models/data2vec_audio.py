12527878f93337173de128b5b55282d047b2544f
dohe0342 <kimdohe1070@gmail.com>
2022-05-23 21:33:29 +0900

    from local

--- examples/data2vec/models/data2vec_audio.py
+++ examples/data2vec/models/data2vec_audio.py
@@ -362,7 +362,7 @@ class Data2VecAudioModel(BaseFairseqModel):
         conv_features = features.clone()
         
         print(features.size())
-        features[0,:,1] = 0.
+        features[0,:,2] = 0.
     
         loss = None
         features_newview = None

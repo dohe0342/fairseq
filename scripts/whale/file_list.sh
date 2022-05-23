9e671ff07a4f42a9f5400d51260ffe713e07e03c
dohe0342 <kimdohe1070@gmail.com>
2022-05-23 11:23:43 +0900

    from local

--- scripts/whale/file_list.sh
+++ scripts/whale/file_list.sh
@@ -1,2 +1,7 @@
-files=(/home/work/workspace/exp/viewmaker_try24_lambda_cosine_annealing_progressive_linear_growing/model/*)
-echo $files
+#files=(/home/work/workspace/exp/viewmaker_try24_lambda_cosine_annealing_progressive_linear_growing/model/*)
+#echo $files
+declare -a arrPics
+for file in *.jpg
+do
+    arrPics=("${Pics[@]}" "$file")
+done

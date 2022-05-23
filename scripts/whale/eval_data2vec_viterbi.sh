361f7b0a54fba214f4fecc17fb4280d267c8c049
dohe0342 <kimdohe1070@gmail.com>
2022-05-23 11:29:44 +0900

    from local

--- scripts/whale/eval_data2vec_viterbi.sh
+++ scripts/whale/eval_data2vec_viterbi.sh
@@ -15,8 +15,9 @@ do
 		decoding.type=viterbi \
 		decoding.unique_wer_file=False \
 		dataset.gen_subset=$subset \
-		common_eval.path=/home/work/workspace/models/data2vec_model/$model \
-		distributed_training.distributed_world_size=4
+		common_eval.path=/home/work/workspace/exp/viewmaker_try23_lambda_cosine_annealing_progressive_linear_growing/model/$model \
+		#common_eval.path=/home/work/workspace/models/data2vec_model/$model \
+		distributed_training.distributed_world_size=1
 	echo ""
 	echo ""
 done

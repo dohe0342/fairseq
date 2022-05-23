1c198ee6069c40d659e7d474b650fc153c005494
dohe0342 <kimdohe1070@gmail.com>
2022-05-23 14:04:18 +0900

    from local

--- scripts/whale/eval_multi_model.py
+++ scripts/whale/eval_multi_model.py
@@ -4,4 +4,6 @@ import os
 model_list = sorted(glob.glob('/home/work/workspace/exp/viewmaker_try23_labmda_cosine_annealing_progressive_linear_growing/model/*.pt'))
 
 for model in model_list[::-1]:
-    os.system(f"./eval_multimodel.sh {model} 0")
+    wer = model.split('_')[-1].replace('.pt','')
+    print(wer)
+    #os.system(f"./eval_multimodel.sh {model} 0")

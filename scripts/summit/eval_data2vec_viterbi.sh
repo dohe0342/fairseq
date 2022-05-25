1ee380381100425befa8ba784b820c4fa768f159
dohe0342 <kimdohe1070@gmail.com>
2022-05-25 16:10:21 +0900

    from local

--- scripts/summit/eval_data2vec_viterbi.sh
+++ scripts/summit/eval_data2vec_viterbi.sh
@@ -3,9 +3,9 @@ source ~/.bashrc
 model=$1
 
 #for subset in "test" "test2" #"dev-clean" "dev-other" "test-clean" "test-other"
-for subset in "dev-clean" "dev-other" "test-clean" "test-other"
+#for subset in "dev-clean" "dev-other" "test-clean" "test-other"
 #for subset in "train-960" #"dev-clean" "dev-other" "test-clean" "test-other"
-#for subset in "train-100" "train-360" "train-500" ##"dev-clean" "dev-other" "test-clean" "test-other"
+for subset in "train-100" "train-360" "train-500" ##"dev-clean" "dev-other" "test-clean" "test-other"
 do
 	echo "====================   $model // $subset   ===================="
 	CUDA_VISIBLE_DEVICES=$2 python /workspace/fairseq/examples/speech_recognition/new/infer.py \

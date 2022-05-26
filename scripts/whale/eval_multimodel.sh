742c772b4ddc5a5e5fa509e2c0832e4dadbe09a3
dohe0342 <kimdohe1070@gmail.com>
2022-05-26 10:54:34 +0900

    from local

--- scripts/whale/eval_multimodel.sh
+++ scripts/whale/eval_multimodel.sh
@@ -18,7 +18,7 @@ do
 		decoding.type=viterbi \
 		decoding.unique_wer_file=False \
 		dataset.gen_subset=$subset \
-		common_eval.path=$model
+		common_eval.path=$model \
 		distributed_training.distributed_world_size=1
 	echo ""
 	echo ""

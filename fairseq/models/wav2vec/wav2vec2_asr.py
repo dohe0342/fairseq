a1aaa1162a49f48775c79d7d8d029c4f2581430c
dohe0342 <kimdohe1070@gmail.com>
2022-05-26 13:57:54 +0900

    from local

--- fairseq/models/wav2vec/wav2vec2_asr.py
+++ fairseq/models/wav2vec/wav2vec2_asr.py
@@ -462,10 +462,10 @@ class Wav2VecEncoder(FairseqEncoder):
             with open_dict(w2v_args):
                 w2v_args.model.checkpoint_activations = cfg.checkpoint_activations
         
-        print(model)
         w2v_args.task.data = cfg.data
         task = tasks.setup_task(w2v_args.task)
         model = task.build_model(w2v_args.model, from_checkpoint=True)
+        print(model)
 
         model.remove_pretraining_modules()
 

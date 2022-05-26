c045615d717a978db8e6dc324fc10e61019e73b4
dohe0342 <kimdohe1070@gmail.com>
2022-05-26 10:53:56 +0900

    from local

--- scripts/whale/eval_multimodel.sh
+++ scripts/whale/eval_multimodel.sh
@@ -3,7 +3,7 @@ source ~/.bashrc
 #path="/home/work/workspace/exp/viewmaker_try23_labmda_cosine_annealing_progressive_linear_growing/model"
 path="/home/work/workspace/exp/viewmaker_try26_labmda_cosine_annealing_slow/model"
 #path="/home/work/workspace/exp/viewmaker_try21_labmda_cosine_annealing/model"
-model7=$1
+model=$1
 
 for subset in "dev-clean" "dev-other" "test-clean" "test-other"
 do

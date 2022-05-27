1e749994465f69fe2c3fbd7810e7b628aedd3fb9
dohe0342 <kimdohe1070@gmail.com>
2022-05-27 13:47:28 +0900

    from local

--- fairseq/trainer.py
+++ fairseq/trainer.py
@@ -802,10 +802,14 @@ class Trainer(object):
         if self.cfg.ema.store_ema and getattr(self.task, "uses_ema", False):
             extra_kwargs["ema_model"] = self.ema.get_model()
         
-        extra_kwargs["uses_branch_v1"] = self.cfg.model.branch_ctc_v1
-        extra_kwargs["uses_branch_v2"] = self.cfg.model.branch_ctc_v2
-        extra_kwargs["uses_branch_v3"] = self.cfg.model.branch_ctc_v3
+        #extra_kwargs["uses_branch_v1"] = self.cfg.model.branch_ctc_v1
+        #extra_kwargs["uses_branch_v2"] = self.cfg.model.branch_ctc_v2
+        #extra_kwargs["uses_branch_v3"] = self.cfg.model.branch_ctc_v3
         
+        extra_kwargs["uses_branch_v1"] = False 
+        extra_kwargs["uses_branch_v2"] = False 
+        extra_kwargs["uses_branch_v3"] = False 
+
         # forward and backward pass
         logging_outputs, sample_size, ooms = [], 0, 0
         for i, sample in enumerate(samples):  # delayed update loop

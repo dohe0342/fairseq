688cceffd3e25ee8ae2821736d1b30f164f13ee8
dohe0342 <kimdohe1070@gmail.com>
2022-05-27 10:36:57 +0900

    from local

--- fairseq/tasks/fairseq_task.py
+++ fairseq/tasks/fairseq_task.py
@@ -616,7 +616,7 @@ class FairseqTask(object):
                     #lambda_ = -0.00001*(update_num/24000.)*(1+torch.cos(torch.tensor(update_num)/200.)) ## try 23
                     #lambda_ = -0.00001*(math.exp(update_num/80000))*(1+torch.cos(torch.tensor(update_num)/200.)) ## try 24
                     #lambda_ = -0.00001*(1+torch.cos(torch.tensor(update_num)/200.)) ## try 25:viewmaker-large
-                    lambda_ = -0.00001*(1+torch.cos(torch.tensor(update_num)*math.pi/1810.)) ## try25 try 26
+                    lambda_ = -0.00001*(1+torch.cos(torch.tensor(update_num)*math.pi/1810.)) ## try25 try 26 try 27
                     optimizer[1].backward(lambda_*(loss[0][1])+loss[1])
 
             '''

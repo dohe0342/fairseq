b617a12271fbeafd94a1dec4ba4b65d8d47dd0f2
dohe0342 <kimdohe1070@gmail.com>
2022-05-31 01:15:41 +0900

    from local

--- fairseq/models/wav2vec/wav2vec2_asr.py
+++ fairseq/models/wav2vec/wav2vec2_asr.py
@@ -1323,7 +1323,7 @@ class Wav2VecEncoderViewMaker(Wav2VecEncoder):
         d = w2v_args.model.encoder_embed_dim
 
         self.w2v_model = model
-        self.viewmaker = ViewMaker3()
+        self.viewmaker = ViewMaker1()
 
         self.final_dropout = nn.Dropout(cfg.final_dropout)
         self.freeze_finetune_updates = cfg.freeze_finetune_updates

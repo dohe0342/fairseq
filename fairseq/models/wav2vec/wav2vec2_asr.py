4f7b3236e312af54f8aa701ea5f4f0318579d3cd
dohe0342 <kimdohe1070@gmail.com>
2022-06-02 14:57:35 +0900

    from local

--- fairseq/models/wav2vec/wav2vec2_asr.py
+++ fairseq/models/wav2vec/wav2vec2_asr.py
@@ -2044,7 +2044,7 @@ class ViewMaker4(BaseFairseqModel):
         self.in8 = torch.nn.InstanceNorm1d(self.num_channels, affine=True)
         
         self.conv9 = ConvLayer2(self.num_channels, self.num_channels, \
-                kernel_size=2, stride=1, groups=self.num_channels/4)
+                kernel_size=2, stride=1, groups=self.group_size)
         self.in9 = torch.nn.InstanceNorm1d(self.num_channels, affine=True)
         
         self.conv10 = ConvLayer2(self.num_channels, self.num_channels, \

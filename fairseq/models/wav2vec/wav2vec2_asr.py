b326f6439f93f36080f1322aa4163e4be74b28a9
dohe0342 <kimdohe1070@gmail.com>
2022-06-02 13:54:22 +0900

    from local

--- fairseq/models/wav2vec/wav2vec2_asr.py
+++ fairseq/models/wav2vec/wav2vec2_asr.py
@@ -2028,6 +2028,7 @@ class ViewMaker4(BaseFairseqModel):
         
         self.conv5 = ConvLayer2(self.num_channels, self.num_channels, \
                 kernel_size=2, stride=1, groups=self.num_channels)
+        self.in4 = torch.nn.InstanceNorm1d(self.num_channels, affine=True)
 
         # Residual layers have +N for added random channels
         if not self.num_noise:

ea889bac1c1bd8e9b95ec812ad826e54f7db6edb
dohe0342 <kimdohe1070@gmail.com>
2022-06-03 19:41:24 +0900

    from local

--- scripts/summit/eval_data2vec_viterbi.sh
+++ scripts/summit/eval_data2vec_viterbi.sh
@@ -3,25 +3,30 @@ source ~/.bashrc
 model=$1
 
 #for subset in "test" "test2" #"dev-clean" "dev-other" "test-clean" "test-other"
-for subset in "dev-clean" "dev-other" "test-clean" "test-other"
+#for subset in "dev-clean" "dev-other" "test-clean" "test-other"
 #for subset in "train-960" #"dev-clean" "dev-other" "test-clean" "test-other"
 #for subset in "train-100" "train-360" "train-500" ##"dev-clean" "dev-other" "test-clean" "test-other"
 #for subset in "it_was2"
+for i in {0..19}
 do
+	first=$i
+	last=$($($i+1)*5)
+	echo $first $last
+
+	#subset="test-clean_
 	echo "====================   $model // $subset   ===================="
 	CUDA_VISIBLE_DEVICES=$2 python /workspace/fairseq/examples/speech_recognition/new/infer.py \
 		--config-dir /workspace/fairseq/examples/speech_recognition/new/conf \
 		--config-name infer \
 		task=audio_finetuning \
-		task.data=/workspace/LibriSpeech/manifests \
+		task.data=/workspace/LibriSpeech/manifests/splited \
 		common.user_dir=examples/data2vec \
 		task.labels=ltr \
 		decoding.type=viterbi \
 		decoding.unique_wer_file=true \
 		dataset.gen_subset=$subset \
 		common_eval.quiet=true \
-		common_eval.path=/workspace/models/data2vec_model/$model \
+		common_eval.path=/workspace/models/wav2vec_model/$model \
 		distributed_training.distributed_world_size=1 
-	echo ""
 	echo ""
 done

bad91d4172568a7047915df50962a7ffa7ed1213
dohe0342 <kimdohe1070@gmail.com>
2022-06-05 16:35:32 +0900

    from local

--- scripts/summit/eval_data2vec_viterbi.sh
+++ scripts/summit/eval_data2vec_viterbi.sh
@@ -7,7 +7,7 @@ model_=$1
 #for subset in "train-960" #"dev-clean" "dev-other" "test-clean" "test-other"
 #for subset in "train-100" "train-360" "train-500" ##"dev-clean" "dev-other" "test-clean" "test-other"
 #for subset in "it_was2"
-for model in "wav2vec_big_100h.pt" "wav2vec_big_960h.pt" "wav2vec_vox_100h_new.pt" "wav2vec_vox_960h_new.pt"
+for model in "wav2vec_small_100h.pt" "wav2vec_small_960h.pt" "wav2vec_big_100h.pt" "wav2vec_big_960h.pt" "wav2vec_vox_100h_new.pt" "wav2vec_vox_960h_new.pt"
 #for model in "wav2vec_vox_100h_new.pt" "wav2vec_vox_960h_new.pt"
 do
 	for subset in "dev-clean" "dev-other" "test-clean" "test-other"

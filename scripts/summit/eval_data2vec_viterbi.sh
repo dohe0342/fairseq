9ff1992626e57af385c10f079e794a87fca4597f
dohe0342 <kimdohe1070@gmail.com>
2022-06-05 16:35:10 +0900

    from local

--- scripts/summit/eval_data2vec_viterbi.sh
+++ scripts/summit/eval_data2vec_viterbi.sh
@@ -10,12 +10,13 @@ model_=$1
 for model in "wav2vec_big_100h.pt" "wav2vec_big_960h.pt" "wav2vec_vox_100h_new.pt" "wav2vec_vox_960h_new.pt"
 #for model in "wav2vec_vox_100h_new.pt" "wav2vec_vox_960h_new.pt"
 do
-	for i in {0..17}
+	for subset in "dev-clean" "dev-other" "test-clean" "test-other"
+	#for i in {0..17}
 	do
-		let first=$i*5
-		let last=($i+1)*5
+		#let first=$i*5
+		#let last=($i+1)*5
 		
-		subset=test-clean_"$first"to"$last"
+		#subset=test-clean_"$first"to"$last"
 		echo "====================   $model // $subset   ===================="
 		CUDA_VISIBLE_DEVICES=$2 python /workspace/fairseq/examples/speech_recognition/new/infer.py \
 			--config-dir /workspace/fairseq/examples/speech_recognition/new/conf \

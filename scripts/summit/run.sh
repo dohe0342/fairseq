0e5de441dff34cd10191f1d12bc74d12027ac385
dohe0342 <kimdohe1070@gmail.com>
2022-06-08 19:53:26 +0900

    from local

---
+++ scripts/summit/run.sh
@@ -0,0 +1,7 @@
+for model in "wav2vec_small_100h.pt" "wav2vec_small_960h.pt" "wav2vec_big_100h.pt" "wav2vec_    big_960h.pt" "wav2vec_vox_100h_new.pt" "wav2vec_vox_960h_new.pt"
+do
+	./eval_data2vec_viterbi.sh $model 0
+	mkdir ./None/$model
+	mv ./None/test-* ./None/$model/
+	python wrong_word_sort.py $model > $model.txt
+done

0b1d7ac9ace91bd6392b003f68866c8207b2d2ef
dohe0342 <kimdohe1070@gmail.com>
2022-06-08 19:06:17 +0900

    from local

--- scripts/summit/wrong_word_sort.py
+++ scripts/summit/wrong_word_sort.py
@@ -25,9 +25,9 @@ for hypo, ref in zip(hypo_list, ref_list):
     d = editDistance(hypo, ref)
     aligned = getStepList(hypo, ref, d)
     aligned_list.append(aligned)
-    #try: alignedPrint(aligned, ref, hypo)
-    #except: count += 1
-    alignedPrint(aligned, hypo, ref)
+    try: alignedPrint(aligned, ref, hypo)
+    except: print(hypo, ref)
+    #alignedPrint(aligned, hypo, ref)
 
     print('\n')
 

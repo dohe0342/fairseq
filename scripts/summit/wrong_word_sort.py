933af09bc0743c2a121a9a26d4f298d801611525
dohe0342 <kimdohe1070@gmail.com>
2022-06-08 15:31:31 +0900

    from local

--- scripts/summit/wrong_word_sort.py
+++ scripts/summit/wrong_word_sort.py
@@ -23,7 +23,7 @@ for hypo, ref in zip(hypo_list, ref_list):
     
     d = editDistance(hypo, ref)
     aligned_list.append(getStepList(hypo, ref, d))
-    if len(aligned_list[-1]) != len(hypo):
+    if len(aligned_list[-1]) == len(hypo):
         print('alarm!!!!')
     
     '''

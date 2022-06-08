ae085337e0e08445c9872bac061ef6d4468dfd00
dohe0342 <kimdohe1070@gmail.com>
2022-06-08 14:54:13 +0900

    from local

--- scripts/summit/wrong_word_sort.py
+++ scripts/summit/wrong_word_sort.py
@@ -16,7 +16,7 @@ for hypo, ref in zip(hypo_list, ref_list):
     #print(hypo)
     #print(ref)
     #print('')
-    if len(hypo) != len(ref):
+    if len(hypo.split(' ')) != len(ref.split(' ')):
         print(hypo)
         print(ref)
         print('')

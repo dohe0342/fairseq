0d749f0e8044408a53875418aef2466ee6f15559
dohe0342 <kimdohe1070@gmail.com>
2022-06-08 14:48:57 +0900

    from local

--- scripts/summit/wrong_word_sort.py
+++ scripts/summit/wrong_word_sort.py
@@ -8,7 +8,7 @@ ref_list = []
 
 for line in infer_log:
     if 'HYPO' in line:
-        hypo_list.append(line[44:].replace('\n', ''))
+        hypo_list.append(line[50:].replace('\n', ''))
     if 'REF' in line:
         ref_list.append(line[49:].replace('\n', ''))
 

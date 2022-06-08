3912906c23c0e87d533b145ac21888932199c47f
dohe0342 <kimdohe1070@gmail.com>
2022-06-08 20:06:35 +0900

    from local

--- scripts/summit/run.sh
+++ scripts/summit/run.sh
@@ -1,5 +1,5 @@
 #for model in "wav2vec_small_100h.pt" "wav2vec_small_960h.pt" "wav2vec_big_100h.pt" "wav2vec_big_960h.pt" "wav2vec_vox_100h_new.pt" "wav2vec_vox_960h_new.pt"
-for model in "wav2vec_big_960.pt"
+for model in "wav2vec_big_960h.pt"
 do
 	./eval_data2vec_viterbi.sh $model 0
 	mkdir ./None/$model

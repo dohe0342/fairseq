6b92fa9f5f286b2431db7de6b4db770092858e43
dohe0342 <kimdohe1070@gmail.com>
2022-06-08 20:20:45 +0900

    from local

--- scripts/summit/run.sh
+++ scripts/summit/run.sh
@@ -1,8 +1,8 @@
-#for model in "wav2vec_small_100h.pt" "wav2vec_small_960h.pt" "wav2vec_big_100h.pt" "wav2vec_big_960h.pt" "wav2vec_vox_100h_new.pt" "wav2vec_vox_960h_new.pt"
-for model in "wav2vec_big_960h.pt"
+for model in "wav2vec_small_100h.pt" "wav2vec_small_960h.pt" "wav2vec_big_100h.pt" "wav2vec_big_960h.pt" "wav2vec_vox_100h_new.pt" "wav2vec_vox_960h_new.pt"
+#for model in "wav2vec_big_960h.pt"
 do
 	./eval_data2vec_viterbi.sh $model 0
 	mkdir ./None/$model
 	mv ./None/test-* ./None/$model/
-	python wrong_word_sort.py $model > $model.txt
+	python wrong_word_sort.py $model > "$model"_to.txt
 done

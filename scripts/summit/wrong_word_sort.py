df5dafa13f1d66bf3abcf552affa4449fa357a5e
dohe0342 <kimdohe1070@gmail.com>
2022-06-08 18:57:33 +0900

    from local

--- scripts/summit/wrong_word_sort.py
+++ scripts/summit/wrong_word_sort.py
@@ -25,6 +25,7 @@ for hypo, ref in zip(hypo_list, ref_list):
     d = editDistance(hypo, ref)
     aligned = getStepList(hypo, ref, d)
     aligned_list.append(aligned)
+    alignedPrint(aligned, ref, hypo)
 
 
     

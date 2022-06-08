8cd24b32e0b08cb69e7f2b4510d87a1046afc2f2
dohe0342 <kimdohe1070@gmail.com>
2022-06-08 14:21:19 +0900

    from local

--- scripts/summit/eval_data2vec_viterbi.sh
+++ scripts/summit/eval_data2vec_viterbi.sh
@@ -29,7 +29,7 @@ do
 			decoding.type=viterbi \
 			decoding.unique_wer_file=true \
 			dataset.gen_subset=$subset \
-			common_eval.quiet=true \
+			common_eval.quiet=false \
 			common_eval.path=/workspace/models/wav2vec_model/$model \
 			distributed_training.distributed_world_size=1 
 		echo ""

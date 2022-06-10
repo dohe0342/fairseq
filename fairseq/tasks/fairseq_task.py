1ea8000230f19fc15e7316706bd1ef41b62d0fee
dohe0342 <kimdohe1070@gmail.com>
2022-06-10 14:11:21 +0900

    from local

--- fairseq/tasks/fairseq_task.py
+++ fairseq/tasks/fairseq_task.py
@@ -577,7 +577,6 @@ class FairseqTask(object):
             return loss, sample_size, logging_output
 
         else:
-            '''
             model.train()
             model.set_num_updates(update_num)
             with torch.autograd.profiler.record_function("forward"):
@@ -621,6 +620,8 @@ class FairseqTask(object):
                 loss *= 0
             with torch.autograd.profiler.record_function("backward"):
                 optimizer.backward(loss)
+            '''
+        
         return loss, sample_size, logging_output
 
     def valid_step(self, sample, model, criterion, uses_branch_v1=False, uses_branch_v2=False, uses_branch_v3=False):

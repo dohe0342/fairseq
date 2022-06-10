fd4fafc99a5b8ccfbe73378fccf400b259106405
dohe0342 <kimdohe1070@gmail.com>
2022-06-11 01:08:28 +0900

    from local

--- scripts/summit/ft_data2vec_text.sh
+++ scripts/summit/ft_data2vec_text.sh
@@ -1,12 +1,12 @@
 git pull
 fairseq-hydra-train \
 	--config-dir /workspace/fairseq/examples/roberta/config/finetuning \
-	--config-name rte \
-	task.data=/workspace/fairseq/examples/roberta/RTE-bin \
+	--config-name mnli \
+	task.data=/workspace/fairseq/examples/roberta/MNLI-bin \
 	checkpoint.restore_file=/workspace/models/data2vec_model/nlp_base.pt \
 	criterion._name=sentence_prediction_viewmaker \
-	optimization.max_update=6108 \
-	optimization.max_epoch=30 \
+	optimization.max_update=100000 \
+	optimization.max_epoch=100 \
 	dataset.batch_size=8
 	#+model.branch_ctc_v1=false \
 	#+model.branch_ctc_v2=false \

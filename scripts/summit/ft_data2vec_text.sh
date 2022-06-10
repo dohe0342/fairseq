ae2a9a50cc568c06c519863b8091c252c8ff43c0
dohe0342 <kimdohe1070@gmail.com>
2022-06-10 11:26:18 +0900

    from local

--- scripts/summit/ft_data2vec_text.sh
+++ scripts/summit/ft_data2vec_text.sh
@@ -2,7 +2,7 @@ fairseq-hydra-train \
 	--config-dir /workspace/fairseq/examples/roberta/config/finetuning \
 	--config-name rte \
 	task.data=/workspace/fairseq/examples/roberta/RTE-bin \
-	checkpoint.restore_file=/workspace/models/data2vec_model/nlp_base.pt \
+	checkpoint.restore_file=/workspace/models/data2vec_model/nlp_base.pt 
 	#+model.branch_ctc_v1=false \
 	#+model.branch_ctc_v2=false \
 	#+model.branch_ctc_v3=false 

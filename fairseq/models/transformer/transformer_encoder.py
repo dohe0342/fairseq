1e661d0905423af208f8e373dda2c9e758a04bae
dohe0342 <kimdohe1070@gmail.com>
2022-06-11 04:33:33 +0900

    from local

--- fairseq/models/transformer/transformer_encoder.py
+++ fairseq/models/transformer/transformer_encoder.py
@@ -207,8 +207,8 @@ class TransformerEncoderBase(FairseqEncoder):
         encoder_padding_mask = src_tokens.eq(self.padding_idx)
         has_pads = src_tokens.device.type == "xla" or encoder_padding_mask.any()
         
-        with torch.no_grad():
-            x, encoder_embedding = self.forward_embedding(src_tokens, token_embeddings)
+        #with torch.no_grad():
+        x, encoder_embedding = self.forward_embedding(src_tokens, token_embeddings)
 
         # account for padding while computing the representation
         if has_pads:

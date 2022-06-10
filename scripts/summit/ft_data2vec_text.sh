d03561d644f7f33e9954c58cc6610b85034542e9
dohe0342 <kimdohe1070@gmail.com>
2022-06-11 02:18:31 +0900

    from local

--- scripts/summit/ft_data2vec_text.sh
+++ scripts/summit/ft_data2vec_text.sh
@@ -7,7 +7,7 @@ fairseq-hydra-train \
 	criterion._name=sentence_prediction_viewmaker \
 	optimization.max_update=6108 \
 	optimization.max_epoch=30 \
-	dataset.batch_size=8
+	dataset.batch_size=16
 	#+model.branch_ctc_v1=false \
 	#+model.branch_ctc_v2=false \
 	#+model.branch_ctc_v3=false 

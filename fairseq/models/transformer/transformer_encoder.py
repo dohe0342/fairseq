79f9971a546524d1adec99b134a1ab40975493d3
dohe0342 <kimdohe1070@gmail.com>
2022-06-11 02:07:49 +0900

    from local

--- fairseq/models/transformer/transformer_encoder.py
+++ fairseq/models/transformer/transformer_encoder.py
@@ -218,10 +218,10 @@ class TransformerEncoderBase(FairseqEncoder):
             loss = 0
             criterion = torch.nn.MSELoss(reduction='mean')
             x_newview, delta = viewmaker(x, None)
-            print(x_newview)
             if has_pads:
                 x_newview = x_newview * (1 - encoder_padding_mask.unsqueeze(-1).type_as(x_newview))
-   
+            
+            print(x_newview)
             x_ori = x.clone().reshape(-1, 768).detach()
 
             loss = criterion(x_newview.reshape(-1, 768), x_ori.reshape(-1, 768))

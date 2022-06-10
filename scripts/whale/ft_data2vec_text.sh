b3b2cb98d15c99dbdaa582e2cbf649afe659c16b
dohe0342 <kimdohe1070@gmail.com>
2022-06-11 04:08:49 +0900

    from local

--- scripts/whale/ft_data2vec_text.sh
+++ scripts/whale/ft_data2vec_text.sh
@@ -4,9 +4,9 @@ fairseq-hydra-train \
     --config-name rte \
     task.data=/home/work/workspace/fairseq/examples/roberta/RTE-bin \
     checkpoint.restore_file=/home/work/workspace/models/data2vec_model/nlp_base.pt \
-    criterion._name=sentence_prediction_viewmaker 
-    #optimization.max_update=6108 \
-    #optimization.max_epoch=30 \
+    criterion._name=sentence_prediction_viewmaker \
+    optimization.max_update=6108 \
+    optimization.max_epoch=30 
     #dataset.batch_size=64
     #+model.branch_ctc_v1=false \
     #+model.branch_ctc_v2=false \

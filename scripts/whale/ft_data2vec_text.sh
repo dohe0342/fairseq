bc9a85883e1eb7a7f2f0eed8471073fffd144915
dohe0342 <kimdohe1070@gmail.com>
2022-06-11 03:38:48 +0900

    from local

--- scripts/whale/ft_data2vec_text.sh
+++ scripts/whale/ft_data2vec_text.sh
@@ -1,6 +1,6 @@
 git pull
 fairseq-hydra-train \
-    --config-dir /workspace/fairseq/examples/roberta/config/finetuning \
+    --config-dir /home/work/workspace/fairseq/examples/roberta/config/finetuning \
     --config-name sst_2 \
     task.data=/workspace/fairseq/examples/roberta/SST-2-bin \
     checkpoint.restore_file=/workspace/models/data2vec_model/nlp_base.pt \

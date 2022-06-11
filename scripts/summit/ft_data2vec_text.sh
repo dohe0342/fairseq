d9f1c1635fe2bc424d1fd0fc87cd415b928a27b5
dohe0342 <kimdohe1070@gmail.com>
2022-06-11 19:20:07 +0900

    from local

--- scripts/summit/ft_data2vec_text.sh
+++ scripts/summit/ft_data2vec_text.sh
@@ -1,7 +1,7 @@
 git pull
 fairseq-hydra-train \
 	--config-dir /workspace/fairseq/examples/roberta/config/finetuning \
-	--config-name sst_2 \
-	task.data=/workspace/fairseq/examples/roberta/SST-2-bin \
+	--config-name qqp \
+	task.data=/workspace/fairseq/examples/roberta/QQP-bin \
 	checkpoint.restore_file=/workspace/models/data2vec_model/nlp_base.pt \
 	criterion._name=sentence_prediction_viewmaker 

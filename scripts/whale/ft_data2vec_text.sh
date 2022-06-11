49ff5b065710ebcb6d9c5cdb28cb517768e28fcf
dohe0342 <kimdohe1070@gmail.com>
2022-06-11 18:31:13 +0900

    from local

--- scripts/whale/ft_data2vec_text.sh
+++ scripts/whale/ft_data2vec_text.sh
@@ -2,9 +2,12 @@ git pull
 task=$1
 TASK=${task^^}
 
-fairseq-hydra-train \
-    --config-dir /home/work/workspace/fairseq/examples/roberta/config/finetuning \
-    --config-name $task \
-    task.data=/home/work/workspace/fairseq/examples/roberta/$TASK-bin \
-    checkpoint.restore_file=/home/work/workspace/models/data2vec_model/nlp_base.pt \
-    criterion._name=sentence_prediction_viewmaker 
+for i in {0..3}
+do
+	fairseq-hydra-train \
+		--config-dir /home/work/workspace/fairseq/examples/roberta/config/finetuning \
+		--config-name $task \
+		task.data=/home/work/workspace/fairseq/examples/roberta/$TASK-bin \
+		checkpoint.restore_file=/home/work/workspace/models/data2vec_model/nlp_base.pt \
+		criterion._name=sentence_prediction_viewmaker 
+done

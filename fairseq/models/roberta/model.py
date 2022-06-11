e810451cf7c4bbfb2a7b5a37f98bb453e7084879
dohe0342 <kimdohe1070@gmail.com>
2022-06-11 14:36:08 +0900

    from local

--- fairseq/models/roberta/model.py
+++ fairseq/models/roberta/model.py
@@ -257,9 +257,11 @@ class RobertaModel(FairseqEncoderModel):
         x, extra = self.encoder(src_tokens, features_only, return_all_hiddens, **kwargs)
 
         if classification_head_name is not None:
-            x_ori = self.classification_heads[classification_head_name](x[0])
-            x_newview = self.classification_heads[classification_head_name](x[1])
-        return (x_ori, x_newview), extra
+            x = self.classification_heads[classification_head_name](x)
+            #x_ori = self.classification_heads[classification_head_name](x[0])
+            #x_newview = self.classification_heads[classification_head_name](x[1])
+        #return (x_ori, x_newview), extra
+        return x
 
     def _get_adaptive_head_loss(self):
         norm_loss = 0

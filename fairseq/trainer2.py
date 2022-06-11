0dfbea11f537feb9991d5ddc20b21b9ae139e192
dohe0342 <kimdohe1070@gmail.com>
2022-06-11 17:34:15 +0900

    from local

--- fairseq/trainer2.py
+++ fairseq/trainer2.py
@@ -1027,9 +1027,10 @@ class Trainer(object):
                             samples, raise_oom
                         )  # recursion to feed in same batch
                 
-                self.task.optimizer_step(
-                    self.optimizer2, model=self.model, update_num=self.get_num_updates()
-                )
+                if self.get_num_updates() % 156 == 0:
+                    self.task.optimizer_step(
+                        self.optimizer2, model=self.model, update_num=self.get_num_updates()
+                    )
                 if self.cfg.common.amp and overflow:
                     if self._amp_retries == self.cfg.common.amp_batch_retries:
                         logger.info("AMP: skipping this batch.")

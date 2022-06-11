b3e69d6405d19e875d4fc1bbc2ea253cc3381e76
dohe0342 <kimdohe1070@gmail.com>
2022-06-11 18:53:49 +0900

    from local

--- scripts/whale/ft_data2vec_text.sh
+++ scripts/whale/ft_data2vec_text.sh
@@ -17,9 +17,10 @@ do
 
 	CUDA_VISIBLE_DEVICES=$3 fairseq-hydra-train \
 		--config-dir /home/work/workspace/fairseq/examples/roberta/config/finetuning \
-		--config-name $task \
-		task.data=/home/work/workspace/fairseq/examples/roberta/$TASK-bin \
+		--config-name $task2 \
+		task.data=/home/work/workspace/fairseq/examples/roberta/$TASK2-bin \
 		checkpoint.restore_file=/home/work/workspace/models/data2vec_model/nlp_base.pt \
-		criterion._name=sentence_prediction_viewmaker 
+		criterion._name=sentence_prediction_viewmaker \
+		checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/"$task1"_try$i
 
 done

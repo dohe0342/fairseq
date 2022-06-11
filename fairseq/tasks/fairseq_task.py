a9c04a20018850e4e108038e50b85b8b4b81d620
dohe0342 <kimdohe1070@gmail.com>
2022-06-11 15:53:00 +0900

    from local

--- fairseq/tasks/fairseq_task.py
+++ fairseq/tasks/fairseq_task.py
@@ -598,8 +598,8 @@ class FairseqTask(object):
                 with torch.autograd.profiler.record_function("backward"):
                     #loss[0][1] /= sample_size
                     #optimizer[0].backward(0.9999*loss[0][0]+0.000001*loss[0][1], retain_graph=True)
-                    optimizer[0].backward(loss[0][0], retain_graph=True)
-                    #optimizer[1].backward(-0.0001*(loss[0][0]+loss[0][1])+loss[1]) ## try12
+                    #optimizer[0].backward(loss[0][0], retain_graph=True)
+                    optimizer[1].backward(-0.0001*(loss[0][0]+loss[0][1])+loss[1]) ## try12
                     ##optimizer[1].backward(-0.00001*(loss[0][1])+loss[1]) ## try15
                     ##optimizer[1].backward(-0.00005*(loss[0][1])+loss[1]) ## try16
                     ##optimizer[1].backward(loss[1]) ## try17, try18:noreduce
@@ -611,7 +611,7 @@ class FairseqTask(object):
                     #lambda_ = -0.00001*(1+torch.cos(torch.tensor(update_num)/200.)) ## try 25:viewmaker-large
                     #lambda_ = -0.00001*(1+torch.cos(torch.tensor(update_num)*math.pi/1810.)) ## try25 try 26 try 27
                     #optimizer[1].backward(lambda_*(loss[0][1])+loss[1])
-                    optimizer[1].backward(-loss[0][1]+loss[1])
+                    #optimizer[1].backward(-loss[0][1]+loss[1])
         else:
             model.train()
             model.set_num_updates(update_num)

88165fe508de0761651b08b91c412e75f08fe13c
dohe0342 <kimdohe1070@gmail.com>
2022-06-18 21:49:58 +0900

    from local

--- fairseq/models/wav2vec/wav2vec2_asr.py
+++ fairseq/models/wav2vec/wav2vec2_asr.py
@@ -555,7 +555,7 @@ class Wav2VecEncoder(FairseqEncoder):
             x = x.transpose(0, 1)
         
         x = self.final_dropout(x)
-        print('here')
+        print('here', x)
 
         if self.proj:
             x = self.proj(x)

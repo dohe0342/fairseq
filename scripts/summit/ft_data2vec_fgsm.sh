4c4aba01864f37f4da73bfa6d45673d2fc29807d
dohe0342 <kimdohe1070@gmail.com>
2022-06-18 21:22:50 +0900

    from local

---
+++ scripts/summit/ft_data2vec_fgsm.sh
@@ -0,0 +1,15 @@
+git pull
+
+exp_name=$1
+
+mkdir /home/work/workspace/fairseq/scripts/whale/outputs/$1
+fairseq-hydra-train \
+	--config-dir /home/work/workspace/fairseq/examples/wav2vec/config/finetuning \
+	--config-name base_100h_whale \
+	common.user_dir=examples/data2vec \
+	task.data=/home/work/workspace/LibriSpeech/manifests \
+	task.normalize=true \
+	model.w2v_path=/home/work/workspace/models/data2vec_model/audio_base_ls.pt \
+	criterion._name=ctc \
+	checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 
+	#+model.viewmaker=true \

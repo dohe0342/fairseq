42824e301869764098c0bdf3ad6b1e7205db43ed
dohe0342 <kimdohe1070@gmail.com>
2022-06-20 12:43:01 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -420,6 +420,7 @@ class CtcCriterion(FairseqCriterion):
 
         snr = 20*torch.log10(torch.norm(origin, dim=1)/torch.norm(sample["net_input"]["source"], dim=1))
         snr = snr.sum() / origin.size()[0]
+        print(snr)
         
         ntokens = (
             sample["ntokens"] if "ntokens" in sample else target_lengths.sum().item()

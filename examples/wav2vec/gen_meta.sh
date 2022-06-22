0a60526b41703638b7c435c625d995a8e13e22b4
dohe0342 <kimdohe1070@gmail.com>
2022-06-22 16:12:00 +0900

    from local

--- examples/wav2vec/gen_meta.sh
+++ examples/wav2vec/gen_meta.sh
@@ -5,4 +5,4 @@ echo "generate tsv file (from $dir to $name.tsv)"
 python wav2vec_manifest.py $dir --dest $dir/manifests --ext flac --output $name
 
 echo "generate ltr, wrd file (from $dir to $name)"
-python libri_labels.py $dir/manifests/$name.tsv --output-dir $dir --output-name $name
+python libri_labels.py $dir/manifests/$name.tsv --output-dir $dir/manifests --output-name $name

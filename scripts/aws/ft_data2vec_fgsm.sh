dd2563ab438ff85fc81699b292a89ade80a478b8
dohe0342 <kimdohe1070@gmail.com>
2022-06-22 16:42:24 +0900

    from local

--- scripts/aws/ft_data2vec_fgsm.sh
+++ scripts/aws/ft_data2vec_fgsm.sh
@@ -2,14 +2,15 @@ git pull
 
 exp_name=$1
 
-mkdir /home/work/workspace/fairseq/scripts/whale/outputs/$1
+#mkdir /home/work/workspace/fairseq/scripts/whale/outputs/$1
+mkdir /opt/ml/code/fairseq/scripts/aws/output/$1
 fairseq-hydra-train \
-	--config-dir /home/work/workspace/fairseq/examples/wav2vec/config/finetuning \
-	--config-name base_100h_summit \
+	--config-dir /opt/ml/code/fairseq/examples/wav2vec/config/finetuning \
+	--config-name base_100h_whale \
 	common.user_dir=examples/data2vec \
-	task.data=/home/work/workspace/LibriSpeech/manifests \
+	task.data=/opt/ml/input/data/train/LibriSpeech/manifests \
 	task.normalize=true \
-	model.w2v_path=/home/work/workspace/models/data2vec_model/audio_base_ls.pt \
-	criterion._name=ctc_fgsm \
-	checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 
+	model.w2v_path=/opt/ml/input/data/model/audio_base_ls.pt \
+	criterion._name=ctc_fgsm 
+	#checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 
 	#+model.viewmaker=true \

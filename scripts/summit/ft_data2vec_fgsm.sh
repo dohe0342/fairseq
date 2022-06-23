1b0a9e973e3c94eb2b37e3e5dbd46117890b1ec4
dohe0342 <kimdohe1070@gmail.com>
2022-06-23 16:51:19 +0900

    from local

--- scripts/summit/ft_data2vec_fgsm.sh
+++ scripts/summit/ft_data2vec_fgsm.sh
@@ -10,5 +10,5 @@ fairseq-hydra-train \
 	task.data=/workspace/LibriSpeech/manifests \
 	task.normalize=true \
 	model.w2v_path=/workspace/models/data2vec_model/audio_base_ls.pt \
-	criterion._name=ctc_fgsm \
+	criterion._name=ctc \
 	checkpoint.save_dir=/workspace/fairseq/scripts/whale/outputs/$1

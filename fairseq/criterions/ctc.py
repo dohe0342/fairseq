0a1119ff8bb2f14f1eade8cc5f85efb00d1234fd
dohe0342 <kimdohe1070@gmail.com>
2022-06-25 15:40:18 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -530,14 +530,6 @@ class CtcCriterion(FairseqCriterion):
         with torch.autograd.profiler.record_function("backward"):
             optimizer.backward(loss, retain_graph=True)
        
-        '''
-        for n, p in model.named_parameters():
-            if 'feature_extractor' in n:
-                print(p.grad)
-            else:
-                print('gradient = ', p.grad)
-        '''
-
         eps = 0.02
         sample["net_input"]["source"].grad.sign_()
 

78ee492a0886546b8585ae56229111a2641ff5b1
dohe0342 <kimdohe1070@gmail.com>
2022-06-25 20:10:39 +0900

    from local

--- scripts/aws/ft_data2vec.sh
+++ scripts/aws/ft_data2vec.sh
@@ -5,13 +5,8 @@ for i in {0..9} ; do
 		common.user_dir=examples/data2vec \
 		task.data=/opt/ml/code/LibriSpeech/manifests \
 		task.normalize=true \
-		model.w2v_path=/home/work/workspace/models/data2vec_model/audio_base_ls.pt \
+		model.w2v_path=/opt/ml/input/data/data/audio_base_ls.pt \
 		criterion._name=viewmaker \
-		checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 \
+		checkpoint.save_dir=/opt/ml/model \
 		+model.viewmaker=true \
-	#model.apply_mask=false \
-    #model.w2v_path=/home/work/workspace/fairseq/scripts/whale/multirun/2022-03-04/18-15-58/0/checkpoints/checkpoint_last.pt\
-    #model.w2v_path=/home/work/workspace/models/data2vec_model/audio_base_ls.pt \
-	#+model.init_transformer=true
-	#job-logging-cfg.console.file.filename='viewmaker_try6_0005.log'
 done

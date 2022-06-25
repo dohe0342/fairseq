dc2cf1dbc67fb42b7ad009ae4cd9ebe504f1e85a
dohe0342 <kimdohe1070@gmail.com>
2022-06-25 17:11:29 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -546,7 +546,6 @@ class CtcCriterion(FairseqCriterion):
 
         sim = torch.mm(origin, conv_feat_.T)
         sim = sim.diagonal().sum()/sim.size()[0]
-        print(sim)
         
         ntokens = (
             sample["ntokens"] if "ntokens" in sample else target_lengths.sum().item()

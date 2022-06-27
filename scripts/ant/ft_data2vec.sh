64f95fe378465b91c0ce5805c33bfb3ec4df821c
dohe0342 <kimdohe1070@gmail.com>
2022-06-27 15:25:11 +0900

    from local

--- scripts/ant/ft_data2vec.sh
+++ scripts/ant/ft_data2vec.sh
@@ -16,8 +16,8 @@ then
 		+model.viewmaker=true
 elif [ $mode == "hubert" ]
 then
-	#fairseq-hydra-train \
-	python /workspace/fairseq/fairseq_cli/hydra_train.py \
+	fairseq-hydra-train \
+	#python /workspace/fairseq/fairseq_cli/hydra_train.py \
 		--config-dir /workspace/fairseq/examples/wav2vec/config/finetuning \
 		--config-name base_100h_ant \
 		common.user_dir=examples/hubert \

8de9e90a0f9202fac84d3b7756220f2a4ce8c0e9
dohe0342 <kimdohe1070@gmail.com>
2022-06-27 21:30:25 +0900

    from local

--- scripts/aws/ft_data2vec_fgsm.sh
+++ scripts/aws/ft_data2vec_fgsm.sh
@@ -2,14 +2,26 @@
 
 #mkdir /home/work/workspace/fairseq/scripts/whale/outputs/$1
 #mkdir /opt/ml/code/fairseq/scripts/aws/output/$1
-fairseq-hydra-train \
-	--config-dir /opt/ml/code/fairseq/examples/wav2vec/config/finetuning \
-	--config-name base_100h_aws_p4 \
-	common.user_dir=examples/data2vec \
-	task.data=/opt/ml/code/LibriSpeech/manifests \
-	task.normalize=true \
-	model.w2v_path=/opt/ml/input/data/model/audio_base_ls.pt \
-	criterion._name=ctc \
-	checkpoint.save_dir=/opt/ml/model \
-	dataset.max_tokens=3200000 \
-	optimization.update_freq=[4]
+mode="w2v"
+
+if [ $mode == "w2v" ]
+then
+	fairseq-hydra-train \
+		--config-dir /opt/ml/code/fairseq/examples/wav2vec/config/finetuning \
+		--config-name base_100h_aws_p4 \
+		common.user_dir=examples/data2vec \
+		task.data=/opt/ml/code/LibriSpeech/manifests \
+		task.normalize=true \
+		model.w2v_path=/opt/ml/input/data/model/audio_base_ls.pt \
+		criterion._name=ctc \
+		checkpoint.save_dir=/opt/ml/model 
+else
+	fairseq-hydra-train \
+		--config-dir /opt/ml/code/fairseq/examples/wav2vec/config/finetuning \
+		--config-name base_100h_aws_p4 \
+		common.user_dir=examples/data2vec \
+		task.data=/opt/ml/code/LibriSpeech/manifests \
+		task.normalize=true \
+		model.w2v_path=/opt/ml/input/data/model/audio_base_ls.pt \
+		criterion._name=ctc \
+		checkpoint.save_dir=/opt/ml/model 

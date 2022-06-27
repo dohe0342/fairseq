7baf38bb495827b9bc1d21ab72adc1accea70a74
dohe0342 <kimdohe1070@gmail.com>
2022-06-27 20:02:27 +0900

    from local

--- scripts/aws/ft_data2vec_fgsm.sh
+++ scripts/aws/ft_data2vec_fgsm.sh
@@ -9,7 +9,7 @@ fairseq-hydra-train \
 	task.data=/opt/ml/code/LibriSpeech/manifests \
 	task.normalize=true \
 	model.w2v_path=/opt/ml/input/data/model/audio_base_ls.pt \
-	criterion._name=ctc_fgsm \
+	criterion._name=ctc \
 	checkpoint.save_dir=/opt/ml/model \
 	dataset.max_tokens=3200000 \
 	optimization.update_freq=[4]

a660f0a70a083ba299a1b57a41f529d50d85eba8
dohe0342 <kimdohe1070@gmail.com>
2022-06-27 14:03:32 +0900

    from local

--- scripts/aws/ft_data2vec.sh
+++ scripts/aws/ft_data2vec.sh
@@ -1,15 +1,36 @@
-for i in {0..9}
-do
-	fairseq-hydra-train \
-		--config-dir /opt/ml/code/fairseq/examples/wav2vec/config/finetuning \
-		--config-name base_100h_aws_g5 \
-		common.user_dir=examples/data2vec \
-		task.data=/opt/ml/code/LibriSpeech/manifests \
-		task.normalize=true \
-		model.w2v_path=/opt/ml/input/data/model/audio_base_ls.pt \
-		criterion._name=viewmaker \
-		checkpoint.save_dir=/opt/ml/model \
-		dataset.max_tokens=3200000 \
-		optimization.update_freq=[4] \
-		+model.viewmaker=true
-done
+mode="w2v"
+
+if [ $mode == "w2v" ]
+then
+	for i in {0..9}
+	do
+		fairseq-hydra-train \
+			--config-dir /opt/ml/code/fairseq/examples/wav2vec/config/finetuning \
+			--config-name base_100h_aws_g5 \
+			common.user_dir=examples/data2vec \
+			task.data=/opt/ml/code/LibriSpeech/manifests \
+			task.normalize=true \
+			model.w2v_path=/opt/ml/input/data/model/audio_base_ls.pt \
+			criterion._name=viewmaker \
+			checkpoint.save_dir=/opt/ml/model \
+			dataset.max_tokens=3200000 \
+			optimization.update_freq=[4] \
+			+model.viewmaker=true
+	done
+else
+	for i in {0..9}
+	do
+		fairseq-hydra-train \
+			--config-dir /opt/ml/code/fairseq/examples/wav2vec/config/finetuning \
+			--config-name base_100h_aws_g5 \
+			common.user_dir=examples/data2vec \
+			task.data=/opt/ml/code/LibriSpeech/manifests \
+			task.normalize=true \
+			model.w2v_path=/opt/ml/input/data/model/audio_base_ls.pt \
+			criterion._name=viewmaker \
+			checkpoint.save_dir=/opt/ml/model \
+			dataset.max_tokens=3200000 \
+			optimization.update_freq=[4] \
+			+model.viewmaker=true
+	done
+fi

874d51e3b75c6f09cc894056a53e03a2330c6bff
dohe0342 <kimdohe1070@gmail.com>
2022-06-27 14:07:31 +0900

    from local

--- scripts/aws/ft_data2vec.sh
+++ scripts/aws/ft_data2vec.sh
@@ -6,7 +6,7 @@ then
 	do
 		fairseq-hydra-train \
 			--config-dir /opt/ml/code/fairseq/examples/wav2vec/config/finetuning \
-			--config-name base_100h_aws_p4 \
+			--config-name base_100h_aws_$instance \
 			common.user_dir=examples/data2vec \
 			task.data=/opt/ml/code/LibriSpeech/manifests \
 			task.normalize=false \
@@ -15,14 +15,14 @@ then
 			checkpoint.save_dir=/opt/ml/model \
 			dataset.max_tokens=3200000 \
 			optimization.update_freq=[2] \
-			+model.viewmaker=tre
+			+model.viewmaker=true
 	done
 else
 	for i in {0..9}
 	do
 		fairseq-hydra-train \
 			--config-dir /opt/ml/code/fairseq/examples/wav2vec/config/finetuning \
-			--config-name base_100h_aws_p4 \
+			--config-name base_100h_aws_$instance \
 			common.user_dir=examples/data2vec \
 			task.data=/opt/ml/code/LibriSpeech/manifests \
 			task.normalize=true \

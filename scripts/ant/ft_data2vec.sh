bf36a76b4521506e6bccc927ab1184272f0418c9
dohe0342 <kimdohe1070@gmail.com>
2022-06-27 13:17:03 +0900

    from local

--- scripts/ant/ft_data2vec.sh
+++ scripts/ant/ft_data2vec.sh
@@ -2,7 +2,8 @@ git pull
 mode="w2v"
 exp_name=$1
 
-if [ $mode -eq "w2v" ]; then
+if [ $mode -eq "w2v" ]
+then
 	echo "here!!"
 	fairseq-hydra-train \
 		--config-dir /workspace/fairseq/examples/wav2vec/config/finetuning \

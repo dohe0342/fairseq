837218ae8cdb145bbadeafe86569a7fed9bb1bcb
dohe0342 <kimdohe1070@gmail.com>
2022-07-06 13:38:33 +0900

    from local

--- fairseq/tasks/hubert_pretraining.py
+++ fairseq/tasks/hubert_pretraining.py
@@ -154,7 +154,6 @@ class HubertPretrainingTask(FairseqTask):
         except:
             self.cfg.fine_tuning = True
             dictionaries = [[None]]
-            print(dictionaries)
         
         return dictionaries[0] if self.cfg.fine_tuning else dictionaries
 

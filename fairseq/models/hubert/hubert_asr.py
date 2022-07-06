9063aa792247a84764fd4cf228b29b130ebf8c4a
dohe0342 <kimdohe1070@gmail.com>
2022-07-06 13:06:27 +0900

    from local

--- fairseq/models/hubert/hubert_asr.py
+++ fairseq/models/hubert/hubert_asr.py
@@ -455,6 +455,7 @@ class HubertEncoderViewMaker(FairseqEncoder):
                 x_new = x_new.transpose(0, 1)
 
         x = self.final_dropout(x)
+        x_new = self.final_dropout(x_new)
 
         if self.proj:
             x = self.proj(x)

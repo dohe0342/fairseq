d190bb806fa0ecedce0bdfcb05843bfcc1e8e1f9
dohe0342 <kimdohe1070@gmail.com>
2022-07-06 13:41:17 +0900

    from local

--- scripts/ant/test_hubert.py
+++ scripts/ant/test_hubert.py
@@ -1,5 +1,5 @@
 import fairseq
 
-ckpt_path = "/path/to/the/checkpoint.pt"
+ckpt_path = "/workspace/models/hubert_model/hubert_base_ls960.pt"
 models, cfg, task = fairseq.checkpoint_utils.load_model_ensemble_and_task([ckpt_path])
 model = models[0]

bc1e300eac61d5f1508ef9f2d0473c18df01ede3
dohe0342 <kimdohe1070@gmail.com>
2022-07-08 16:04:26 +0900

    from local

--- scripts/whale/ft_data2vec.sh
+++ scripts/whale/ft_data2vec.sh
@@ -5,7 +5,7 @@ exp_name=$1
 if [ $mode == "w2v" ]
 then
 	fairseq-hydra-train \
-		--config-dir /workspace/fairseq/examples/wav2vec/config/finetuning \
+		--config-dir /home/work/workspace/fairseq/examples/wav2vec/config/finetuning \
 		--config-name base_100h_ant \
 		common.user_dir=examples/data2vec \
 		task.data=/workspace/LibriSpeech/manifests \

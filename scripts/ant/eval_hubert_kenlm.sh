da5cff145d57c74a6134c794a308a1cba00c7e2a
dohe0342 <kimdohe1070@gmail.com>
2022-07-09 11:47:07 +0900

    from local

--- scripts/ant/eval_hubert_kenlm.sh
+++ scripts/ant/eval_hubert_kenlm.sh
@@ -6,9 +6,9 @@ for subset in "dev-clean" "dev-other" "test-clean" "test-other"
 do
 	echo "====================   $model // $subset   ===================="
 	python /workspace/fairseq/examples/speech_recognition/new/infer.py \
-		--config-dir /workspace/fairseq//examples/hubert/config/decode \
+		--config-dir /workspace/fairseq/examples/hubert/config/decode \
 		--config-name infer_kenlm \
-		task.data=/path/to/data \
+		task.data=/workspace/LibriSpeech/manifests \
 		task.normalize=[true|false] \
 		decoding.exp_dir=/path/to/experiment/directory \
 		common_eval.path=/path/to/checkpoint

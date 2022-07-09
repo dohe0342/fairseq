d762f7e8f7b9a507a4cb0572b4eecaba70b36109
dohe0342 <kimdohe1070@gmail.com>
2022-07-09 11:05:29 +0900

    from local

--- watchdog_v2.py
+++ watchdog_v2.py
@@ -1,44 +1,6 @@
 import os 
 import time 
-dirToWatchList = [
-            "./",
-            "./workspace",
-            "./scripts/", 
-            "./scripts/whale/", 
-            "./scripts/summit/", 
-            "./scripts/ant/", 
-            "./scripts/aws/", 
-            "./fairseq_cli/", 
-            "./fairseq/", 
-            "./fairseq/tasks/", 
-            "./fairseq/optim/",
-            "./fairseq/criterions/",
-            "./fairseq/modules/", 
-            "./fairseq/models/", 
-            "./fairseq/models/wav2vec/",
-            "./fairseq/models/hubert/",
-            "./fairseq/models/roberta/",
-            "./fairseq/models/transformer/", 
-            "./examples/wav2vec/",
-            "./examples/wav2vec/config/",
-            "./examples/wav2vec/config/finetuning/",
-            "./examples/wav2vec/config/pretraining/",
-            "./examples/data2vec/",
-            "./examples/data2vec/models/",
-            "./examples/data2vec/config/audio/pretraining/",
-            "./examples/speech_recognition/",
-            "./examples/speech_recognition/new/",
-            "./examples/speech_recognition/new/conf/",
-            "./examples/speech_recognition/new/decoders/",
-            "./examples/roberta/config/pretraining/",
-            "./examples/roberta/config/finetuning/",
-            "./examples/hubert/",
-            "./examples/hubert/config/",
-            "./examples/hubert/config/finetune/",
-            ]
 
-#lastmod = [dirpath[0] for dirpath in os.walk(path)]
-#lastmod = [int(os.path.getmtime(dirToWatch)) for dirToWatch in dirToWatchList]
 lastmod = []
 dirToWatchList = []
 path = './'

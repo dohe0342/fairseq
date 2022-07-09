821c5cb20fd9b9bc1e58d9ea8e5f3367dadd2b6b
dohe0342 <kimdohe1070@gmail.com>
2022-07-09 11:46:09 +0900

    from local

--- scripts/ant/eval_hubert_kenlm.sh
+++ scripts/ant/eval_hubert_kenlm.sh
@@ -1,12 +1,18 @@
+git pull
+source ~/.bashrc
+model=$1
 
-
-python examples/speech_recognition/new/infer.py \
-  --config-dir /path/to/fairseq-py/examples/hubert/config/decode \
-  --config-name infer_kenlm \
-  task.data=/path/to/data \
-  task.normalize=[true|false] \
-  decoding.exp_dir=/path/to/experiment/directory \
-  common_eval.path=/path/to/checkpoint
-  dataset.gen_subset=test \
-  decoding.decoder.lexicon=/path/to/lexicon \
-  decoding.decoder.lmpath=/path/to/arpa
+for subset in "dev-clean" "dev-other" "test-clean" "test-other" 
+do
+	echo "====================   $model // $subset   ===================="
+	python examples/speech_recognition/new/infer.py \
+		--config-dir /path/to/fairseq-py/examples/hubert/config/decode \
+		--config-name infer_kenlm \
+		task.data=/path/to/data \
+		task.normalize=[true|false] \
+		decoding.exp_dir=/path/to/experiment/directory \
+		common_eval.path=/path/to/checkpoint
+		dataset.gen_subset=test \
+		decoding.decoder.lexicon=/path/to/lexicon \
+		decoding.decoder.lmpath=/path/to/arpa
+done

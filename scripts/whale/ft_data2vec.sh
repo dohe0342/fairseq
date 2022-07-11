93d38a8309b0c24a00493f83d15c473d6aacfd30
dohe0342 <kimdohe1070@gmail.com>
2022-07-11 17:14:15 +0900

    from local

--- scripts/whale/ft_data2vec.sh
+++ scripts/whale/ft_data2vec.sh
@@ -23,8 +23,9 @@ then
 		task.data=/home/work/workspace/LibriSpeech/manifests \
 		task.normalize=false \
 		model.w2v_path=/home/work/workspace/models/hubert_model/hubert_base_ls960.pt \
-		criterion._name=ctc \
-		checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1
+		criterion._name=viewmaker \
+		checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 \
+		_model.viewmaker=true
 else
 	fairseq-hydra-train \
 		--config-dir /home/work/workspace/fairseq/examples/wav2vec/config/finetuning \

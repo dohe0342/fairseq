8157ceb1ce100771a68c8d202459610a4d98a700
dohe0342 <kimdohe1070@gmail.com>
2022-07-11 16:59:06 +0900

    from local

--- scripts/ant/eval_hubert_kenlm.sh
+++ scripts/ant/eval_hubert_kenlm.sh
@@ -8,9 +8,9 @@ do
 	python /workspace/fairseq/examples/speech_recognition/new/infer.py \
 		--config-dir /workspace/fairseq/examples/hubert/config/decode \
 		--config-name infer_kenlm \
+		common_eval.path=/workspace/models/hubert_model/hubert_baseline.pt \
 		task.data=/workspace/LibriSpeech/manifests \
 		task.normalize=false \
-		common_eval.path=/workspace/models/hubert_model/hubert_baseline.pt \
 		dataset.gen_subset=$subset \
 		decoding.lexicon=/workspace/models/lm_model/librispeech_lexicon.lst \
 		decoding.lmpath=/workspace/models/lm_model/4-gram.arpa.gz

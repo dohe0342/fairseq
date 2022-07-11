cc5972ed7a2b9e53d4be7a0b33922051a2c1ee7a
dohe0342 <kimdohe1070@gmail.com>
2022-07-11 17:42:39 +0900

    from local

--- scripts/aws/ft_data2vec.sh
+++ scripts/aws/ft_data2vec.sh
@@ -21,14 +21,14 @@ then
 elif [ $mode == "hubert" ]
 then
     fairseq-hydra-train \
-        --config-dir /home/work/workspace/fairseq/examples/hubert/config/finetune \
+        --config-dir /opt/ml/code/fairseq/examples/hubert/config/finetune \
         --config-name base_100h_whale \
-        task.label_dir=/home/work/workspace/LibriSpeech/manifests \
-        task.data=/home/work/workspace/LibriSpeech/manifests \
+        task.label_dir=/opt/ml/code/LibriSpeech/manifests \
+        task.data=/opt/ml/code/LibriSpeech/manifests \
         task.normalize=false \
-        model.w2v_path=/home/work/workspace/models/hubert_model/hubert_base_ls960.pt \
+        model.w2v_path=/opt/ml/code/models/hubert_model/hubert_base_ls960.pt \
         criterion._name=viewmaker \
-        checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 \
+        checkpoint.save_dir=/opt/ml/code/fairseq/scripts/whale/outputs/$1 \
         +model.viewmaker=true
 	rm /opt/ml/model/crash.pt
 

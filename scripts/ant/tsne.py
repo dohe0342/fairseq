5e3c28ca395bd8fb8d1741733d002e05bb233db4
dohe0342 <kimdohe1070@gmail.com>
2022-07-12 22:19:37 +0900

    from local

--- scripts/ant/tsne.py
+++ scripts/ant/tsne.py
@@ -27,7 +27,7 @@ for cls in tqdm(class_list):
         else:
             np_list = np.concatenate((np_list, file.reshape(1, 512)), axis=0)
         
-        if enum >= 50:
+        if enum >= 100:
             break
 
     num_list.append(enum)

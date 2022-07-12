55cf16a3b457779128c9420252512b752e99e090
dohe0342 <kimdohe1070@gmail.com>
2022-07-12 17:39:08 +0900

    from local

--- scripts/ant/wrong_word_sort.py
+++ scripts/ant/wrong_word_sort.py
@@ -37,6 +37,19 @@ for hypo, ref in zip(hypo1_list, ref_list):
         try: wrong_dict[(r, h)] += 1
         except: wrong_dict[(r, h)] = 1
 
+for hypo, ref in zip(hypo2_list, ref_list):
+    hypo = hypo.split()
+    ref = ref.split()
+    
+    d = editDistance(ref, hypo)
+    aligned = getStepList(ref, hypo, d)
+    aligned_list.append(aligned)
+    
+    ref_s, hypo_s = alignedPrint(aligned, ref, hypo)
+    for r, h in zip(ref_s, hypo_s):
+        try: wrong_dict[(r, h)] += 1
+        except: wrong_dict[(r, h)] = 1
+
 wrong_dict = sorted(wrong_dict.items(), key=lambda x:x[1], reverse=True)
 for pair, count in wrong_dict:
     print(pair[0], pair[1], count)

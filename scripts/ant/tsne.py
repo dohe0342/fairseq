f8a14e4fb11983469e6fa2e81cea95eba4d60cf3
dohe0342 <kimdohe1070@gmail.com>
2022-07-12 22:06:01 +0900

    from local

--- scripts/ant/tsne.py
+++ scripts/ant/tsne.py
@@ -37,7 +37,7 @@ print(sum(num_list))
 colors = cm.rainbow(np.linspace(0, 1, len(num_list)))
 
 for o, c in zip(output, colors):
-    plt.scatter(o[0], o[1], color=
+    plt.scatter(o[0], o[1], color=c)
 scatter_list.append(ax.scatter(output[:, 0], 
                         output[:, 1], 
                         #c = color_list[i],

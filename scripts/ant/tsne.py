909958057943d3bd11596ee2cfe45f9289917e8d
dohe0342 <kimdohe1070@gmail.com>
2022-07-12 22:26:06 +0900

    from local

--- scripts/ant/tsne.py
+++ scripts/ant/tsne.py
@@ -42,7 +42,6 @@ print(sum(num_list))
 colors = cm.rainbow(np.linspace(0, 1, len(num_list)))
 
 for enum in range(output.shape[0]):
-    print(o)
     plt.scatter(output[enum][0], output[enum][1], color=colors[enum])
 
 plt.savefig('./vanilla_decision.png', bbox_inches='tight', dpi=300)

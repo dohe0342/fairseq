ba47abd2313fc0881003828454f7fc71cf8119d9
dohe0342 <kimdohe1070@gmail.com>
2022-07-12 21:06:08 +0900

    from local

--- scripts/ant/tsne.py
+++ scripts/ant/tsne.py
@@ -29,6 +29,7 @@ for cls in class_list:
             first = False
         else:
             np.stack((np_list, file.reshape(1, 512)))
+        print(file.reshape(1, 512).shape)
         print('np shape = ', np_list.shape)
 
 print(np_list.shape)

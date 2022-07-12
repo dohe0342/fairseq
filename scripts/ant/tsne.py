19b3d1bfcda2daf0233c9081439e4060acf00072
dohe0342 <kimdohe1070@gmail.com>
2022-07-12 21:04:27 +0900

    from local

--- scripts/ant/tsne.py
+++ scripts/ant/tsne.py
@@ -29,7 +29,7 @@ for cls in class_list:
             first = False
         else:
             np.concatenate((np_list, file.reshape(1, 512)), axis=0)
-        print(np_list.shape)
+            print(file.shape)
 
 
 print(np_list.shape)

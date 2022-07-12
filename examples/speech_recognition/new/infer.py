29f39b00dee5afb961c6ba899c4bd895932cf077
dohe0342 <kimdohe1070@gmail.com>
2022-07-12 20:11:47 +0900

    from local

--- examples/speech_recognition/new/infer.py
+++ examples/speech_recognition/new/infer.py
@@ -424,6 +424,7 @@ class InferenceProcessor:
                         #print(feat.shape)
                         #np.save(f'./t100_cnnfeat/{int(idx[time])}/{str(random.randint(0,1000000)).zfill(7)}.npy', feat)
                         np.save('./temp.npy', feat)
+                        exit()
 
             #print(zero_count.sum().item(), zero_count.size()[0])
             #print(idx)

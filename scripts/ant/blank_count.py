a5570d151025f88a0f7bae6808d3f5ebc03afef3
dohe0342 <kimdohe1070@gmail.com>
2022-07-12 19:01:07 +0900

    from local

---
+++ scripts/ant/blank_count.py
@@ -0,0 +1,14 @@
+filename_list = ["d2v_vanilla_blank.txt", "d2v_mba_blank.txt"]
+
+for filename in filename_list:
+    file = open(filename, 'r').readlines()
+    all_blank = 0
+    all_count = 0
+    for line in file[4:-3]:
+        blank, length = line.split(' ')
+        blank = int(blank)
+        length = int(length.replace('\n', ''))
+        all_blank += blank
+        all_length += length
+
+    print(filename, all_blank, all_length)

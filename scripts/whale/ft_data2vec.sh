76551330f8d9ac3b99efc9dfcef787008755daec
dohe0342 <kimdohe1070@gmail.com>
2022-07-13 18:03:51 +0900

    from local

--- scripts/whale/ft_data2vec.sh
+++ scripts/whale/ft_data2vec.sh
@@ -4,39 +4,48 @@ exp_name=$1
 
 if [ $mode == "w2v" ]
 then
-	fairseq-hydra-train \
-		--config-dir /home/work/workspace/fairseq/examples/wav2vec/config/finetuning \
-		--config-name base_100h_whale \
-		common.user_dir=examples/data2vec \
-		task.data=/home/work/workspace/LibriSpeech/manifests \
-		task.normalize=false \
-		model.w2v_path=/home/work/workspace/models/wav2vec_model/wav2vec_small.pt \
-		criterion._name=viewmaker \
-		checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 \
-		+model.viewmaker=true
+	for i in {0..9}
+	do
+		fairseq-hydra-train \
+			--config-dir /home/work/workspace/fairseq/examples/wav2vec/config/finetuning \
+			--config-name base_100h_whale \
+			common.user_dir=examples/data2vec \
+			task.data=/home/work/workspace/LibriSpeech/manifests \
+			task.normalize=false \
+			model.w2v_path=/home/work/workspace/models/wav2vec_model/wav2vec_small.pt \
+			criterion._name=viewmaker \
+			checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 \
+			+model.viewmaker=true
+	done
 elif [ $mode == "hubert" ]
 then
-	fairseq-hydra-train \
-		--config-dir /home/work/workspace/fairseq/examples/hubert/config/finetune \
-		--config-name base_100h_whale \
-		task.label_dir=/home/work/workspace/LibriSpeech/manifests \
-		task.data=/home/work/workspace/LibriSpeech/manifests \
-		task.normalize=false \
-		model.w2v_path=/home/work/workspace/models/hubert_model/hubert_base_ls960.pt \
-		criterion._name=viewmaker \
-		checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 \
-		+model.viewmaker=true
+	for i in {0..9}
+	do
+		fairseq-hydra-train \
+			--config-dir /home/work/workspace/fairseq/examples/hubert/config/finetune \
+			--config-name base_100h_whale \
+			task.label_dir=/home/work/workspace/LibriSpeech/manifests \
+			task.data=/home/work/workspace/LibriSpeech/manifests \
+			task.normalize=false \
+			model.w2v_path=/home/work/workspace/models/hubert_model/hubert_base_ls960.pt \
+			criterion._name=viewmaker \
+			checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 \
+			+model.viewmaker=true
+	done
 else
-	fairseq-hydra-train \
-		--config-dir /home/work/workspace/fairseq/examples/wav2vec/config/finetuning \
-		--config-name base_100h_whale \
-		common.user_dir=examples/data2vec \
-		task.data=/home/work/workspace/LibriSpeech/manifests \
-		task.normalize=true \
-		model.w2v_path=/home/work/workspace/models/data2vec_model/audio_base_ls.pt \
-		criterion._name=viewmaker \
-		checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 \
-		+model.viewmaker=true
+	for i in {0..9}
+	do
+		fairseq-hydra-train \
+			--config-dir /home/work/workspace/fairseq/examples/wav2vec/config/finetuning \
+			--config-name base_100h_whale \
+			common.user_dir=examples/data2vec \
+			task.data=/home/work/workspace/LibriSpeech/manifests \
+			task.normalize=true \
+			model.w2v_path=/home/work/workspace/models/data2vec_model/audio_base_ls.pt \
+			criterion._name=viewmaker \
+			checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 \
+			+model.viewmaker=true
+	done
 fi
 
 :<<'END'

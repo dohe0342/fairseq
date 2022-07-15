3d0893795f3064ba7c32349ccd7ba7c3b7add374
dohe0342 <kimdohe1070@gmail.com>
2022-07-15 13:55:39 +0900

    from local

--- examples/speech_recognition/new/infer.py
+++ examples/speech_recognition/new/infer.py
@@ -107,6 +107,8 @@ class InferenceProcessor:
             del models
             models_hardloading, _, _ = fairseq.checkpoint_utils.load_model_ensemble_and_task(['/workspace/models/wav2vec_model/w2v_l_1gen.pt'])
             models = models_hardloading
+
+            print(models)
         self.models = models
         '''
         with torch.no_grad():

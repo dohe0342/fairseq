fc8e2e571fa436219545dbe94f9864dbaabc7309
dohe0342 <kimdohe1070@gmail.com>
2022-07-18 13:59:37 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -1000,11 +1000,11 @@ class CtcCriterion(FairseqCriterion):
                         logging_output["c_errors"] = c_err
                         logging_output["c_total"] = c_len
                     else:
-                        logging_output["wv_errors"] = wv_errs
-                        logging_output["w_errors"] = w_errs
-                        logging_output["w_total"] = w_len
-                        logging_output["c_errors"] = c_err
-                        logging_output["c_total"] = c_len
+                        logging_output["wv_errors viewmaker"] = wv_errs
+                        logging_output["w_errors viewmaker"] = w_errs
+                        logging_output["w_total viewmaker"] = w_len
+                        logging_output["c_errors viewmaker"] = c_err
+                        logging_output["c_total viewmaker"] = c_len
  
         return [loss, net_output["loss"]], sample_size, logging_output
 

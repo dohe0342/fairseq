90adc50058cda98f49018b371ff6d9f2a2327dc2
dohe0342 <kimdohe1070@gmail.com>
2022-07-18 14:53:34 +0900

    from local

--- fairseq/models/wav2vec/wav2vec2.py
+++ fairseq/models/wav2vec/wav2vec2.py
@@ -604,6 +604,10 @@ class Wav2Vec2Model(BaseFairseqModel):
                     features = GradMultiply.apply(features, self.feature_grad_mult)
             else:
                 with torch.no_grad():
+                    print('*'*30)
+                    print(source.size())
+                    print('*'*30)
+
                     features = self.feature_extractor(source)
 
             features_pen = features.float().pow(2).mean()

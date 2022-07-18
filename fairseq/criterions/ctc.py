7cfc0dbf99042b5f7be4898e766ad7c5fb491752
dohe0342 <kimdohe1070@gmail.com>
2022-07-18 14:03:20 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -1105,13 +1105,13 @@ class CtcCriterion(FairseqCriterion):
                 lambda meters: safe_round(
                     meters["_w_errors viewmaker"].sum * 100.0 / meters["_w_total viewmaker"].sum, 3
                 )
-                if meters["_w_total"].sum > 0
+                if meters["_w_total viewmaker"].sum > 0
                 else float("nan"),
             )
             metrics.log_derived(
                 "raw_wer",
                 lambda meters: safe_round(
-                    meters["_wv_errors"].sum * 100.0 / meters["_w_total"].sum, 3
+                    meters["_wv_errors viewmaker"].sum * 100.0 / meters["_w_total"].sum, 3
                 )
                 if meters["_w_total"].sum > 0
                 else float("nan"),

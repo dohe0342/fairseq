0cf905a8d20805473d4232a1c9aacf8d296edda1
dohe0342 <kimdohe1070@gmail.com>
2022-07-18 14:01:34 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -1057,7 +1057,7 @@ class CtcCriterion(FairseqCriterion):
         c_total_new = sum(log.get("c_total viewmaker", 0) for log in logging_outputs)
         metrics.log_scalar("_c_total viewmaker", c_total)
         w_errors_new = sum(log.get("w_errors viewmaker", 0) for log in logging_outputs)
-        metrics.log_scalar("_w_errors", w_errors)
+        metrics.log_scalar("_w_errors viewmaker", w_errors)
         wv_errors_new = sum(log.get("wv_errors", 0) for log in logging_outputs)
         metrics.log_scalar("_wv_errors", wv_errors)
         w_total_new = sum(log.get("w_total", 0) for log in logging_outputs)

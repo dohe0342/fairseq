9d749c7b71767f5b0b50aaf739fdd9b428978cb7
dohe0342 <kimdohe1070@gmail.com>
2022-07-18 14:57:02 +0900

    from local

--- fairseq/models/wav2vec/wav2vec2.py
+++ fairseq/models/wav2vec/wav2vec2.py
@@ -962,8 +962,8 @@ class ConvFeatureExtractionModel(nn.Module):
         x = x.unsqueeze(1)
 
         for conv in self.conv_layers:
-            print(x.size())
             x = conv(x)
+            print(x.size())
 
         return x
 

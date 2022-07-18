c94c4c338112be2ab1a252d2508c7ffa0750cc86
dohe0342 <kimdohe1070@gmail.com>
2022-07-18 16:38:28 +0900

    from local

--- scripts/ant/ft_hubert.sh
+++ scripts/ant/ft_hubert.sh
@@ -1,9 +1,9 @@
 fairseq-hydra-train \
-    --config-dir /opt/ml/code/fairseq/examples/hubert/config/finetune \
+    --config-dir /workspace/fairseq/examples/hubert/config/finetune \
     --config-name base_100h_ant \
-    task.label_dir=/opt/ml/code/LibriSpeech/manifests \
-    task.data=/opt/ml/code/LibriSpeech/manifests \
+    task.label_dir=/workspace/LibriSpeech/manifests \
+    task.data=/workspace/LibriSpeech/manifests \
     task.normalize=false \
-    model.w2v_path=/opt/ml/input/data/model/hubert_base_ls960.pt \
+    model.w2v_path=/workspace/model/hubert_base_ls960.pt \
     criterion._name=ctc \
     checkpoint.save_dir=/opt/ml/model 

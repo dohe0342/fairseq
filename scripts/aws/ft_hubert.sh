cb3e9232e29b69b3216521eb28bbc5c3a949f30d
dohe0342 <kimdohe1070@gmail.com>
2022-07-18 16:33:33 +0900

    from local

--- scripts/aws/ft_hubert.sh
+++ scripts/aws/ft_hubert.sh
@@ -1,6 +1,11 @@
-git pull
-python ../fairseq_cli/hydra_train.py \
-  --config-dir ../examples/hubert/config/finetune \
-  --config-name base_10h \
-  task.data=/path/to/data task.label_dir=/path/to/trans \
-  model.w2v_path=/path/to/checkpoint
+instance="g5"
+fairseq-hydra-train \
+	--config-dir /opt/ml/code/fairseq/examples/hubert/config/finetune \
+	--config-name base_100h_aws_$instance \
+	task.label_dir=/opt/ml/code/LibriSpeech/manifests \
+	task.data=/opt/ml/code/LibriSpeech/manifests \
+	task.normalize=false \
+	model.w2v_path=/opt/ml/input/data/model/hubert_base_ls960.pt \
+	criterion._name=viewmaker \
+	checkpoint.save_dir=/opt/ml/model \
+	+model.viewmaker=true

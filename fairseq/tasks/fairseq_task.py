51ce625166a3185886ee760b14aee27ae6db4ba4
dohe0342 <kimdohe1070@gmail.com>
2022-07-19 12:06:01 +0900

    from local

--- fairseq/tasks/fairseq_task.py
+++ fairseq/tasks/fairseq_task.py
@@ -616,7 +616,7 @@ class FairseqTask(object):
                     #lambda_ = -0.00001*(1+torch.cos(torch.tensor(update_num)*math.pi/1810.)) ## try25 try 26 try 27
                     #lambda_ = -0.00001*(1+torch.cos(torch.tensor(update_num)*math.pi/1810.)) ## try25 try 26 try 27
                     #lambda_ = -0.00001*(1+torch.cos(torch.tensor(update_num)*math.pi/1810.)) ## try25 try 26 try 27
-                    lambda_ = -0.00001*(1+torch.cos(torch.tensor(update_num)*math.pi/1810.)) ## for hubert
+                    lambda_ = -0.00001*(1+torch.cos(torch.tensor(update_num)*math.pi/3610.)) ## for hubert
                     optimizer[1].backward(lambda_*loss[0][1]+loss[1])
                     #optimizer[1].backward(lambda_*loss[0][1])
                     #optimizer[1].backward(-loss[0][1]+loss[1])

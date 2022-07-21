a37fd172db2061ac5ad1a5a3b541b347475bd35b
dohe0342 <kimdohe1070@gmail.com>
2022-07-21 18:46:54 +0900

    from local

--- scripts/aws/ft_data2vec.sh
+++ scripts/aws/ft_data2vec.sh
@@ -14,22 +14,20 @@ then
 			checkpoint.save_dir=/opt/ml/model \
 			+model.viewmaker=true
 	rm /opt/ml/model/crash.pt
-:<<'END'
-	for i in {0..9}
-	do
-		fairseq-hydra-train \
-			--config-dir /opt/ml/code/fairseq/examples/wav2vec/config/finetuning \
-			--config-name base_100h_aws_$instance \
-			common.user_dir=examples/data2vec \
-			task.data=/opt/ml/code/LibriSpeech/manifests \
-			task.normalize=false \
-			model.w2v_path=/opt/ml/input/data/model/wav2vec_small.pt \
-			criterion._name=viewmaker \
-			checkpoint.save_dir=/opt/ml/model \
-			+model.viewmaker=true
-		rm /opt/ml/model/crash.pt
-	done
-END
+	#for i in {0..9}
+	#do
+	#	fairseq-hydra-train \
+	#		--config-dir /opt/ml/code/fairseq/examples/wav2vec/config/finetuning \
+	#		--config-name base_100h_aws_$instance \
+	#		common.user_dir=examples/data2vec \
+	#		task.data=/opt/ml/code/LibriSpeech/manifests \
+	#		task.normalize=false \
+	#		model.w2v_path=/opt/ml/input/data/model/wav2vec_small.pt \
+	#		criterion._name=viewmaker \
+	#		checkpoint.save_dir=/opt/ml/model \
+	#		+model.viewmaker=true
+	#	rm /opt/ml/model/crash.pt
+	#done
 
 elif [ $mode == "hubert" ]
 then

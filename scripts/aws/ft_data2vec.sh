f8c21b518bbe3b69a375659f67e6823f760eeb36
dohe0342 <kimdohe1070@gmail.com>
2022-07-22 11:26:23 +0900

    from local

--- scripts/aws/ft_data2vec.sh
+++ scripts/aws/ft_data2vec.sh
@@ -19,7 +19,7 @@ END
 				--config-name base_100h_aws_$instance \
 				common.user_dir=examples/data2vec \
 				task.data=/opt/ml/code/LibriSpeech/manifests \
-				task.normalize=false \
+				task.normalize=true \
 				model.w2v_path=/opt/ml/input/data/model/wav2vec_vox_new.pt \
 				criterion._name=viewmaker \
 				checkpoint.save_dir=/opt/ml/model \

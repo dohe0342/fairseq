471e29ce32dd41d504f26108876b2049fc7384d5
dohe0342 <kimdohe1070@gmail.com>
2022-07-22 10:28:34 +0900

    from local

--- scripts/aws/ft_data2vec.sh
+++ scripts/aws/ft_data2vec.sh
@@ -1,5 +1,4 @@
-mode="w2v"
-instance="g5"
+mode="w2v" instance="g5"
 
 if [ $mode == "w2v" ]
 then
@@ -13,6 +12,20 @@ then
 			checkpoint.save_dir=/opt/ml/model 
 	rm /opt/ml/model/crash.pt
 END
+for i in {0..9}
+	do
+		fairseq-hydra-train \
+			--config-dir /opt/ml/code/fairseq/examples/wav2vec/config/finetuning \
+			--config-name base_100h_aws_$instance \
+			task.data=/opt/ml/code/LibriSpeech/manifests \
+			task.normalize=false \
+			model.w2v_path=/opt/ml/input/data/model/wav2vec_vox_new.pt \
+			criterion._name=viewmaker \
+			checkpoint.save_dir=/opt/ml/model \
+			+model.viewmaker=true
+		rm /opt/ml/model/crash.pt
+	done
+
 elif [ $mode == "hubert" ]
 then
 	#for i in {0..9}

1a63191cfe6ae33fcd9f3214baebeaf3c5492860
dohe0342 <kimdohe1070@gmail.com>
2022-08-01 14:58:21 +0900

    from local

--- scripts/ant/wav_display.py
+++ scripts/ant/wav_display.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import librosa
+import librosa.display
 
 y, sr = librosa.load('/workspace/LibriSpeech/test-clean/3570/5694/3570-5694-0008.wav', sr=16000)
 plt.figure()

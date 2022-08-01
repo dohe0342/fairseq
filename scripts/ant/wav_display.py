c5ff79b1cd93a4d9615dfb81ff6fd453a3646fa8
dohe0342 <kimdohe1070@gmail.com>
2022-08-01 14:59:56 +0900

    from local

--- scripts/ant/wav_display.py
+++ scripts/ant/wav_display.py
@@ -4,5 +4,5 @@ import librosa.display as display
 
 y, sr = librosa.load('/workspace/LibriSpeech/test-clean/3570/5694/3570-5694-0008.wav', sr=16000)
 plt.figure()
-display.waveplot(y, sr=sr)
+display.waveshow(y, sr=sr)
 plt.savefig('./test_wavform.png', dpi=300)

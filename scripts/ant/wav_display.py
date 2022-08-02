2b94f93229ac812915090492d3cd739069e87215
dohe0342 <kimdohe1070@gmail.com>
2022-08-02 15:58:05 +0900

    from local

--- scripts/ant/wav_display.py
+++ scripts/ant/wav_display.py
@@ -1,8 +1,11 @@
 import matplotlib.pyplot as plt
 import librosa
 import librosa.display as display
-
-y, sr = librosa.load('/workspace/LibriSpeech/test-clean/3570/5694/3570-5694-0008.wav', sr=16000)
-plt.figure()
-display.waveshow(y, sr=sr)
-plt.savefig('./test_wavform.png', dpi=300)
+import glob
+#y, sr = librosa.load('/workspace/LibriSpeech/test-clean/3570/5694/3570-5694-0008.wav', sr=16000)
+file_list = sorted(glob.glob('/workspace/play/*'))
+for wav_file in file_list:
+    y, sr = librosa.load(wav_file, sr=48000)
+    plt.figure()
+    display.waveshow(y, sr=sr)
+    plt.savefig('./test_wavform.png', dpi=300)

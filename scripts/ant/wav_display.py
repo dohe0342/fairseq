84810ddb90b03ae8041e115be87635ddcd10ef21
dohe0342 <kimdohe1070@gmail.com>
2022-08-02 15:59:01 +0900

    from local

--- scripts/ant/wav_display.py
+++ scripts/ant/wav_display.py
@@ -5,7 +5,8 @@ import glob
 #y, sr = librosa.load('/workspace/LibriSpeech/test-clean/3570/5694/3570-5694-0008.wav', sr=16000)
 file_list = sorted(glob.glob('/workspace/play/*'))
 for wav_file in file_list:
+    file_name = wav_file.split('/')[-1]
     y, sr = librosa.load(wav_file, sr=48000)
     plt.figure()
     display.waveshow(y, sr=sr)
-    plt.savefig('./test_wavf.png', dpi=300)
+    plt.savefig('./.png', dpi=300)

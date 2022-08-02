7808cca9946ead002b09ec87ca1f2afd664a1daa
dohe0342 <kimdohe1070@gmail.com>
2022-08-02 16:32:03 +0900

    from local

--- scripts/ant/wav2vec2_korean.py
+++ scripts/ant/wav2vec2_korean.py
@@ -4,20 +4,11 @@ import soundfile as sf
 import torch
 from jiwer import wer
 
-processor = Wav2Vec2Processor.from_pretrained("kresnik/wav2vec2-large-xlsr-korean")
-
-model = Wav2Vec2ForCTC.from_pretrained("kresnik/wav2vec2-large-xlsr-korean").to('cuda')
-
-ds = load_dataset("kresnik/zeroth_korean", "clean")
-
-test_ds = ds['test']
 
 def map_to_array(batch):
     speech, _ = sf.read(batch["file"])
     batch["speech"] = speech
     return batch
-
-test_ds = test_ds.map(map_to_array)
 
 def map_to_pred(batch):
     inputs = processor(batch["speech"], sampling_rate=16000, return_tensors="pt", padding="longest")
@@ -31,6 +22,11 @@ def map_to_pred(batch):
     batch["transcription"] = transcription
     return batch
 
+processor = Wav2Vec2Processor.from_pretrained("kresnik/wav2vec2-large-xlsr-korean")
+model = Wav2Vec2ForCTC.from_pretrained("kresnik/wav2vec2-large-xlsr-korean").to('cuda')
+ds = load_dataset("kresnik/zeroth_korean", "clean")
+test_ds = ds['test']
+test_ds = test_ds.map(map_to_array)
 result = test_ds.map(map_to_pred, batched=True, batch_size=16, remove_columns=["speech"])
 
 print("WER:", wer(result["text"], result["transcription"]))

723a75ea69a876dae7de5ea77ada21112d74c958
dohe0342 <kimdohe1070@gmail.com>
2022-08-02 16:03:03 +0900

    from local

--- scripts/ant/wav_display.py
+++ scripts/ant/wav_display.py
@@ -8,6 +8,7 @@ for wav_file in file_list:
     file_name = wav_file.split('/')[-1]
     y, sr = librosa.load(wav_file, sr=48000)
     y_resample = librosa.resample(y, orig_sr=sr, target_sr=16000)
+    librosa.output.write_wav(wav_file.replace('.wav', '_resample.wav'), y_resample, 16000)
     plt.figure()
     display.waveshow(y, sr=sr)
     plt.savefig(f'./{file_name}.png', dpi=300)

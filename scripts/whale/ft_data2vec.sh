86726f30b035ecb89bf1ed951b24d6c38b69928a
dohe0342 <kimdohe1070@gmail.com>
2022-08-04 15:57:35 +0900

    from local

--- scripts/whale/ft_data2vec.sh
+++ scripts/whale/ft_data2vec.sh
@@ -12,7 +12,7 @@ then
 			task.data=/home/work/workspace/LibriSpeech/manifests \
 			task.normalize=false \
 			model.w2v_path=/home/work/workspace/models/wav2vec_model/wav2vec_small.pt \
-			checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 \
+			checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 
 			#criterion._name=viewmaker \
 			#+model.viewmaker=true
 	done

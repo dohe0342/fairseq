6427966e9da6c0cf1c3dbe2ed52337d9dc0b46f5
dohe0342 <kimdohe1070@gmail.com>
2022-08-04 15:10:15 +0900

    from local

--- scripts/ant/eval_data2vec_kenlm.sh
+++ scripts/ant/eval_data2vec_kenlm.sh
@@ -9,13 +9,13 @@ do
 		--config-name infer \
 		task=audio_finetuning \
 		task.data=/workspace/LibriSpeech/manifests \
-		common.user_dir=/workspace/fairseq/examples/data2vec \
 		task.labels=ltr \
 		decoding.type=kenlm \
 		decoding.lmweight=1.74 decoding.wordscore=0.52 decoding.silweight=0 \
 		decoding.lexicon=/workspace/models/lm_model/librispeech_lexicon.lst \
 		decoding.lmpath=/workspace/models/lm_model/4-gram.arpa.gz \
 		decoding.unique_wer_file=true \
+		decoding.results_path=/workspace/fairseq/scripts/ant \
 		dataset.gen_subset=$subset \
 		common_eval.path=/workspace/models/wav2vec_model/$model \
 	   	decoding.beam=1500 \

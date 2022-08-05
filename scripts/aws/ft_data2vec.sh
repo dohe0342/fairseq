4791e8e75e85840d815ca8e85a00387c1b26588e
dohe0342 <kimdohe1070@gmail.com>
2022-08-05 15:11:16 +0900

    from local

--- scripts/aws/ft_data2vec.sh
+++ scripts/aws/ft_data2vec.sh
@@ -18,6 +18,7 @@ END
 				--config-dir /opt/ml/code/fairseq/examples/wav2vec/config/finetuning \
 				--config-name base_960h_aws_$instance \
 				task.data=/opt/ml/code/LibriSpeech/manifests \
+				task.normalize=false \
 				model.w2v_path=/opt/ml/input/data/model/wav2vec_small.pt \
 				checkpoint.save_dir=/opt/ml/model \
 				#criterion._name=viewmaker \

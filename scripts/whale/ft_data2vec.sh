a7e8e2b4cf9d508a4a19b4f2943b6a1ad3b866c3
dohe0342 <kimdohe1070@gmail.com>
2022-08-05 19:04:16 +0900

    from local

--- scripts/whale/ft_data2vec.sh
+++ scripts/whale/ft_data2vec.sh
@@ -12,8 +12,8 @@ then
 			task.data=/home/work/workspace/LibriSpeech/manifests \
 			model.w2v_path=/home/work/workspace/models/wav2vec_model/wav2vec_small.pt \
 			checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1
-			#criterion._name=viewmaker \
-			#+model.viewmaker=true
+			criterion._name=viewmaker \
+			+model.viewmaker=true
 	done
 elif [ $mode == "hubert" ]
 then

eb95abbcb7b984e8e7a957fdfe84806cd5df7578
dohe0342 <kimdohe1070@gmail.com>
2022-08-09 04:20:05 +0900

    from local

--- fairseq/trainer2.py
+++ fairseq/trainer2.py
@@ -605,10 +605,6 @@ class Trainer(object):
                     if 'viewmaker' in key:
                         del state["model"][key]
                 del keys
-                for n in state["model"]:
-                    if 'viewmaker' in n:
-                        print(f'delete {n}')
-                        del state["model"][n]
                         
                 self.model.load_state_dict(
                     state["model"], strict=False, model_cfg=self.cfg.model

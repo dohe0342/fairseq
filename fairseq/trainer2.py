92dda96143ddd999b016ce28a4399de308dd8a59
dohe0342 <kimdohe1070@gmail.com>
2022-08-09 19:03:26 +0900

    from local

--- fairseq/trainer2.py
+++ fairseq/trainer2.py
@@ -46,7 +46,7 @@ class Trainer(object):
 
     def __init__(self, cfg: FairseqConfig, task, model, criterion, quantizer=None):
         if 1: 
-            with open('/home/work/workspace/models/wavlm_model/wavlm_base.pickle', 'rb') as f:
+            with open('/workspace/models/wavlm_model/wavlm_base.pickle', 'rb') as f:
                 pickle_load_weights = pickle.load(f) 
        
             wavlm_name = [] 

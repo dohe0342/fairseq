8812c6e20430c4507a733317dec2393589cea658
dohe0342 <kimdohe1070@gmail.com>
2022-08-09 10:37:30 +0900

    from local

--- fairseq/trainer2.py
+++ fairseq/trainer2.py
@@ -611,8 +611,6 @@ class Trainer(object):
                 self.model.load_state_dict(
                     state["model"], strict=False, model_cfg=self.cfg.model
                 )
-                print(state["optimizer"])
-                print(state["optimizer2"])
                 # save memory for later steps
                 del state["model"]
                 if utils.has_parameters(self.get_criterion()):

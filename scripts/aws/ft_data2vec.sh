50e47b70d094fea4d8acaafba4eaa9895ac4bfdf
dohe0342 <kimdohe1070@gmail.com>
2022-08-09 18:50:26 +0900

    from local

--- scripts/aws/ft_data2vec.sh
+++ scripts/aws/ft_data2vec.sh
@@ -66,7 +66,7 @@ then
         fairseq-hydra-train \
             --config-dir /opt/ml/code/fairseq/examples/wav2vec/config/finetuning \
             --config-name base_100h_whale \
-            task.data=/home/work/workspace/LibriSpeech/manifests \
+            task.data=/opt/ml/code/LibriSpeech/manifests \
             model.w2v_path=/home/work/workspace/models/wav2vec_model/wav2vec_small.pt \
             checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 \
             +model.wavlm=true

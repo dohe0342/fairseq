23dcd428771dff1695a1de3be550f90a2538ed11
dohe0342 <kimdohe1070@gmail.com>
2022-08-14 00:03:24 +0900

    from local

--- scripts/whale2/ft_data2vec.sh
+++ scripts/whale2/ft_data2vec.sh
@@ -10,21 +10,21 @@ then
 		if [ $init -eq 0 ] 
 		then
 			fairseq-hydra-train \
-				--config-dir /home/work/workspace/fairseq/examples/wav2vec/config/finetuning \
+				--config-dir /home/work/workspace/fairseq_whale2/examples/wav2vec/config/finetuning \
 				--config-name base_960h_whale \
 				task.data=/home/work/workspace/LibriSpeech/manifests \
 				model.w2v_path=/home/work/workspace/models/wav2vec_model/wav2vec_small.pt \
-				checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 \
+				checkpoint.save_dir=/home/work/workspace/fairseq_whale2/scripts/whale2/outputs/$1 \
 				criterion._name=viewmaker \
 				+model.viewmaker=true \
 				+model.init_viewmaker=true 
 		else
 			fairseq-hydra-train \
-				--config-dir /home/work/workspace/fairseq/examples/wav2vec/config/finetuning \
+				--config-dir /home/work/workspace/fairseq_whale2/examples/wav2vec/config/finetuning \
 				--config-name base_960h_whale \
 				task.data=/home/work/workspace/LibriSpeech/manifests \
 				model.w2v_path=/home/work/workspace/models/wav2vec_model/wav2vec_small.pt \
-				checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 \
+				checkpoint.save_dir=/home/work/workspace/fairseq_whale2/scripts/whale2/outputs/$1 \
 				criterion._name=viewmaker \
 				+model.viewmaker=true 
 		fi

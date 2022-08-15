c514cfae8cd0c0700a8df3ee7ab1ca28db57642c
dohe0342 <kimdohe1070@gmail.com>
2022-08-15 23:57:54 +0900

    from local

--- fairseq/models/wav2vec/wav2vec2_asr.py
+++ fairseq/models/wav2vec/wav2vec2_asr.py
@@ -275,7 +275,7 @@ class Wav2VecCtc(BaseFairseqModel):
         elif cfg.wavlm:
             w2v_encoder = WavLMEncoder(cfg, len(task.target_dictionary))
         else:
-            w2v_encoder = Wav2VecEncoder(cfg, 32)#len(task.target_dictionary))
+            w2v_encoder = Wav2VecEncoder(cfg, len(task.target_dictionary))
 
         return cls(cfg, w2v_encoder)
 

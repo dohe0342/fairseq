2486bdb26e224d7676ad4f16e20b1f223133a1d5
dohe0342 <kimdohe1070@gmail.com>
2022-08-15 23:52:40 +0900

    from local

--- scripts/ant/eval_wav2vec_viterbi.sh
+++ scripts/ant/eval_wav2vec_viterbi.sh
@@ -4,7 +4,6 @@ do
 	CUDA_VISIBLE_DEVICES=$3 python /workspace/fairseq/examples/speech_recognition/infer.py \
 	  /workspace/LibriSpeech/manifests \
 	  --task audio_pretraining \
-	  --path /workspace/models/$model \
 	  --gen-subset $subset \
 	  --results-path /workspace/fairseq/examples/wav2vec/ctc_eval \
 	  --w2l-decoder viterbi \

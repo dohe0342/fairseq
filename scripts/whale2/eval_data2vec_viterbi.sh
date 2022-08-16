342bd3ba222e98e53be895a42c9380870ca293e8
dohe0342 <kimdohe1070@gmail.com>
2022-08-17 00:31:40 +0900

    from local

--- scripts/whale2/eval_data2vec_viterbi.sh
+++ scripts/whale2/eval_data2vec_viterbi.sh
@@ -14,7 +14,7 @@ do
 			dataset.gen_subset=$subset \
 			common_eval.path=/home/work/workspace/models/hubert_model/$model 
 	else
-		CUDA_VISIBLE_DEVICES=$2 python /home/work/workspace/fairseq/examples/speech_recognition/new/infer.py \
+		CUDA_VISIBLE_DEVICES=$2 python /home/work/workspace/fairseq_whale2/examples/speech_recognition/new/infer.py \
 			--config-dir /home/work/workspace/fairseq/examples/speech_recognition/new/conf \
 			--config-name infer \
 			task=audio_finetuning \

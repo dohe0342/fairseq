a0277d6b651ad9e29b4e7e1760b3e03c6524e851
dohe0342 <kimdohe1070@gmail.com>
2022-09-13 13:22:28 +0900

    from local

--- scripts/whale/eval_kenlm.sh
+++ scripts/whale/eval_kenlm.sh
@@ -1,7 +1,11 @@
 for subset in "dev-clean" "dev-other" "test-clean" "test-other"
 do
-	python /home/work/workspace/fairseq/examples/speech_recognition/new/infer.py --config-dir examples/speech_recognition/new/conf \
-	--config-name infer task=audio_finetuning task.data=/path/to/manifests common.user_dir=examples/data2vec \
+	python /home/work/workspace/fairseq/examples/speech_recognition/new/infer.py \
+		--config-dir examples/speech_recognition/new/conf \
+		--config-name infer \
+		task=audio_finetuning \
+		task.data=/home/work/workspace/LibriSpeech/manifests \
+		common.user_dir=/home/work/workspace/fairseq/examples/data2vec \
 	task.labels=ltr decoding.type=kenlm \
 	decoding.lmweight=${lmweight} decoding.wordscore=${wordscore} decoding.silweight=${silscore} \
 	decoding.lexicon=/path/to/lexicon \

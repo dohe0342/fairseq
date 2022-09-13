16e541986cec75cc7aee236c2e40d6dbc944ef2a
dohe0342 <kimdohe1070@gmail.com>
2022-09-13 13:26:55 +0900

    from local

--- scripts/whale/eval_kenlm.sh
+++ scripts/whale/eval_kenlm.sh
@@ -6,10 +6,13 @@ do
 		task=audio_finetuning \
 		task.data=/home/work/workspace/LibriSpeech/manifests \
 		common.user_dir=/home/work/workspace/fairseq/examples/data2vec \
-	task.labels=ltr decoding.type=kenlm \
-	decoding.lmweight=${lmweight} decoding.wordscore=${wordscore} decoding.silweight=${silscore} \
-	decoding.lexicon=/path/to/lexicon \
-	decoding.lmpath=/path/to/lm decoding.unique_wer_file=True \
-	dataset.gen_subset=dev_clean,dev_other,test_clean,test_other \
-	common_eval.path=/path/to/checkpoint.pt decoding.beam=1500 distributed_training.distributed_world_size=${num_gpus}
+		task.labels=ltr \
+		decoding.type=kenlm \
+		decoding.lmweight=2.0 \ 
+		decoding.wordscore=-0.3 \
+		decoding.silweight=0.0 \
+		decoding.lexicon=/home/work/workspace/models/lm/librispeech-lexicon.txt \
+		decoding.lmpath=/path/to/lm decoding.unique_wer_file=True \
+		dataset.gen_subset=dev_clean,dev_other,test_clean,test_other \
+		common_eval.path=/path/to/checkpoint.pt decoding.beam=1500 distributed_training.distributed_world_size=${num_gpus}
 done

fb7fe9d5d527f81d74385f1342b856060adb2a3e
dohe0342 <kimdohe1070@gmail.com>
2022-09-13 13:27:12 +0900

    from local

--- scripts/whale/eval_kenlm.sh
+++ scripts/whale/eval_kenlm.sh
@@ -14,6 +14,6 @@ do
 		decoding.lexicon=/home/work/workspace/models/lm/librispeech-lexicon.txt \
 		decoding.lmpath=/path/to/lm \
 		decoding.unique_wer_file=False \
-		dataset.gen_subset=dev_clean,dev_other,test_clean,test_other \
+		dataset.gen_subset=$subset \
 		common_eval.path=/path/to/checkpoint.pt decoding.beam=1500 distributed_training.distributed_world_size=${num_gpus}
 done

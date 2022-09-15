4af0b2110eb54ffae43e8dc8e69e2458786d7d0f
dohe0342 <kimdohe1070@gmail.com>
2022-09-15 14:51:46 +0900

    from local

--- loss-landscape/model.py
+++ loss-landscape/model.py
@@ -28,6 +28,14 @@ class Wav2Vec2Ctc(nn.Module):
 
         self.blank_weight = cfg.blank_weight
         self.blank_mode = cfg.blank_mode
+        
+    
+    def overwrite_param(self, path):
+        model = torch.load(path)['model']
+        for n , p in self.w2v_encoder.named_parameters():
+            try: p.data = model[n]
+            except: print('model has no params named {n}!')
+            
 
     #FIXME: More clever way!
     @staticmethod

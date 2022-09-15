527d80e407ead659a09f5bebaa2e79d7a6ff4f2f
dohe0342 <kimdohe1070@gmail.com>
2022-09-15 18:37:21 +0900

    from local

--- loss-landscape/plot_surface.py
+++ loss-landscape/plot_surface.py
@@ -282,8 +282,8 @@ if __name__ == '__main__':
     mpi.barrier(comm)
     _, cfg, task = fairseq.checkpoint_utils.load_model_ensemble_and_task([args.model_file])
     
-    print(task.data)
-    print(task.subset)
+    print(task.cfg.data)
+    print(task.cfg.subset)
     task.load_dataset(
             'train-960',
             task_cfg=task.cfg,

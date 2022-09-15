23f7c1ab8ebefb04fa8f3e887173845d406d5869
dohe0342 <kimdohe1070@gmail.com>
2022-09-15 15:51:36 +0900

    from local

--- loss-landscape/plot_surface.py
+++ loss-landscape/plot_surface.py
@@ -258,7 +258,6 @@ if __name__ == '__main__':
     if rank == 0:
         setup_surface_file(args, surf_file, dir_file)
 
-    exit()
     # wait until master has setup the direction file and surface file
     mpi.barrier(comm)
     print('6')

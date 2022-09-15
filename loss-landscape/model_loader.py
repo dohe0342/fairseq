0735a257a06ed305b16090bb33b5ab52a5372c05
dohe0342 <kimdohe1070@gmail.com>
2022-09-15 14:26:03 +0900

    from local

--- loss-landscape/model_loader.py
+++ loss-landscape/model_loader.py
@@ -4,4 +4,7 @@ import cifar10.model_loader
 def load(dataset, model_name, model_file, data_parallel=False):
     if dataset == 'cifar10':
         net = cifar10.model_loader.load(model_name, model_file, data_parallel)
+
+    elif dataset == 'LibriSpeech':
+        net = 
     return net

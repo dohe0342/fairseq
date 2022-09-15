be1753437caebd74b0c1d9994a0c9d42fc9657a9
dohe0342 <kimdohe1070@gmail.com>
2022-09-15 23:50:30 +0900

    from local

--- loss-landscape/draw_landscape2.sh
+++ loss-landscape/draw_landscape2.sh
@@ -4,4 +4,4 @@ mpirun -n 8 python plot_surface.py \
     --x=-0.5:0.5:50 --y=-0.5:0.5:50 \
     --model_file /home/work/workspace/fairseq/scripts/whale/outputs/w2v_b_960h_mba3f/wav2vec_small_960h_mba.pt \
     --dir_type weights --xnorm filter --xignore biasbn --ynorm filter --yignore biasbn \
-    --plot --loss_name ctc --subset dev-clean 
+    --plot --loss_name ctc --subset test-clean 

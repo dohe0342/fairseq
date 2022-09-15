5ae57d26391b7d2c886a171e639dab12adcd571b
dohe0342 <kimdohe1070@gmail.com>
2022-09-15 21:45:48 +0900

    from local

--- loss-landscape/draw_landscape.sh
+++ loss-landscape/draw_landscape.sh
@@ -9,6 +9,6 @@ mpirun -n 8 python plot_surface.py \
 mpirun -n 8 python plot_surface.py \
 	--mpi --cuda --model wav2vec --dataset LibriSpeech \
 	--x=-0.5:0.5:50 --y=-0.5:0.5:50 \
-	--model_file /home/work/workspace/models/wav2vec_model/wav2vec_small_960h.pt \
+	--model_file /home/work/workspace/fairseq/scripts/whale/outputs/w2v_b_960h_mba3f/wav2vec_small_960h_mba.pt \
 	--dir_type weights --xnorm filter --xignore biasbn --ynorm filter --yignore biasbn \
 	--plot --loss_name ctc --subset train-960 

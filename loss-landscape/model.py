db7bd944beb4bb6c3302122d16b3a284116bce44
dohe0342 <kimdohe1070@gmail.com>
2022-09-15 15:11:06 +0900

    from local

--- loss-landscape/model.py
+++ loss-landscape/model.py
@@ -5,6 +5,26 @@ from fairseq.models.wav2vec.wav2vec2_asr import Wav2Vec2CtcConfig
 from fairseq.data.data_utils import lengths_to_padding_mask
 
 
+def load(path, data_parallel=True):
+    net = models[model_name]()
+    if data_parallel: # the model is saved in data paralle mode
+        net = torch.nn.DataParallel(net)
+
+    if model_file:
+        assert os.path.exists(model_file), model_file + " does not exist."
+        stored = torch.load(model_file, map_location=lambda storage, loc: storage)
+        if 'state_dict' in stored.keys():
+            net.load_state_dict(stored['state_dict'])
+        else:
+            net.load_state_dict(stored)
+
+    if data_parallel: # convert the model back to the single GPU version
+        net = net.module
+
+    net.eval()
+    return net 
+
+
 class Wav2Vec2Ctc(nn.Module):
     def __init__(
             self,

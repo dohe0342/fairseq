c50a40cd68d966868509f4d577d340b4fe27f27d
dohe0342 <kimdohe1070@gmail.com>
2022-09-15 15:15:03 +0900

    from local

--- loss-landscape/model_loader.py
+++ loss-landscape/model_loader.py
@@ -7,8 +7,8 @@ def load(dataset, model_name=None, model_file=None, data_parallel=False):
     if dataset == 'cifar10':
         net = cifar10.model_loader.load(model_name, model_file, data_parallel)
     elif dataset == 'LibriSpeech':
-        net = w2v_load('/home/work/workspace/models/wav2vec_model/wav2vec_small_100h.pt', data_parallel) 
+        net = w2v_load(model_name, data_parallel) 
     return net
 
 if __name__ == '__main__':
-    load('LibriSpeech', 
+    load('LibriSpeech', '/home/work/workspace/models/wav2vec_model/wav2vec_small_100h.pt'

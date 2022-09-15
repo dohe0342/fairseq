569efca8a4e1da11ecfb3251252bfbfe6332bc6b
dohe0342 <kimdohe1070@gmail.com>
2022-09-15 15:15:06 +0900

    from local

--- loss-landscape/model_loader.py
+++ loss-landscape/model_loader.py
@@ -11,4 +11,4 @@ def load(dataset, model_name=None, model_file=None, data_parallel=False):
     return net
 
 if __name__ == '__main__':
-    load('LibriSpeech', '/home/work/workspace/models/wav2vec_model/wav2vec_small_100h.pt'
+    load('LibriSpeech', '/home/work/workspace/models/wav2vec_model/wav2vec_small_100h.pt')

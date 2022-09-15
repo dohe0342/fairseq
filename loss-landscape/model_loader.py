a274e2edb928545fdb87d0b4145cfbe52ed2dfcb
dohe0342 <kimdohe1070@gmail.com>
2022-09-15 14:27:50 +0900

    from local

--- loss-landscape/model_loader.py
+++ loss-landscape/model_loader.py
@@ -1,11 +1,13 @@
 import os
 import cifar10.model_loader
-from fairseq.models.wav2vec.wav2vec2_asr import wav2vec_ctc
+from fairseq.models.wav2vec.wav2vec2_asr import Wav2Vec2CtcConfig
+from fairseq.models.wav2vec.wav2vec2_asr import Wav2Vec2CtcConfig
 
 def load(dataset, model_name, model_file, data_parallel=False):
     if dataset == 'cifar10':
         net = cifar10.model_loader.load(model_name, model_file, data_parallel)
 
-    elif dataset == 'LibriSpeech':
-        net = 
     return net
+
+if __name__ == '__main__':
+    

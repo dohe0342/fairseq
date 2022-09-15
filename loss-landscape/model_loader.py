75be02020db495564740857e72980e5d149bb274
dohe0342 <kimdohe1070@gmail.com>
2022-09-15 14:27:55 +0900

    from local

--- loss-landscape/model_loader.py
+++ loss-landscape/model_loader.py
@@ -1,6 +1,6 @@
 import os
 import cifar10.model_loader
-from fairseq.models.wav2vec.wav2vec2_asr import Wav2Vec2CtcConfig
+from fairseq.models.wav2vec.wav2vec2_asr import Wav2VecEncoder
 from fairseq.models.wav2vec.wav2vec2_asr import Wav2Vec2CtcConfig
 
 def load(dataset, model_name, model_file, data_parallel=False):

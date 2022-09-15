4c1addb0b2e40a6d986e50c7ab1bb1f4c4e3c988
dohe0342 <kimdohe1070@gmail.com>
2022-09-15 14:56:30 +0900

    from local

--- loss-landscape/model.py
+++ loss-landscape/model.py
@@ -34,6 +34,8 @@ class Wav2Vec2Ctc(nn.Module):
         for n , p in self.w2v_encoder.named_parameters():
             try: p.data = model[n]
             except: print(f'model has no params named {n}!')
+        for n in model:
+            print(n)
         
 
     #FIXME: More clever way!

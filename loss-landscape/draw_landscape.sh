654be2cf2bddd3e06470517d224e5cf40dc78abf
dohe0342 <kimdohe1070@gmail.com>
2022-09-15 21:10:44 +0900

    from local

--- loss-landscape/draw_landscape.sh
+++ loss-landscape/draw_landscape.sh
@@ -1,2 +1,2 @@
 git pull
-mpirun -n 8 python plot_surface.py --mpi --cuda --model resnet56 --dataset LibriSpeech --x=-1:1:51 --y=-1:1:51 --model_file /home/work/workspace/fairseq/scripts/whale/outputs/w2v_b_960h_mba3f/checkpoint_best.pt --dir_type weights --xnorm filter --xignore biasbn --ynorm filter --yignore biasbn  --plot --loss_name ctc --subset dev-clean 
+mpirun -n 8 python plot_surface.py --mpi --cuda --model resnet56 --dataset LibriSpeech --x=-1:1:10 --y=-1:1:10 --model_file /home/work/workspace/fairseq/scripts/whale/outputs/w2v_b_960h_mba3f/checkpoint_best.pt --dir_type weights --xnorm filter --xignore biasbn --ynorm filter --yignore biasbn  --plot --loss_name ctc --subset dev-clean 

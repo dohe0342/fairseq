a5e75642c8c55d52d91091909a16a8f5a82e302b
dohe0342 <kimdohe1070@gmail.com>
2022-09-15 18:59:16 +0900

    from local

--- loss-landscape/plot_surface.py
+++ loss-landscape/plot_surface.py
@@ -310,9 +310,6 @@ if __name__ == '__main__':
                 disable_iterator_cache=False,
             ).next_epoch_itr(shuffle=False)
     
-    for sample in trainloader:
-        print(sample["id"].get_device())
-        exit()
     #--------------------------------------------------------------------------
     # Start the computation
     #--------------------------------------------------------------------------

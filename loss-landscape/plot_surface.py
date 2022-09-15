6c9a614cda730ae9418b4d8528197a0a78db051d
dohe0342 <kimdohe1070@gmail.com>
2022-09-15 19:18:29 +0900

    from local

--- loss-landscape/plot_surface.py
+++ loss-landscape/plot_surface.py
@@ -109,8 +109,7 @@ def crunch(surf_file, net, w, s, d, dataloader, loss_key, acc_key, comm, rank, a
     if args.loss_name == 'mse':
         criterion = nn.MSELoss()
     elif args.loss_name == 'ctc':
-        cfg = CtcCriterionConfig()
-        criterion = CtcCriterion
+        criterion = CtcCriterion(cfg.criterion, task)
 
     # Loop over all uncalculated loss values
     for count, ind in enumerate(inds):

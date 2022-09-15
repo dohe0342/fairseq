f96829429c0074734c21808889d862d6dc14a82a
dohe0342 <kimdohe1070@gmail.com>
2022-09-15 19:25:16 +0900

    from local

--- loss-landscape/plot_surface.py
+++ loss-landscape/plot_surface.py
@@ -253,7 +253,6 @@ if __name__ == '__main__':
     #net = model_loader.load(args.dataset, args.model, args.model_file)
     models, cfg, task = fairseq.checkpoint_utils.load_model_ensemble_and_task([args.model_file])
     net = models[0]
-    print(net)
     w = net_plotter.get_weights(net) # initial parameters
     s = copy.deepcopy(net.state_dict()) # deepcopy since state_dict are references
     if args.ngpu > 1:

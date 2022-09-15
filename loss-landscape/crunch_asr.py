2acd684a6186318e12b82cb1bc7393a9345648cc
dohe0342 <kimdohe1070@gmail.com>
2022-09-15 19:00:19 +0900

    from local

---
+++ loss-landscape/crunch_asr.py
@@ -0,0 +1,86 @@
+import numpy as np
+
+
+def crunch(surf_file, net, w, s, d, dataloader, loss_key, acc_key, comm, rank, args):
+    """
+        Calculate the loss values and accuracies of modified models in parallel
+        using MPI reduce.
+    """
+
+    f = h5py.File(surf_file, 'r+' if rank == 0 else 'r')
+    losses, accuracies = [], []
+    xcoordinates = f['xcoordinates'][:]
+    ycoordinates = f['ycoordinates'][:] if 'ycoordinates' in f.keys() else None
+
+    if loss_key not in f.keys():
+        shape = xcoordinates.shape if ycoordinates is None else (len(xcoordinates),len(ycoordinates))
+        losses = -np.ones(shape=shape)
+        accuracies = -np.ones(shape=shape)
+        if rank == 0:
+            f[loss_key] = losses
+            f[acc_key] = accuracies
+    else:
+        losses = f[loss_key][:]
+        accuracies = f[acc_key][:]
+
+    # Generate a list of indices of 'losses' that need to be filled in.
+    # The coordinates of each unfilled index (with respect to the direction vectors
+    # stored in 'd') are stored in 'coords'.
+    inds, coords, inds_nums = scheduler.get_job_indices(losses, xcoordinates, ycoordinates, comm)
+
+    print('Computing %d values for rank %d'% (len(inds), rank))
+    start_time = time.time()
+    total_sync = 0.0
+
+    criterion = nn.CrossEntropyLoss()
+    if args.loss_name == 'mse':
+        criterion = nn.MSELoss()
+
+    # Loop over all uncalculated loss values
+    for count, ind in enumerate(inds):
+        # Get the coordinates of the loss value being calculated
+        coord = coords[count]
+
+        # Load the weights corresponding to those coordinates into the net
+        if args.dir_type == 'weights':
+            net_plotter.set_weights(net.module if args.ngpu > 1 else net, w, d, coord)
+        elif args.dir_type == 'states':
+            net_plotter.set_states(net.module if args.ngpu > 1 else net, s, d, coord)
+
+        # Record the time to compute the loss value
+        loss_start = time.time()
+        loss, acc = evaluation.eval_loss(net, criterion, dataloader, args.cuda)
+        loss_compute_time = time.time() - loss_start
+
+        # Record the result in the local array
+        losses.ravel()[ind] = loss
+        accuracies.ravel()[ind] = acc
+
+        # Send updated plot data to the master node
+        syc_start = time.time()
+        losses     = mpi.reduce_max(comm, losses)
+        accuracies = mpi.reduce_max(comm, accuracies)
+        syc_time = time.time() - syc_start
+        total_sync += syc_time
+
+        # Only the master node writes to the file - this avoids write conflicts
+        if rank == 0:
+            f[loss_key][:] = losses
+            f[acc_key][:] = accuracies
+            f.flush()
+
+        print('Evaluating rank %d  %d/%d  (%.1f%%)  coord=%s \t%s= %.3f \t%s=%.2f \ttime=%.2f \tsync=%.2f' % (
+                rank, count, len(inds), 100.0 * count/len(inds), str(coord), loss_key, loss,
+                acc_key, acc, loss_compute_time, syc_time))
+
+    # This is only needed to make MPI run smoothly. If this process has less work than
+    # the rank0 process, then we need to keep calling reduce so the rank0 process doesn't block
+    for i in range(max(inds_nums) - len(inds)):
+        losses = mpi.reduce_max(comm, losses)
+        accuracies = mpi.reduce_max(comm, accuracies)
+
+    total_time = time.time() - start_time
+    print('Rank %d done!  Total time: %.2f Sync: %.2f' % (rank, total_time, total_sync))
+
+    f.close()
+

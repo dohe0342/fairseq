fe45256166e0b29ac887fb36e8dd2fd088551219
dohe0342 <kimdohe1070@gmail.com>
2022-09-15 14:33:33 +0900

    from local

--- loss-landscape/model.py
+++ loss-landscape/model.py
@@ -1,5 +1,7 @@
 from fairseq.models.wav2vec.wav2vec2_asr import Wav2VecEncoder
 from fairseq.models.wav2vec.wav2vec2_asr import Wav2Vec2CtcConfig
+from fairseq.data.data_utils import lengths_to_padding_mask
+
 
 class Wav2Vec2Ctc(nn.Module):
     def __init__(

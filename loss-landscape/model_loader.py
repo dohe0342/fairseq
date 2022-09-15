dd021c70db55f571c64034e9adedf4c5365253e0
dohe0342 <kimdohe1070@gmail.com>
2022-09-15 15:13:32 +0900

    from local

--- loss-landscape/model_loader.py
+++ loss-landscape/model_loader.py
@@ -1,36 +1,16 @@
 import os
 import cifar10.model_loader
-from model import load
+from model import w2v_load
 
 
 def load(dataset, model_name, model_file, data_parallel=False):
     if dataset == 'cifar10':
         net = cifar10.model_loader.load(model_name, model_file, data_parallel)
-
+    elif dataset == 'LibriSpeech':
+        net = 
     return net
 
 if __name__ == '__main__':
     net = Wav2Vec2Ctc()
     net.overwrite_param('/home/work/workspace/models/wav2vec_model/wav2vec_small_100h.pt')
     print('overwrite done!')
-
-
-def load(model_name, model_file=None, data_parallel=False):
-    net = models[model_name]()
-    if data_parallel: # the model is saved in data paralle mode
-        net = torch.nn.DataParallel(net)
-
-    if model_file:
-        assert os.path.exists(model_file), model_file + " does not exist."
-        stored = torch.load(model_file, map_location=lambda storage, loc: storage)
-        if 'state_dict' in stored.keys():
-            net.load_state_dict(stored['state_dict'])
-        else:
-            net.load_state_dict(stored)
-
-    if data_parallel: # convert the model back to the single GPU version
-        net = net.module
-
-    net.eval()
-    return net 
-

751481de8899d63c32db5ff18548bcc060ecd307
dohe0342 <kimdohe1070@gmail.com>
2022-09-15 14:26:34 +0900

    from local

--- loss-landscape/model_loader.py
+++ loss-landscape/model_loader.py
@@ -1,5 +1,6 @@
 import os
 import cifar10.model_loader
+from fairseq.models.wav2vec.wav2vec2_asr import wav2vec_ctc
 
 def load(dataset, model_name, model_file, data_parallel=False):
     if dataset == 'cifar10':

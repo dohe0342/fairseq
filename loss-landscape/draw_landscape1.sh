a46337d498962b4abee3bdd8569cdcc6cd64f267
dohe0342 <kimdohe1070@gmail.com>
2022-09-16 14:44:02 +0900

    from local

--- loss-landscape/draw_landscape1.sh
+++ loss-landscape/draw_landscape1.sh
@@ -1,7 +1,7 @@
 git pull
 mpirun -n 4 python plot_surface.py \
 	--mpi --cuda --model wav2vec --dataset LibriSpeech \
-	--x=-0.2:0.2:75 --y=-0.2:0.2:75 \
+	--x=-0.2:0.2:5 --y=-0.2:0.2:5 \
 	--model_file /home/work/workspace/models/wav2vec_model/wav2vec_small_960h.pt \
 	--dir_type weights --xnorm filter --xignore biasbn --ynorm filter --yignore biasbn \
 	--plot --loss_name ctc --subset $1 --root_rank $2

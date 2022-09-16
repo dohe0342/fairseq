b9a7ce564fbbb897247b18015216b3c1a95fcb28
dohe0342 <kimdohe1070@gmail.com>
2022-09-16 10:58:54 +0900

    from local

--- loss-landscape/draw_landscape2.sh
+++ loss-landscape/draw_landscape2.sh
@@ -1,6 +1,6 @@
 git pull
 mpirun -n 8 python plot_surface.py \
-    --mpi --cuda --model viewmaker --dataset LibriSpeech \
+    --mpi --cuda --model ctc --dataset LibriSpeech \
     --x=-0.5:0.5:50 --y=-0.5:0.5:50 \
     --model_file /home/work/workspace/fairseq/scripts/whale/outputs/w2v_b_960h_mba3f/wav2vec_small_960h_mba.pt \
     --dir_type weights --xnorm filter --xignore biasbn --ynorm filter --yignore biasbn \

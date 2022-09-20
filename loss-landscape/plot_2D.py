163d6b0feacf77f60de6661af6d4d0c03dc614cb
dohe0342 <kimdohe1070@gmail.com>
2022-09-20 12:42:01 +0900

    from local

--- loss-landscape/plot_2D.py
+++ loss-landscape/plot_2D.py
@@ -69,7 +69,7 @@ def plot_2d_contour(surf_file, surf_name='train_loss', vmin=0.1, vmax=10, vlevel
     # --------------------------------------------------------------------
     fig = plt.figure()
     ax = Axes3D(fig)
-    surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+    surf = ax.plot_surface(Y, X, Z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
     fig.colorbar(surf, shrink=0.5, aspect=5)
     fig.savefig(surf_file + '_' + surf_name + '_3dsurface.pdf', dpi=300,
                 bbox_inches='tight', format='pdf')

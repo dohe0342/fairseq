ab15c504a9c12eefc90888390049e0a40f3ae3aa
dohe0342 <kimdohe1070@gmail.com>
2022-09-20 13:44:24 +0900

    from local

--- loss-landscape/plot_multi_2D.py
+++ loss-landscape/plot_multi_2D.py
@@ -93,6 +93,7 @@ def plot_2d_contour(surf_file1, surf_file2, surf_name='train_loss', vmin=0.1, vm
     # Plot 3D surface
     # --------------------------------------------------------------------
     #fig = plt.figure()
+    figure, axis = plt.subplots(1, 2)
 
     ax = Axes3D(fig, elev=20)
     ax.set_zlim(zmin=0, zmax=730)

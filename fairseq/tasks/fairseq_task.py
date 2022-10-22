b7cd131b7d7169a8d923b0a85c1e45811a2c3f9c
dohe0342 <kimdohe1070@gmail.com>
2022-10-22 14:51:18 +0900

    from local

--- fairseq/tasks/fairseq_task.py
+++ fairseq/tasks/fairseq_task.py
@@ -598,6 +598,8 @@ class FairseqTask(object):
                     ## loss[0] = [original ctc loss, perturb ctc loss]
                     ## loss[1] = MSE loss between original, perturb cnn feat
                     ## hard coding for freeze updates
+
+                    '''
                     if update_num <= -1:
                         optimizer[1].backward(loss[1])
                     elif -1 < update_num <= 10000:
@@ -619,13 +621,13 @@ class FairseqTask(object):
                     #lambda_ = -0.00001*(1+torch.cos(torch.tensor(update_num)*math.pi/34090.)) ## for wav2vec2 vox 960h train ##ver1
                     #lambda_ = -0.00001*(1+torch.cos(torch.tensor(update_num)*math.pi/34090.))-0.00001 ## for wav2vec2 base 960h train
                     #lambda_ = -0.00003 ## for wav2vec2 vox 960h train
-                    lambda_ = -0.0001 ## for wav2vec2 vox 960h train
+                    #lambda_ = -0.0001 ## for wav2vec2 vox 960h train
                     #lambda_ = -0.00001*(1+torch.cos(torch.tensor(update_num)*math.pi/1810.)) ## for hubert
-                    optimizer[1].backward(lambda_*loss[0][1]+loss[1])
+                    #optimizer[1].backward(lambda_*loss[0][1]+loss[1])
+                    optimizer[1].backward(loss[1])
                     #loss[0][1] /= sample_size
                     #optimizer[0].backward((loss[0][0] + loss[0][1]), retain_graph=True)
                     #optimizer[0].backward((loss[0][0] + loss[0][1]))
-                    '''
                     '''
                     optimizer[0].backward(loss[0][0], retain_graph=True)
                     del loss[0][0]

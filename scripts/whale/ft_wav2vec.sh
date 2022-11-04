87cbdde9dca3c30d4ffb33309893778ea8df8e13
dohe0342 <kimdohe1070@gmail.com>
2022-11-04 16:42:50 +0900

    from local

--- scripts/whale/ft_wav2vec.sh
+++ scripts/whale/ft_wav2vec.sh
@@ -8,6 +8,4 @@ fairseq-hydra-train \
 	task.data=/home/work/workspace/LibriSpeech/manifests 
 	model.w2v_path=/home/work/workspace/models/wav2vec_model/wav2vec_small.pt \
 	checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 \
-	criterion._name=viewmaker \
-	+model.viewmaker=true \
-	+model.init_viewmaker=true 
+	criterion._name=ctc

4f8730fa8f115c0e335fdabb30ca74fc84e0262b
dohe0342 <kimdohe1070@gmail.com>
2022-11-07 18:56:49 +0900

    from local

--- scripts/whale/eval_data2vec_viterbi.sh
+++ scripts/whale/eval_data2vec_viterbi.sh
@@ -25,7 +25,7 @@ do
 			decoding.type=viterbi \
 			decoding.unique_wer_file=False \
 			dataset.gen_subset=$subset \
-			common_eval.path=$model\\$3\\$4 \
+			common_eval.path=$model//$3//$4 \
 			distributed_training.distributed_world_size=1 
 	fi
 	echo ""

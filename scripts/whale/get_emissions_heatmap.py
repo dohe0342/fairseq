8d1469229c3ad5c6fc8015bb0a9a0659571c20d0
dohe0342 <kimdohe1070@gmail.com>
2022-11-07 18:03:38 +0900

    from local

--- scripts/whale/get_emissions_heatmap.py
+++ scripts/whale/get_emissions_heatmap.py
@@ -6,6 +6,7 @@ from tqdm import tqdm
 filelist = glob.glob('./test-clean-part_emissions/*.npy')
 for filename in tqdm(filelist):
     emissions = np.load(filename)
+    emissions = emissions.transpose((1, 2, 0))
     model_number = filename.split('/')[-1].split('_')[0].strip()
     for b, emission in tqdm(enumerate(emissions), leave=False):
         plt.matshow(emission)

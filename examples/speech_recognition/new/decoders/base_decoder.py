37fc7612653e03da2cc748af29ca278c4a68b1fe
dohe0342 <kimdohe1070@gmail.com>
2022-11-07 17:43:47 +0900

    from local

--- examples/speech_recognition/new/decoders/base_decoder.py
+++ examples/speech_recognition/new/decoders/base_decoder.py
@@ -35,6 +35,7 @@ class BaseDecoder:
             k: v for k, v in sample["net_input"].items() if k != "prev_output_tokens"
         }
         emissions = self.get_emissions(models, encoder_input)
+        print(emissions.keys())
         #return (self.decode(emissions[0]), emissions[1], emissions[2])
         return self.decode(emissions)
 

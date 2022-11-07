2750f05a1561ff13b06739b332546885ee348a74
dohe0342 <kimdohe1070@gmail.com>
2022-11-07 17:59:33 +0900

    from local

--- scripts/whale/get_emissions_heatmap.py
+++ scripts/whale/get_emissions_heatmap.py
@@ -3,7 +3,7 @@ import numpy as np
 import glob
 
 filelist = glob.glob('./test-clean-part_emissions/*.npy')
-for filname in filelist:
+for filename in filelist:
     emissions = np.load(filename)
     model_number = filename.split('/')[-1].split('_')[0].strip()
     for b, emission in enumerate(emissions):

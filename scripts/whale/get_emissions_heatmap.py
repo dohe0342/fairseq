6474d6797f2bfeb38e27a85c174802bf9210028e
dohe0342 <kimdohe1070@gmail.com>
2022-11-07 17:58:34 +0900

    from local

--- scripts/whale/get_emissions_heatmap.py
+++ scripts/whale/get_emissions_heatmap.py
@@ -6,7 +6,7 @@ filelist = glob.glob('./test-clean-part_emissions/*.npy')
 for filname in filelist:
     emissions = np.load(filename)
     model_number = filename.split('/')[-1].split('_')[0].strip()
-    for emission in emissions:
+    for b, emission in enumerate(emissions):
         plt.matshow(emission)
         plt.colorbar()
-        plt.savefig('fig1.png', dpi=300)
+        plt.savefig(f'{', dpi=300)

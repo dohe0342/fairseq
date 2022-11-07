16516fe8ca030556d017f5b819d916126f244a05
dohe0342 <kimdohe1070@gmail.com>
2022-11-07 18:00:58 +0900

    from local

--- scripts/whale/get_emissions_heatmap.py
+++ scripts/whale/get_emissions_heatmap.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import glob
+from tqdm import tqdm
 
 filelist = glob.glob('./test-clean-part_emissions/*.npy')
-for filename in filelist:
+for filename in tqdm(filelist):
     emissions = np.load(filename)
     model_number = filename.split('/')[-1].split('_')[0].strip()
-    for b, emission in enumerate(emissions):
+    for b, emission in tqdm(enumerate(emissions), leave=False):
         plt.matshow(emission)
         plt.colorbar()
         plt.savefig(f'./test-clean-part_emissions/{model_number}_{str(b).zfill(2)}.png', dpi=300)

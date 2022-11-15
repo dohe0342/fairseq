88f9246d445ccca81574c6da714783ba7c9f17ae
dohe0342 <kimdohe1070@gmail.com>
2022-11-16 01:20:27 +0900

    from local

---
+++ scripts/whale/ft_data2vec.sh
@@ -0,0 +1,9 @@
+fairseq-hydra-train \
+	--config-dir /home/work/workspace/fairseq/examples/wav2vec/config/finetuning \
+	--config-name base_100h_whale \
+	task.data=/home/work/workspace/LibriSpeech/manifests \
+	task.normalize=true \
+	model.w2v_path=/home/work/workspace/models/data2vec_model/audio_base_ls.pt \
+	criterion._name=viewmaker \
+	checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 \
+	+model.viewmaker=true

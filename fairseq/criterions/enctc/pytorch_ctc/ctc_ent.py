633e1f89a7a3cea50a8b04cdd25c05380afb4fc8
dohe0342 <kimdohe1070@gmail.com>
2022-11-19 10:37:22 +0900

    from local

--- fairseq/criterions/enctc/pytorch_ctc/ctc_ent.py
+++ fairseq/criterions/enctc/pytorch_ctc/ctc_ent.py
@@ -226,5 +226,5 @@ if __name__ == '__main__':
     os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   # see issue #152
     os.environ["CUDA_VISIBLE_DEVICES"]=sys.argv[1]
     alpha = float(sys.argv[1])
-    print(f'_________alpha________')
+    print(f'_________alpha_________')
     test_seg_ctc(use_mine=True, use_log=True, alpha=alpha)

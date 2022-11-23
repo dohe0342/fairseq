55c848aa8fcc5b08fb0d83eaff321d56b05599df
dohe0342 <kimdohe1070@gmail.com>
2022-11-23 15:02:14 +0900

    from local

--- scripts/whale/ft_data2vec.sh
+++ scripts/whale/ft_data2vec.sh
@@ -7,4 +7,4 @@ fairseq-hydra-train \
 	task.normalize=true \
 	model.w2v_path=/home/work/workspace/models/data2vec_model/audio_base_ls.pt \
 	criterion._name=ctc \
-	checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 \
+	checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 

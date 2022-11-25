11e8e111149256a4dba9fd7bf1275a72fdfe088b
dohe0342 <kimdohe1070@gmail.com>
2022-11-25 13:34:06 +0900

    from local

--- scripts/ant/eval_data2vec_kenlm.sh
+++ scripts/ant/eval_data2vec_kenlm.sh
@@ -12,7 +12,7 @@ do
 		task.data=/workspace/LibriSpeech/manifests \
 		task.labels=ltr \
 		decoding.type=kenlm \
-		decoding.lmweight=0. decoding.wordscore=-0. decoding.silweight=0 \
+		decoding.lmweight=2. decoding.wordscore=-1. decoding.silweight=0 \
 		decoding.lmpath=/workspace/models/lm_model/4-gram.bin \
 		decoding.unique_wer_file=false \
 		dataset.gen_subset=$subset \

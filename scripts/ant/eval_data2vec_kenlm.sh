82631c2e45a59b41f1775e98be0c7a48968c8b80
dohe0342 <kimdohe1070@gmail.com>
2022-11-25 13:33:55 +0900

    from local

--- scripts/ant/eval_data2vec_kenlm.sh
+++ scripts/ant/eval_data2vec_kenlm.sh
@@ -13,7 +13,6 @@ do
 		task.labels=ltr \
 		decoding.type=kenlm \
 		decoding.lmweight=0. decoding.wordscore=-0. decoding.silweight=0 \
-		decoding.lexicon=/workspace/models/lm_model/librispeech_lexicon.lst \
 		decoding.lmpath=/workspace/models/lm_model/4-gram.bin \
 		decoding.unique_wer_file=false \
 		dataset.gen_subset=$subset \
@@ -22,3 +21,4 @@ do
 	   	decoding.beam=1000 \
 		distributed_training.distributed_world_size=1
 done
+#decoding.lexicon=/workspace/models/lm_model/librispeech_lexicon.lst \

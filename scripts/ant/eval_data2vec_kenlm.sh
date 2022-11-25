39a9f645009b2e1cbf90217dc8dc96c9da20316b
dohe0342 <kimdohe1070@gmail.com>
2022-11-25 15:25:35 +0900

    from local

--- scripts/ant/eval_data2vec_kenlm.sh
+++ scripts/ant/eval_data2vec_kenlm.sh
@@ -19,6 +19,6 @@ do
 		dataset.gen_subset=$subset \
 		common_eval.path=/workspace/models/wav2vec_model/wav2vec_small_100h.pt \
 		common_eval.quiet=true \
-	   	decoding.beam=50 \
+	   	decoding.beam=100 \
 		distributed_training.distributed_world_size=1
 done

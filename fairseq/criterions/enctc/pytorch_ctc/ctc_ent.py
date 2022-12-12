f658248bbf4cc4e69a2a091413a8f5cabfdd2dbe
dohe0342 <kimdohe1070@gmail.com>
2022-12-12 16:54:25 +0900

    from local

--- fairseq/criterions/enctc/pytorch_ctc/ctc_ent.py
+++ fairseq/criterions/enctc/pytorch_ctc/ctc_ent.py
@@ -78,7 +78,8 @@ def ctc_ent_loss(pred, pred_len, token, token_len, blank=0):
     alpha = collect_label(alphas)
     beta = collect_label(betas)
 
-    H = -beta/alpha + T.log(alpha+eps)
+    #H = -beta/alpha + T.log(alpha+eps)
+    H = T.log(beta+eps)
     costs = -T.log(alpha+eps)
     return H, costs
 

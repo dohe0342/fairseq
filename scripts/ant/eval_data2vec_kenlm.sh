570985150caa5fbbbf850a32cab3ef70581af094
dohe0342 <kimdohe1070@gmail.com>
2022-12-14 01:27:00 +0900

    from local

--- scripts/ant/eval_data2vec_kenlm.sh
+++ scripts/ant/eval_data2vec_kenlm.sh
@@ -12,13 +12,13 @@ do
 		task.data=/workspace/LibriSpeech/manifests \
 		task.labels=ltr \
 		decoding.type=kenlm \
-		decoding.lmweight=0. decoding.wordscore=0. decoding.silweight=0 \
+		decoding.lmweight=2. decoding.wordscore=-0.2 decoding.silweight=0 \
 		decoding.lmpath=/workspace/models/lm_model/4-gram.bin \
 		decoding.lexicon=/workspace/models/lm_model/librispeech_lexicon.lst \
 		decoding.unique_wer_file=false \
 		dataset.gen_subset=$subset \
 		common_eval.path=/workspace/models/wav2vec_model/wav2vec_small_960h.pt \
 		common_eval.quiet=true \
-	   	decoding.beam=3000 \
+	   	decoding.beam=5000 \
 		distributed_training.distributed_world_size=1
 done

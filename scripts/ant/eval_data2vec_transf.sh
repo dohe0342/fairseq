80922ea05cc5e8b0a2c984c3c21b2d7319529c95
dohe0342 <kimdohe1070@gmail.com>
2023-01-06 15:01:25 +0900

    from local

--- scripts/ant/eval_data2vec_transf.sh
+++ scripts/ant/eval_data2vec_transf.sh
@@ -19,6 +19,6 @@ do
 		dataset.gen_subset=$subset \
 		common_eval.path=/workspace/models/wav2vec_model/wav2vec_small_960h.pt \
 		common_eval.quiet=true \
-	   	decoding.beam=5000 \
+	   	decoding.beam=5 \
 		distributed_training.distributed_world_size=1
 done

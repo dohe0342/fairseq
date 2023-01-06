ab71ea117569711ebf3e1cd8ef36b6ae99c82ebb
dohe0342 <kimdohe1070@gmail.com>
2023-01-06 15:18:19 +0900

    from local

--- examples/speech_recognition/new/decoders/flashlight_decoder.py
+++ examples/speech_recognition/new/decoders/flashlight_decoder.py
@@ -250,6 +250,7 @@ class FairseqLM(LM):
             res = self.model(prefix.cuda(), incremental_state=incremental_state)
             print(res[0])
             probs = self.model.get_normalized_probs(res, log_probs=True, sample=None)
+            print(res[0].size())
             print(probs[0, -1])
 
         if incremental_state is not None:

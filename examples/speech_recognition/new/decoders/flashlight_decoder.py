139e673ca7c6097e1a4f27596376f166a69461c7
dohe0342 <kimdohe1070@gmail.com>
2023-01-13 11:11:54 +0900

    from local

--- examples/speech_recognition/new/decoders/flashlight_decoder.py
+++ examples/speech_recognition/new/decoders/flashlight_decoder.py
@@ -413,8 +413,6 @@ class FairseqLMDecoder(BaseDecoder):
                     word_idx = self.word_dict.index(word)
                     _, score = self.lm.score(start_state, word_idx, no_cache=True)
 
-                    print(word_idx, score)
-
                 for spelling in spellings:
                     spelling_idxs = [tgt_dict.index(token) for token in spelling]
                     assert (

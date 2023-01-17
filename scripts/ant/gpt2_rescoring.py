6706f748d16b453c0126f50fcad4f86b659cd1c2
dohe0342 <kimdohe1070@gmail.com>
2023-01-17 15:02:16 +0900

    from local

--- scripts/ant/gpt2_rescoring.py
+++ scripts/ant/gpt2_rescoring.py
@@ -38,7 +38,7 @@ class GPT2Decoder():
 
 
 if __name__ == "__main__":
-    decoder = GPT2Decoder(lm_weight=0.5)
+    decoder = GPT2Decoder(lm_weight=0.05)
     
     f = open('./dev-clean_w2v-b-100h_hypo.txt', 'r').readlines()
     score_dict = {}

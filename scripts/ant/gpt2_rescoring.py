52450fefc510cd6f05c0961f91ced15d8bb1f65b
dohe0342 <kimdohe1070@gmail.com>
2023-01-17 13:24:19 +0900

    from local

--- scripts/ant/gpt2_rescoring.py
+++ scripts/ant/gpt2_rescoring.py
@@ -18,7 +18,7 @@ class GPT2Decoder():
     def score(self, sentence):
         encodings = self.tokenizer(sentence, return_tensors="pt")
         encodings = encodings.to(self.device)
-        print(encodings["input_ids"].size())
+        #print(encodings["input_ids"].size())
         input_ids = torch.cat([self.eos, encodings["input_ids"]])
         
         score_list = []

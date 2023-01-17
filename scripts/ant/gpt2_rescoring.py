83a66a3540cab22b7ab385994aac1e811e5cc76c
dohe0342 <kimdohe1070@gmail.com>
2023-01-17 15:03:59 +0900

    from local

--- scripts/ant/gpt2_rescoring.py
+++ scripts/ant/gpt2_rescoring.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 
 
 class GPT2Decoder():
-    def __init__(self, model_id="gpt2", device="cuda", lm_weight=2.0, ins_p=-0.5):
+    def __init__(self, model_id="gpt2", device="cuda", lm_weight=2.0, ins_p=-0.1):
         self.device = device
         self.model_id = model_id
         self.model = GPT2LMHeadModel.from_pretrained(model_id).to(device)

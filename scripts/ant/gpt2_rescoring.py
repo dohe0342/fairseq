cdcd8ca98c1ab0e8f44fc1012b476fe4de883a5e
dohe0342 <kimdohe1070@gmail.com>
2023-01-17 13:38:22 +0900

    from local

--- scripts/ant/gpt2_rescoring.py
+++ scripts/ant/gpt2_rescoring.py
@@ -30,8 +30,8 @@ class GPT2Decoder():
                 #print(input_ids[0].size())
                 output = self.model(input_ids[0][:i+1].unsqueeze(0))
                 print(output["logits"].size())
-                print(input_ids[i+1])
-                #print(output["logits"][-1][-1][input_ids[i+1]])
+                #print(input_ids[i+1])
+                print(output["logits"][-1][-1][input_ids[0][i+1]])
                 #score_list.append(output["logits"][-1][-1][input_ids[i+1]])
         print(score_list)
 

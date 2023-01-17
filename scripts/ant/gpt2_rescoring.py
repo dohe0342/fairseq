c6177cb578e7306d3bc0987d61438c13ae90ff01
dohe0342 <kimdohe1070@gmail.com>
2023-01-17 13:28:01 +0900

    from local

--- scripts/ant/gpt2_rescoring.py
+++ scripts/ant/gpt2_rescoring.py
@@ -27,6 +27,7 @@ class GPT2Decoder():
         with torch.no_grad():
             for i in range(0, len(input_ids)-1):
                 output = self.model(input_ids[:i+1])
+                print(output["logits"][-1][-1][input_ids[i+1]])
                 score_list.append(output["logits"][-1][-1][input_ids[i+1]])
         print(score_list)
 

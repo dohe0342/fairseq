b4f8f9c158efa63a7891354a413af59a48bff0aa
dohe0342 <kimdohe1070@gmail.com>
2023-01-17 13:07:00 +0900

    from local

--- scripts/ant/gpt2_rescoring.py
+++ scripts/ant/gpt2_rescoring.py
@@ -3,10 +3,13 @@ from datasets import load_dataset
 import torch
 from tqdm import tqdm
 
-device = "cuda"
-model_id = "gpt2"
-model = GPT2LMHeadModel.from_pretrained(model_id).to(device)
-tokenizer = GPT2TokenizerFast.from_pretrained(model_id)
+
+class GPT2Decoder():
+    def __init__(self, model_id, device):
+        device = "cuda"
+        model_id = "gpt2"
+        model = GPT2LMHeadModel.from_pretrained(model_id).to(device)
+        tokenizer = GPT2TokenizerFast.from_pretrained(model_id)
 
 text = 'I am a student.'
 encodings = tokenizer(text, return_tensors="pt")
@@ -16,41 +19,5 @@ encodings = encodings.to(device)
 
 with torch.no_grad():
     outputs = model(encodings["input_ids"])
-#print(outputs.keys())
+
 print(outputs["logits"].size())
-
-#test = load_dataset("wikitext", "wikitext-2-raw-v1", split="test")
-#encodings = tokenizer("\n\n".join(test["text"]), return_tensors="pt")
-
-#max_length = model.config.n_positions
-#stride = 512
-#seq_len = encodings.input_ids.size(1)
-
-
-'''
-nlls = []
-prev_end_loc = 0
-for begin_loc in tqdm(range(0, seq_len, stride)):
-    end_loc = min(begin_loc + max_length, seq_len)
-    trg_len = end_loc - prev_end_loc  # may be different from stride on last loop
-    input_ids = encodings.input_ids[:, begin_loc:end_loc].to(device)
-    target_ids = input_ids.clone()
-    target_ids[:, :-trg_len] = -100
-
-    with torch.no_grad():
-        outputs = model(input_ids, labels=target_ids)
-
-        # loss is calculated using CrossEntropyLoss which averages over input tokens.
-        # Multiply it with trg_len to get the summation instead of average.
-        # We will take average over all the tokens to get the true average
-        # in the last step of this example.
-        neg_log_likelihood = outputs.loss * trg_len
-
-    nlls.append(neg_log_likelihood)
-
-    prev_end_loc = end_loc
-    if end_loc == seq_len:
-        break
-
-ppl = torch.exp(torch.stack(nlls).sum() / end_loc)
-'''

65c2e37b4c13163fd70f441507606240ddafa00d
dohe0342 <kimdohe1070@gmail.com>
2023-01-17 12:58:06 +0900

    from local

--- scripts/ant/gpt2_rescoring.py
+++ scripts/ant/gpt2_rescoring.py
@@ -1,17 +1,17 @@
 from transformers import GPT2LMHeadModel, GPT2TokenizerFast
+from datasets import load_dataset
+import torch
+from tqdm import tqdm
 
 device = "cuda"
-model_id = "gpt2-large"
+model_id = "gpt2"
 model = GPT2LMHeadModel.from_pretrained(model_id).to(device)
 tokenizer = GPT2TokenizerFast.from_pretrained(model_id)
 
-from datasets import load_dataset
 
 test = load_dataset("wikitext", "wikitext-2-raw-v1", split="test")
 encodings = tokenizer("\n\n".join(test["text"]), return_tensors="pt")
 
-import torch
-from tqdm import tqdm
 
 max_length = model.config.n_positions
 stride = 512

743115b7001993dc4e9b4c382a90f9fc839d8c46
dohe0342 <kimdohe1070@gmail.com>
2023-01-17 09:07:36 +0900

    from local

--- examples/speech_recognition/new/decoders/flashlight_decoder.py
+++ examples/speech_recognition/new/decoders/flashlight_decoder.py
@@ -490,7 +490,7 @@ class FairseqLMDecoder(BaseDecoder):
             nbest_results = results[: self.nbest]
             
             hypos.append([make_hypo(result) for result in nbest_results])
-            print(hypos)
+            print(hypos[0]["words"])
             exit()
 
             self.lm.empty_cache()

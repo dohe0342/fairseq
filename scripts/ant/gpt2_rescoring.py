a60a4edb66223c96c70bb85953a202d9eb88d77e
dohe0342 <kimdohe1070@gmail.com>
2023-01-17 17:35:58 +0900

    from local

--- scripts/ant/gpt2_rescoring.py
+++ scripts/ant/gpt2_rescoring.py
@@ -84,5 +84,5 @@ if __name__ == "__main__":
             break
 
     wer = metric(lm_hyps, refs[:count])
-    origin_wer = metric(hyps[:count], refs[:count])
+    #origin_wer = metric(hyps[:count], refs[:count])
     print(origin_wer, wer)

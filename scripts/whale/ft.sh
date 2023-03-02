a87010f72b19d482690b52ff30ade17722df21f4
dohe0342 <kimdohe1070@gmail.com>
2023-03-02 11:37:52 +0900

    from local

--- scripts/whale/ft.sh
+++ scripts/whale/ft.sh
@@ -11,7 +11,7 @@ then
 		then
 			fairseq-hydra-train \
 				--config-dir /home/work/workspace/fairseq/examples/wav2vec/config/finetuning \
-				--config-name vox_960h_whale \
+				--config-name base_100h_whale \
 				task.data=/home/work/workspace/LibriSpeech/manifests \
 				model.w2v_path=/home/work/workspace/models/wav2vec_model/wav2vec_vox_new.pt \
 				checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 \

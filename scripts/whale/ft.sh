3c456888dac1589b713424b0973a3d1da3c9d2b7
dohe0342 <kimdohe1070@gmail.com>
2023-03-02 11:38:21 +0900

    from local

--- scripts/whale/ft.sh
+++ scripts/whale/ft.sh
@@ -23,7 +23,7 @@ then
 				--config-dir /home/work/workspace/fairseq/examples/wav2vec/config/finetuning \
 				--config-name base_100h_whale \
 				task.data=/home/work/workspace/LibriSpeech/manifests \
-				model.w2v_path=/home/work/workspace/models/wav2vec_model/wav2vec_vox_new.pt \
+				model.w2v_path=/home/work/workspace/models/wav2vec_model/wav2vec_small.pt \
 				checkpoint.save_dir=/home/work/workspace/fairseq/scripts/whale/outputs/$1 \
 				criterion._name=viewmaker \
 				+model.viewmaker=true 

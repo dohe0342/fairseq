7d5bae50530be3147f5f60487f69466cf094b9e4
dohe0342 <kimdohe1070@gmail.com>
2023-03-04 23:39:59 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -1216,7 +1216,7 @@ class CtcCriterion(FairseqCriterion):
         conv_feat = conv_feat + 10*conv_feat.grad/eps
         if 27211 in sample["id"]:
             if update_num < 362:
-                np.save(f'origin.npy', conv_feat.detach().cpu().numpy())
+                np.save(f'origin.npy', origin_convfeat.detach().cpu().numpy())
             #index = (sample["id"] == 5705).nonzero(as_tuple=True)[0].item()
             print(sample["id"], conv_feat.size())
             np.save(f'fgsm_{update_num}.npy', conv_feat.detach().cpu().numpy())

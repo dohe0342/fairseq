bba036496fc9ee3b0da7575520dd2eeb466caca2
dohe0342 <kimdohe1070@gmail.com>
2023-03-04 23:12:19 +0900

    from local

--- fairseq/criterions/ctc.py
+++ fairseq/criterions/ctc.py
@@ -1614,9 +1614,9 @@ class CtcCriterionViewmaker(FairseqCriterion):
         
         if 27211 in sample["id"]:
             conv_feat = net_output["pac_output"]
-            index = (sample["id"] == 5705).nonzero(as_tuple=True)[0].item()
+            #index = (sample["id"] == 5705).nonzero(as_tuple=True)[0].item()
             print(conv_feat.size())
-            np.save(f'pac_5705_{update_num}.npy', conv_feat[index].detach().cpu().numpy())
+            np.save(f'pac_{update_num}.npy', conv_feat.detach().cpu().numpy())
 
         if not model.training:
             import editdistance

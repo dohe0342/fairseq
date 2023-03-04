8c0dff9825075648747544bf99be3740ddfe7e8c
dohe0342 <kimdohe1070@gmail.com>
2023-03-04 15:22:42 +0900

    from local

--- fairseq/models/wav2vec/wav2vec2.py
+++ fairseq/models/wav2vec/wav2vec2.py
@@ -725,8 +725,7 @@ class Wav2Vec2Model(BaseFairseqModel):
             #print('x new:', x_new.size())
             #print('x: ', x.size())
             x_stack = torch.cat((x, x_new), dim=0)
-            print(padding_mask)
-            padding_mask = torch.cat((padding_mask, padding_mask), dim=0)
+            padding_mask = torch.cat((padding_mask, padding_mask), dim=0) if padding_mask is not None
             
             x_stack, layer_results, dropped_layer = self.encoder(
                 x_stack, 

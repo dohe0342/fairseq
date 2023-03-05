c828ec14ec4edd4a00593dae335ac8e097ba4d69
dohe0342 <kimdohe1070@gmail.com>
2023-03-05 13:58:51 +0900

    from local

--- scripts/whale/ft_data2vec.sh
+++ scripts/whale/ft_data2vec.sh
@@ -2,7 +2,7 @@ git pull
 
 fairseq-hydra-train \
 	--config-dir /home/work/workspace/fairseq/examples/wav2vec/config/finetuning \
-	--config-name base_960h_whale \
+	--config-name base_100h_nospec \
 	task.data=/home/work/workspace/LibriSpeech/manifests \
 	task.normalize=true \
 	model.w2v_path=/home/work/workspace/models/data2vec_model/audio_base_ls.pt \

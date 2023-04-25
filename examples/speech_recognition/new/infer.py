a88609cfc04f38cf889a611b9eacfaacd84cd02c
dohe0342 <kimdohe1070@gmail.com>
2023-04-25 16:49:46 +0900

    from local

--- examples/speech_recognition/new/infer.py
+++ examples/speech_recognition/new/infer.py
@@ -405,10 +405,6 @@ class InferenceProcessor:
         for i in sample['target'][0]:
             target_sentence += label_dict[i.item()]
         
-        print('target sentence = ')
-        print(target_sentence)
-        print('\n\n')
-        exit()
         '''
         if padding_mask is not None:
             padding_mask = ~padding_mask
